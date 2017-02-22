0b09ac37c7fbf674b89d1fe62ffe76ada5ad9023
Simppa Akaslompolo <simppa.akaslompolo@alumni.aalto.fi>
2017-02-22 10:52:07 +0100

    Now compiles and links on hydra

--- Makefile
+++ Makefile
@@ -1,4 +1,9 @@
+#CC=icc
+#CFLAGS= -std=c99 $(DEFINES) $(FLAGS) 
+#CFLAGS=-fopenmp -qno-openmp-offload -std=c99 $(DEFINES) $(FLAGS) 
+CFLAGS=-fopenmp -std=c99 -Wall $(DEFINES) $(FLAGS) 
 CC=h5cc
+#CFLAGS= -std=c99 $(DEFINES) $(FLAGS) 
 
 ifdef NSIMD
 	DEFINES+=-DNSIMD=$(NSIMD)
@@ -31,7 +36,9 @@ ifeq ($(MPI),1)
 	CC=mpiicc
 endif
 
-CFLAGS=-fopenmp -std=c99 $(DEFINES) $(FLAGS) 
+# Expose rand48 headers to compiler
+#DEFINES+= -D__USE_SVID=1
+
 
 HEADERS=ascot5.h B_GS.h math.h consts.h \
 	   	wall_2d.h ascot4_interface.h distributions.h B_2D.h \
@@ -42,24 +49,39 @@ HEADERS=ascot5.h B_GS.h math.h consts.h \
 		particle.h filip5.h endcond.h orbit_write.h \
 		B_field.h E_field.h wall.h phys_orbit.h
 
-OBJS=ascot4_interface.o B_GS.o math.o consts.o  \
+HOST_ONLY_OBJS=ascot4_interface.o hdf5_helpers.o hdf5_histogram.o \
+	 hdf5_particlestate.o 
+
+
+TARGET_OBJS=B_GS.o math.o consts.o  \
      wall_2d.o distributions.o B_2D.o B_ST.o B_TC.o  \
 	 plasma_1d.o interact.o step_gc_rk4.o step_fo_lf.o step_fo_vpa.o \
-	 hdf5_helpers.o hdf5_histogram.o B_3D.o simulate_fo_lf.o \
-	 simulate_gc_rk4.o wall_3d.o list.o octree.o hdf5_particlestate.o \
+	 B_3D.o simulate_fo_lf.o \
+	 simulate_gc_rk4.o wall_3d.o list.o octree.o\
      particle.o endcond.o B_field.o E_field.o wall.o simulate.o orbit_write.o \
 	step_gc_cashkarp.o phys_orbit.o
+
+OBJS=$(HOST_ONLY_OBJS) $(TARGET_OBJS)
 
 BINS=test_math \
 	 test_wall_2d test_ascot4_interface test_plasma_1d \
 	 test_interact test_hdf5 test_wall_3d test_particle filip5 \
 	 test_B ascot5_gc test_simulate_orbit
 
+# Make sure we first compile HOST_ONLY 
+hosts : $(HOST_ONLY_OBJS)
+
 all: $(BINS)
+
+objs : $(OBJS)
+
 
 ascotpy: CFLAGS+=-fPIC
 ascotpy: ascotpy.c B_none.o B_GS.o B_2Dlin.o B_2D.o B_3D.o
 	f2py ascotpy.pyf ascotpy.c B_none.o B_GS.o B_2Dlin.o B_2D.o B_3D.o -c $(DEFINES)
+
+test_offload : test_offload.o
+	$(CC) -o $@ $^ $(CFLAGS)
 
 ascot5_gc: ascot5_gc.o $(OBJS)
 	$(CC) -o $@ $^ $(CFLAGS)
@@ -102,3 +124,6 @@ test_interact: test_interact.o $(OBJS)
 
 clean:
 	rm -f *.o *.test *.optrpt $(BINS)
+
+
+
--- compile.cmd
+++ compile.cmd
@@ -23,7 +23,7 @@ export KMP_AFFINITY=verbose,compact
 export OFFLOAD_DEVICES=0
 export OFFLOAD_REPORT=2
 export MIC_ENV_PREFIX=MIC_
-export MIC_OMP_NUM_THREADS=1
+export MIC_OMP_NUM_THREADS=10
 export MIC_KMP_AFFINITY=explicit,granularity=fine,proclist=[1-236:1]
 
 # run the program
@@ -32,8 +32,37 @@ export MIC_KMP_AFFINITY=explicit,granularity=fine,proclist=[1-236:1]
 #poe /u/${USER}/mictest.exe
 
 #module unload intel
-module load   hdf5-serial/1.8.16 xeon-phi-rt/16.0 gcc
+#module unload intel/16.0
+module load   intel/16.0  hdf5-serial/1.8.16 xeon-phi-rt/16.0 gcc
 module list
 
+#make clean
+
+#make TARGET=1 ascot5_gc FLAGS=" -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -fPIC -L/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib /u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib/libhdf5_hl.a /u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib/libhdf5.a -lrt -lz -ldl -lm -Wl,-rpath -Wl,/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib -I/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/include/"
+
+#make clean
+#make TARGET=1 hosts
+
+#make TARGET=1 ascot5_gc FLAGS=" -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -fPIC -L/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib -lhdf5_hl -lhdf5 -lrt -lz -ldl -lm -Wl,-rpath -Wl,/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib -I/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/include/"
+
+#nm /u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib/libhdf5.a > libhdf5.a.txt
+
 make clean
-make TARGET=1
+#h5cc -c -o ascot4_interface.o ascot4_interface.c -fopenmp -qno-offload -std=c99 -Wall -DCOULOMBCOLL=0
+
+#make TARGET=1 NSIMD=16 test_offload
+
+#echo ----------------
+
+#./test_offload
+
+#echo ----------------
+
+
+
+make TARGET=1 NSIMD=16 ascot5_gc
+
+#make TARGET=1 ascot4_interface.o
+#make TARGET=1 NSIMD=16 objs
+
+#icc -o ascot5_gc ascot5_gc.o ascot4_interface.o hdf5_helpers.o hdf5_histogram.o hdf5_particlestate.o B_GS.o math.o consts.o wall_2d.o distributions.o B_2D.o B_ST.o B_TC.o plasma_1d.o interact.o step_gc_rk4.o step_fo_lf.o step_fo_vpa.o B_3D.o simulate_fo_lf.o simulate_gc_rk4.o wall_3d.o list.o octree.o particle.o endcond.o B_field.o E_field.o wall.o simulate.o orbit_write.o step_gc_cashkarp.o phys_orbit.o -fopenmp -std=c99 -Wall -DNSIMD=16 -DCOULOMBCOLL=0  -D_LARGEFILE64_SOURCE -D_LARGEFILE_SOURCE -fPIC -L/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib /u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib/libhdf5_hl.a /u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib/libhdf5.a -lrt -lz -ldl -lm -Wl,-rpath -Wl,/u/system/SLES11/soft/hdf5/1.8.16/intel16.0/serial/lib
