902eb36719af069730631a976162c1eded6a6778
Joona Kontula <joona.kontula@aalto.fi>
2017-04-12 13:15:09 +0300

    Better test routines for bfield and efield.

--- test_B.c
+++ test_B.c
@@ -1,6 +1,7 @@
 /**
  * @file test_B.c
  * @brief Test program for magnetic fields
+ * Output is written to standard output, redirect with test_B > output.filename
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,31 +10,31 @@
 
 int main(int argc, char** argv) {
     if(argc < 10) {
-        printf("Usage: test_B nr rmin rmax nz zmin zmax nphi phimin phimax\n");
+        printf("Usage: test_B nr rmin rmax nphi phimin phimax nz zmin zmax\n");
         exit(1);
     }
 
+    /* Init magnetic background */
     B_field_offload_data offload_data;
     real* offload_array;
     B_field_init_offload(&offload_data, &offload_array);
-
     B_field_data Bdata;
     B_field_init(&Bdata, &offload_data, offload_array);
 
     real B[3];
     real B_dB[12];
-    real psi[1];
-    real rho[1];
+    real psi;
+    real rho;
 
     int n_r = atof(argv[1]);
     real r_min = atof(argv[2]);
-    real r_max =atof(argv[3]);
-    int n_z =atof(argv[4]);
-    real z_min =atof(argv[5]);
-    real z_max = atof(argv[6]);
-    int n_phi = atof(argv[7]);
-    real phi_min = atof(argv[8]) / 180 * math_pi;
-    real phi_max = atof(argv[9]) / 180 * math_pi;
+    real r_max = atof(argv[3]);
+    int n_phi = atof(argv[4]);
+    real phi_min = atof(argv[5]) / 180 * math_pi;
+    real phi_max = atof(argv[6]) / 180 * math_pi;
+    int n_z = atof(argv[7]);
+    real z_min =atof(argv[8]);
+    real z_max = atof(argv[9]);
 
     real* r = (real*) malloc(n_r * sizeof(real));
     math_linspace(r, r_min, r_max, n_r);
@@ -42,24 +43,26 @@ int main(int argc, char** argv) {
     real* phi = (real*) malloc(n_phi * sizeof(real));
     math_linspace(phi, phi_min, phi_max, n_phi);
 
+    /* Write header specifying grid dimensions */
+    printf("%d, %le, %le, ", n_r, r_min, r_max);
+    printf("%d, %le, %le, ", n_phi, phi_min, phi_max);
+    printf("%d, %le, %le\n", n_z, z_min, z_max);
+    
     int i, j, k;
     for(i = 0; i < n_r; i++) {
         for(j = 0; j < n_z; j++) {
             for(k = 0; k < n_phi; k++) {
                 B_field_eval_B(B, r[i], phi[k], z[j], &Bdata);
                 B_field_eval_B_dB(B_dB, r[i], phi[k], z[j], &Bdata);
-                B_field_eval_psi(psi, r[i], phi[k], z[j], &Bdata);
-                B_field_eval_rho(rho, psi[0], &Bdata);
-                printf("%le, %le, %le, ", r[i], phi[k], z[j]);
-                printf("%le, %le, %le, ", B[0], B[1], B[2]);
-                printf("%le, %le, %le, ", B_dB[0], B_dB[4], B_dB[8]);
-                printf("%le, %le, %le, ", B_dB[1], B_dB[2], B_dB[3]);
-                printf("%le, %le, %le, ", B_dB[5], B_dB[6], B_dB[7]);
-                printf("%le, %le, %le, ", B_dB[9], B_dB[10], B_dB[11]);
-                printf("%le, %le\n", psi[0], rho[0]);
+                B_field_eval_psi(&psi, r[i], phi[k], z[j], &Bdata);
+                B_field_eval_rho(&rho, psi, &Bdata);
+                printf("%le %le %le ", B[0], B[1], B[2]);
+                printf("%le %le %le ", B_dB[1], B_dB[2], B_dB[3]);
+                printf("%le %le %le ", B_dB[5], B_dB[6], B_dB[7]);
+                printf("%le %le %le ", B_dB[9], B_dB[10], B_dB[11]);
+                printf("%le\n", rho);
             }
         }
     }
-
     return 0;
 }
--- test_E.c
+++ test_E.c
@@ -1,11 +1,7 @@
 /**
  * @file test_E.c
  * @brief Test program for electric fields
- *
  * Output is written to standard output, redirect with test_E > output.filename
- * Output is in the format:
- *   r, phi, z, rho, drho/dr, drho/dphi, drho/dz, E_r, E_phi, E_z
- * 
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,23 +10,20 @@
 #include "math.h"
 
 int main(int argc, char** argv) {
-
     if(argc < 10) {
-        printf("Usage: test_E nr rmin rmax nz zmin zmax nphi phimin phimax\n");
+        printf("Usage: test_E nr rmin rmax nphi phimin phimax nz zmin zmax\n");
         exit(1);
     }
 
-    /* Init background */
+    /* Init magnetic background */
     B_field_offload_data offload_Bdata;
     real* offload_array;
     B_field_init_offload(&offload_Bdata, &offload_array);
-
     B_field_data Bdata;
     B_field_init(&Bdata, &offload_Bdata, offload_array);
-
+    /* Init electric field */    
     E_field_offload_data offload_Edata;
     E_field_init_offload(&offload_Edata, &offload_array);
-
     E_field_data Edata;
     E_field_init(&Edata, &offload_Edata, offload_array);
 
@@ -39,13 +32,13 @@ int main(int argc, char** argv) {
 
     int n_r = atof(argv[1]);
     real r_min = atof(argv[2]);
-    real r_max =atof(argv[3]);
-    int n_z =atof(argv[4]);
-    real z_min =atof(argv[5]);
-    real z_max = atof(argv[6]);
-    int n_phi = atof(argv[7]);
-    real phi_min = atof(argv[8]) / 180 * math_pi;
-    real phi_max = atof(argv[9]) / 180 * math_pi;
+    real r_max = atof(argv[3]);
+    int n_phi = atof(argv[4]);
+    real phi_min = atof(argv[5]) / 180 * math_pi;
+    real phi_max = atof(argv[6]) / 180 * math_pi;
+    int n_z = atof(argv[7]);
+    real z_min = atof(argv[8]);
+    real z_max = atof(argv[9]);
 
     real* r = (real*) malloc(n_r * sizeof(real));
     math_linspace(r, r_min, r_max, n_r);
@@ -54,18 +47,21 @@ int main(int argc, char** argv) {
     real* phi = (real*) malloc(n_phi * sizeof(real));
     math_linspace(phi, phi_min, phi_max, n_phi);
 
+    /* Write header specifying grid dimensions */
+    printf("%d, %le, %le, ", n_r, r_min, r_max);
+    printf("%d, %le, %le, ", n_phi, phi_min, phi_max);
+    printf("%d, %le, %le\n", n_z, z_min, z_max);
+
     int i, j, k;
     for(i = 0; i < n_r; i++) {
         for(j = 0; j < n_z; j++) {
             for(k = 0; k < n_phi; k++) {
                 B_field_eval_rho_drho(rho, r[i], phi[k], z[j], &Bdata);
                 E_field_eval_E(E, rho, &Edata);
-                printf("%le, %le, %le, ", r[i], phi[k], z[j]);
-                printf("%le, %le, %le, %le, ", rho[0], rho[1], rho[2], rho[3]);
-                printf("%le, %le, %le\n", E[0], E[1], E[2]);
+                printf("%le %le %le %le ", rho[0], rho[1], rho[2], rho[3]);
+                printf("%le %le %le\n", E[0], E[1], E[2]);
             }
         }
     }
-    
     return 0;
 }
