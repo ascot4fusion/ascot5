aab404b82a03f69b69b0896e72a43962f68a30c5
Jari Varje <jari.varje@aalto.fi>
2017-07-27 14:20:54 +0300

    Fixed header for particlestate_write

--- hdf5io/hdf5_particlestate.h
+++ hdf5io/hdf5_particlestate.h
@@ -8,6 +8,6 @@
 #include <hdf5.h>
 #include "../particle.h"
 
-int hdf5_particlestate_write(char* fn, char *state, int n, input_particle* p);
+int hdf5_particlestate_write(char* fn, char *state, int n, particle_state* p);
 
 #endif
