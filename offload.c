160aa2ebb873756e25232fb1bc6b2c153f2c417a
Jari Varje <jari.varje@aalto.fi>
2017-07-28 12:26:55 +0300

    Offload array outside offload package struct

--- offload.c
+++ offload.c
@@ -8,46 +8,43 @@
 #include "ascot5.h"
 #include "offload.h"
 
-void offload_init_offload(offload_package* o) {
-    o->offload_array = NULL;
+void offload_init_offload(offload_package* o, real** offload_array) {
+    *offload_array = NULL;
     o->offload_array_length = 0;
     o->unpack_pos = 0;
 }
 
-void offload_free_offload(offload_package* o) {
-    free(o->offload_array);
+void offload_free_offload(offload_package* o, real** offload_array) {
+    free(*offload_array);
+    *offload_array = NULL;
     o->offload_array_length = 0;
     o->unpack_pos = 0;
 }
 
-void offload_pack(offload_package* o, real* pack_array, size_t pack_length) {
+void offload_pack(offload_package* o, real** offload_array, real* pack_array,
+                  size_t pack_length) {
     size_t new_length = o->offload_array_length + pack_length;
 
     real* new_array = (real*) malloc(new_length * sizeof(real));
 
     if(o->offload_array_length > 0) {
-        memcpy(new_array, o->offload_array, o->offload_array_length);
+        memcpy(new_array, *offload_array, o->offload_array_length*sizeof(real));
     }
 
-    memcpy(new_array+o->offload_array_length, pack_array, pack_length);
+    memcpy(new_array+o->offload_array_length, pack_array,
+           pack_length*sizeof(real));
 
-    free(o->offload_array);
+    free(*offload_array);
 
-    o->offload_array = new_array;
+    *offload_array = new_array;
     o->offload_array_length = new_length;
 }
 
-void offload_init(offload_package* o, size_t offload_array_length,
-                  real* offload_array) {
-    o->offload_array = offload_array;
-    o->offload_array_length = offload_array_length;
-    o->unpack_pos = 0;
-}
+real* offload_unpack(offload_package* o, real* offload_array,
+                     size_t pack_length) {
+    real* ptr = offload_array + o->unpack_pos;
 
-real* offload_unpack(offload_package* o, size_t pack_length) {
-    real* ptr = o->offload_array + o->unpack_pos;
-
-    o->unpack_pos = o->unpack_pos + pack_length;
+    o->unpack_pos += pack_length;
 
     return ptr;
 }
--- offload.h
+++ offload.h
@@ -6,20 +6,19 @@
 #define OFFLOAD_H
 
 typedef struct {
-    real* offload_array;
     size_t offload_array_length;
     size_t unpack_pos;
 } offload_package;
 
-void offload_init_offload(offload_package* o);
-void offload_free_offload(offload_package* o);
+void offload_init_offload(offload_package* o, real** offload_array);
+void offload_free_offload(offload_package* o, real** offload_array);
 
-void offload_pack(offload_package* o, real* pack_array, size_t pack_length);
+void offload_pack(offload_package* o, real** offload_array, real* pack_array,
+                  size_t pack_length);
 
 #pragma omp declare target
-void offload_init(offload_package* o, size_t offload_array_length,
-                  real* offload_array);
-real* offload_unpack(offload_package* o, size_t pack_length);
+real* offload_unpack(offload_package* o, real* offload_array,
+                     size_t pack_length);
 #pragma omp end declare target
 
 #endif
