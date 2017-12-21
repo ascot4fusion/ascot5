8950380d9a647fbf2fbba088fddd6ea113a44ed2
Joona Kontula <joona.kontula@aalto.fi>
2017-12-21 13:58:28 +0200

    Uncommented simdlen statements in interp1Dcomp.

--- spline/interp1Dcomp.h
+++ spline/interp1Dcomp.h
@@ -20,13 +20,13 @@ typedef struct {
 #pragma omp declare target
 int interp1Dcomp_init(interp1D_data* str, real* f, int n_r,
 		      real r_min, real r_max, real r_grid);
-#pragma omp declare simd uniform(str) /* simdlen(8) */
+#pragma omp declare simd uniform(str) simdlen(8)
 int interp1Dcomp_eval_B(real* B, interp1D_data* str, real r);
-#pragma omp declare simd linear(i) uniform(B, str) /* simdlen(8) */
+#pragma omp declare simd linear(i) uniform(B, str) simdlen(8)
 int interp1Dcomp_eval_B_SIMD(int i, real B[NSIMD], interp1D_data* str, real r);
-#pragma omp declare simd uniform(str) /* simdlen(8) */
+#pragma omp declare simd uniform(str) simdlen(8)
 int interp1Dcomp_eval_dB(real* B_dB, interp1D_data* str, real r);
-#pragma omp declare simd linear(i) uniform(B_dB, str) /* simdlen(8) */
+#pragma omp declare simd linear(i) uniform(B_dB, str) simdlen(8)
 int interp1Dcomp_eval_dB_SIMD(int i, real B_dB[6][NSIMD], interp1D_data* str, real r);
 void interp1Dcomp_free(interp1D_data* str);
 #pragma omp end declare target
