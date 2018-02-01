0808b621dda5dfc5b0325636b7d002a51b7f22ab
sarkimk1 <konsta.sarkimaki@aalto.fi>
2018-02-01 15:03:36 +0200

    Marker input compatible with new hdf5 structure.

--- hdf5io/hdf5_markers.c
+++ hdf5io/hdf5_markers.c
@@ -6,7 +6,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <hdf5.h>
-#include "hdf5_hl.h"
+#include <hdf5_hl.h>
+#include "hdf5_helpers.h"
 #include "../particle.h"
 #include "../math.h"
 #include "../consts.h"
@@ -23,17 +24,37 @@
 void hdf5_markers_init(hid_t f, int *n, input_particle** p) {
     herr_t err;
 
-    err = H5LTfind_dataset(f, "/markers/");
+    err = H5LTfind_dataset(f, "/marker/");
     if(err < 0) {
         return;
     }
 
+    #if VERBOSE > 0
+        printf("Reading marker input from the HDF5 file...\n");
+    #endif
+    
+    err = hdf5_find_group(f, "/marker/");
+    if(err < 0) {
+        return -1;
+    }
+    
+    char active[11];
+    err = H5LTget_attribute_string(f, "/marker/", "active", active);
+    if(err < 0) {
+        return -1;
+    }
+    active[10] = '\0';
+    
+    #if VERBOSE > 0
+        printf("Active qid is %s\n", active);
+    #endif
+
     int n_particle;
     int n_guiding_center;
     int n_field_line;    
-    err = H5LTget_attribute_int(f, "/markers/", "n_particle", &n_particle);
-    err = H5LTget_attribute_int(f, "/markers/", "n_guiding_center", &n_guiding_center);
-    err = H5LTget_attribute_int(f, "/markers/", "n_field_line", &n_field_line);
+    err = H5LTget_attribute_int(f, "/marker/", "n_particle", &n_particle);
+    err = H5LTget_attribute_int(f, "/marker/", "n_guiding_center", &n_guiding_center);
+    err = H5LTget_attribute_int(f, "/marker/", "n_field_line", &n_field_line);
     
     *p = (input_particle*) malloc((n_particle + n_guiding_center + n_field_line) * sizeof(input_particle));
 
@@ -43,17 +64,33 @@ void hdf5_markers_init(hid_t f, int *n, input_particle** p) {
     input_particle* guiding_center = &(*p)[n_particle];
     input_particle* field_line = &(*p)[n_particle + n_guiding_center];
 
-    hdf5_markers_init_particle(f, n_particle, particle);
-    hdf5_markers_init_guiding_center(f, n_guiding_center, guiding_center);
-    hdf5_markers_init_field_line(f, n_field_line, field_line);
+    /* Go through all different input types and see which one the active qid corresponds to.
+     * Then read this input. */
+    char path[256];
+	
+    hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX", active, path);
+    if(hdf5_find_group(f, path) == 0) {
+	hdf5_markers_init_particle(f, n_particle, particle, active);
+    }
+    
+    hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX", active, path);
+    if(hdf5_find_group(f, path) == 0) {
+	hdf5_markers_init_guiding_center(f, n_guiding_center, guiding_center, active);
+    }
+    
+    hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX", active, path);
+    if(hdf5_find_group(f, path) == 0) {
+	hdf5_markers_init_field_line(f, n_field_line, field_line, active);
+    }
 
     *n = n_particle + n_guiding_center + n_field_line;
 
 }
 
 
-void hdf5_markers_init_particle(hid_t f, int n, input_particle* p) {
+void hdf5_markers_init_particle(hid_t f, int n, input_particle* p, char* qid) {
     herr_t err;
+    char path[256];
     int i;
     
     real* r      = malloc(n * sizeof(real));
@@ -68,17 +105,17 @@ void hdf5_markers_init_particle(hid_t f, int n, input_particle* p) {
     real* time   = malloc(n * sizeof(real));
     integer* id  = malloc(n * sizeof(integer));
      
-    err = H5LTread_dataset_double(f,"/markers/particle/r", r);
-    err = H5LTread_dataset_double(f,"/markers/particle/phi", phi);    
-    err = H5LTread_dataset_double(f,"/markers/particle/z", z);
-    err = H5LTread_dataset_double(f,"/markers/particle/v_r", v_r);
-    err = H5LTread_dataset_double(f,"/markers/particle/v_phi", v_phi);
-    err = H5LTread_dataset_double(f,"/markers/particle/v_z", v_z);
-    err = H5LTread_dataset_double(f,"/markers/particle/mass", mass);
-    err = H5LTread_dataset_int(f,"/markers/particle/charge", charge);
-    err = H5LTread_dataset_double(f,"/markers/particle/weight", weight);   
-    err = H5LTread_dataset_double(f,"/markers/particle/time", time);
-    err = H5LTread_dataset_long(f,"/markers/particle/id", id);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/r", qid, path), r);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/phi", qid, path), phi);    
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/z", qid, path), z);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/v_r", qid, path), v_r);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/v_phi", qid, path), v_phi);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/v_z", qid, path), v_z);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/mass", qid, path), mass);
+    err = H5LTread_dataset_int(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/charge", qid, path), charge);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/weight", qid, path), weight);   
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/time", qid, path), time);
+    err = H5LTread_dataset_long(f, hdf5_generate_qid_path("/marker/particle-XXXXXXXXXX/id", qid, path), id);
 
     for(i = 0; i < n; i++) {
         p[i].p.r      = r[i];
@@ -109,8 +146,9 @@ void hdf5_markers_init_particle(hid_t f, int n, input_particle* p) {
 }
 
 
-void hdf5_markers_init_guiding_center(hid_t f, int n, input_particle* p) {
+void hdf5_markers_init_guiding_center(hid_t f, int n, input_particle* p, char* qid) {
     herr_t err;
+    char path[256];
     int i;
     
     real* r      = malloc(n * sizeof(real));
@@ -125,17 +163,17 @@ void hdf5_markers_init_guiding_center(hid_t f, int n, input_particle* p) {
     real* time   = malloc(n * sizeof(real));
     integer* id  = malloc(n * sizeof(integer));
      
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/r", r);
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/phi", phi);    
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/z", z);
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/energy", energy);
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/pitch", pitch); 
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/theta", theta);
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/mass", mass);
-    err = H5LTread_dataset_int(f,"/markers/guiding_center/charge", charge);
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/weight", weight);   
-    err = H5LTread_dataset_double(f,"/markers/guiding_center/time", time);
-    err = H5LTread_dataset_long(f,"/markers/guiding_center/id", id);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/r", qid, path), r);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/phi", qid, path), phi);    
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/z", qid, path), z);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/energy", qid, path), energy);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/pitch", qid, path), pitch); 
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/theta", qid, path), theta);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/mass", qid, path), mass);
+    err = H5LTread_dataset_int(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/charge", qid, path), charge);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/weight", qid, path), weight);   
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/time", qid, path), time);
+    err = H5LTread_dataset_long(f, hdf5_generate_qid_path("/marker/guiding_center-XXXXXXXXXX/id", qid, path), id);
         
     for(i = 0; i < n; i++) {
         p[i].p_gc.r      = r[i];
@@ -165,8 +203,9 @@ void hdf5_markers_init_guiding_center(hid_t f, int n, input_particle* p) {
     free(id);
 }
 
-void hdf5_markers_init_field_line(hid_t f, int n, input_particle* p) {
+void hdf5_markers_init_field_line(hid_t f, int n, input_particle* p, char* qid) {
     herr_t err;
+    char path[256];
     int i;
     
     real* r      = malloc(n * sizeof(real));
@@ -176,14 +215,14 @@ void hdf5_markers_init_field_line(hid_t f, int n, input_particle* p) {
     real* weight = malloc(n * sizeof(real));
     real* time   = malloc(n * sizeof(real));
     integer* id  = malloc(n * sizeof(integer));
-     
-    err = H5LTread_dataset_double(f,"/markers/field_line/r", r);
-    err = H5LTread_dataset_double(f,"/markers/field_line/phi", phi);    
-    err = H5LTread_dataset_double(f,"/markers/field_line/z", z);
-    err = H5LTread_dataset_double(f,"/markers/field_line/pitch", pitch);
-    err = H5LTread_dataset_double(f,"/markers/field_line/weight", weight);
-    err = H5LTread_dataset_double(f,"/markers/field_line/time", time);
-    err = H5LTread_dataset_long(f,"/markers/field_line/id", id);
+    
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/r", qid, path), r);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/phi", qid, path), phi);    
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/z", qid, path), z);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/pitch", qid, path), pitch);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/weight", qid, path), weight);
+    err = H5LTread_dataset_double(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/time", qid, path), time);
+    err = H5LTread_dataset_long(f, hdf5_generate_qid_path("/marker/field_line-XXXXXXXXXX/id", qid, path), id);
         
     for(i = 0; i < n; i++) {
         p[i].p_ml.r      = r[i];
--- hdf5io/hdf5_markers.h
+++ hdf5io/hdf5_markers.h
@@ -6,10 +6,10 @@
 
 void hdf5_markers_init(hid_t f, int *n, input_particle** p);
 
-void hdf5_markers_init_particle(hid_t f, int n, input_particle* p);
+void hdf5_markers_init_particle(hid_t f, int n, input_particle* p, char* qid);
 
-void hdf5_markers_init_guiding_center(hid_t f, int n, input_particle* p);
+void hdf5_markers_init_guiding_center(hid_t f, int n, input_particle* p, char* qid);
 
-void hdf5_markers_init_field_line(hid_t f, int n, input_particle* p);
+void hdf5_markers_init_field_line(hid_t f, int n, input_particle* p, char* qid);
 
 #endif
