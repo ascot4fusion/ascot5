c9ce586be07cd5637d40938afe7f6a76227ee525
Jari Varje <jari.varje@aalto.fi>
2018-04-24 11:48:14 +0300

    Write distributions correctly if both 5D and 6D enabled simultaneously

--- hdf5io/hdf5_diag.c
+++ hdf5io/hdf5_diag.c
@@ -4,11 +4,11 @@
 
 void hdf5_diag_write(sim_offload_data* sim, real* diag_offload_array, char* out, char* qid) {
     if(sim->diag_offload_data.dist5D_collect) {
-        hdf5_dist_write_5D(&sim->diag_offload_data.dist5D, diag_offload_array, out, qid);
+        hdf5_dist_write_5D(&sim->diag_offload_data.dist5D, &diag_offload_array[sim->diag_offload_data.offload_dist5D_index], out, qid);
     }
 
     if(sim->diag_offload_data.dist6D_collect) {
-        hdf5_dist_write_6D(&sim->diag_offload_data.dist6D, diag_offload_array, out, qid);
+        hdf5_dist_write_6D(&sim->diag_offload_data.dist6D, &diag_offload_array[sim->diag_offload_data.offload_dist6D_index], out, qid);
     }
 }
 
