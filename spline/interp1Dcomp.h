c5bef65c81e482f2444d73f565fa263036ee6071
Joona Kontula <joona.kontula@aalto.fi>
2018-06-08 14:22:01 +0300

    Slight refactor of interp1D code.

--- spline/interp1Dcomp.h
+++ spline/interp1Dcomp.h
@@ -5,17 +5,7 @@
 #ifndef INTERP1DCOMP_H
 #define INTERP1DCOMP_H
 #include "../ascot5.h"
-
-/**
- * @brief Interpolation struct
- */
-typedef struct {
-    int n_r;                  /**< number of r grid points */
-    real r_min;               /**< minimum r coordinate in the grid */
-    real r_max;               /**< r grid interval (r_max-r_min)/(n_r-1) */
-    real r_grid;              /**< r grid interval (r_max-r_min)/(n_r-1) */
-    real* c;                  /**< pointer to array with spline coefficients */
-} interp1D_data;
+#include "interp1D.h"
 
 #pragma omp declare target
 int interp1Dcomp_init(interp1D_data* str, real* f, int n_r,
@@ -30,4 +20,5 @@ int interp1Dcomp_eval_dB(real* B_dB, interp1D_data* str, real r);
 int interp1Dcomp_eval_dB_SIMD(int i, real B_dB[6][NSIMD], interp1D_data* str, real r);
 void interp1Dcomp_free(interp1D_data* str);
 #pragma omp end declare target
+
 #endif
