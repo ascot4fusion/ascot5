abf36dc0351057c2f76ac4e62f2e66f03424e288
lauranu1 <unna.lauranto@aalto.fi>
2018-06-18 14:02:14 +0300

    added rho-distributions in 5D and 6D

--- hdf5io/hdf5_diag.c
+++ hdf5io/hdf5_diag.c
@@ -10,5 +10,12 @@ void hdf5_diag_write(sim_offload_data* sim, real* diag_offload_array, char* out,
     if(sim->diag_offload_data.dist6D_collect) {
         hdf5_dist_write_6D(&sim->diag_offload_data.dist6D, &diag_offload_array[sim->diag_offload_data.offload_dist6D_index], out, qid);
     }
+    if(sim->diag_offload_data.distrho5D_collect) {
+        hdf5_dist_write_rho5D(&sim->diag_offload_data.distrho5D, &diag_offload_array[sim->diag_offload_data.offload_distrho5D_index], out, qid);
+    }
+
+    if(sim->diag_offload_data.distrho6D_collect) {
+        hdf5_dist_write_rho6D(&sim->diag_offload_data.distrho6D, &diag_offload_array[sim->diag_offload_data.offload_distrho6D_index], out, qid);
+    }
 }
 
