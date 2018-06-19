9feaa7ae23165c8563d45b3a305827ff487805f7
Joona Kontula <joona.kontula@aalto.fi>
2018-06-19 17:01:31 +0300

    Comments and more descriptive variable names for symmetry module.

--- symmetry.h
+++ symmetry.h
@@ -13,9 +13,9 @@ typedef enum symmetry_type {
 } symmetry_type;
 
 #pragma omp declare target
-a5err symmetry_apply_scalar(real rphiz[], real r, real phi, real z,
+a5err symmetry_apply_scalar(real r_phi_z[], real r0, real phi0, real z0,
                             symmetry_type type, real period_length);
-a5err symmetry_apply_vector(real rphiz[], real scaling[], real r, real phi, real z,
+a5err symmetry_apply_vector(real r_phi_z[], real scaling[], real r0, real phi0, real z0,
                             symmetry_type type, real period_length);
 #pragma omp end declare target   
 
