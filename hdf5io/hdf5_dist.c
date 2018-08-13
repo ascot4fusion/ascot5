e8ed7c12ab72b5e5af89b12b9018c71a40fafad1
Joona Kontula <joona.kontula@aalto.fi>
2018-08-13 13:11:25 +0300

    Phi abscissa in distribution output is now in degrees, consistently with abscissa_name and input format.

--- hdf5io/hdf5_dist.c
+++ hdf5io/hdf5_dist.c
@@ -3,6 +3,7 @@
 #include "../dist_5D.h"
 #include "../dist_6D.h"
 #include "../print.h"
+#include "../math.h"
 #include "hdf5_helpers.h"
 #include "hdf5_histogram.h"
 
@@ -27,7 +28,7 @@ void hdf5_dist_write_5D(dist_5D_offload_data* dist, real* hist, char* filename,
 
     double abscissa_min[7];
     abscissa_min[0] = dist->min_r;
-    abscissa_min[1] = dist->min_phi;
+    abscissa_min[1] = math_rad2deg(dist->min_phi);
     abscissa_min[2] = dist->min_z;
     abscissa_min[3] = dist->min_vpara;
     abscissa_min[4] = dist->min_vperp;
@@ -36,7 +37,7 @@ void hdf5_dist_write_5D(dist_5D_offload_data* dist, real* hist, char* filename,
 
     double abscissa_max[7];
     abscissa_max[0] = dist->max_r;
-    abscissa_max[1] = dist->max_phi;
+    abscissa_max[1] = math_rad2deg(dist->max_phi);
     abscissa_max[2] = dist->max_z;
     abscissa_max[3] = dist->max_vpara;
     abscissa_max[4] = dist->max_vperp;
@@ -100,7 +101,7 @@ void hdf5_dist_write_6D(dist_6D_offload_data* dist, real* hist, char* filename,
 
     double abscissa_min[8];
     abscissa_min[0] = dist->min_r;
-    abscissa_min[1] = dist->min_phi;
+    abscissa_min[1] = math_rad2deg(dist->min_phi);
     abscissa_min[2] = dist->min_z;
     abscissa_min[3] = dist->min_vr;
     abscissa_min[4] = dist->min_vphi;
@@ -110,7 +111,7 @@ void hdf5_dist_write_6D(dist_6D_offload_data* dist, real* hist, char* filename,
 
     double abscissa_max[8];
     abscissa_max[0] = dist->max_r;
-    abscissa_max[1] = dist->max_phi;
+    abscissa_max[1] = math_rad2deg(dist->max_phi);
     abscissa_max[2] = dist->max_z;
     abscissa_max[3] = dist->max_vr;
     abscissa_max[4] = dist->max_vphi;
