0a99cb9fa5fc9564ded3e36c89f525bfebf8f966
sarkimk1 <konsta.sarkimaki@aalto.fi>
2018-11-05 18:48:16 +0200

    Wrote documentation on endconds. See #12

--- endcond.c
+++ endcond.c
@@ -1,6 +1,52 @@
 /**
  * @file endcond.c
- * @brief Check for end conditions
+ * @brief Marker simulation end conditions
+ *
+ * In the absence of errors, marker simulation is ended when marker meets even
+ * one of the active end conditions. User can choose which end conditions are
+ * active.
+ *
+ * The end conditions are:
+ * - tmax: Marker time exceeds maximum simulation time
+ *
+ * - emin: Marker energy is below minimum value
+ *
+ * - therm: energy is below value derived from local thermal electron energy
+ *
+ * - wall: Marker has intersected wall
+ *
+ * - rhomin: Marker has reached minimum rho (normalized poloidal flux) value
+ *
+ * - rhomax: has reached maximum rho value
+ *
+ * - polmax: The total cumulative distance marker has travelled poloidally
+ *   exceeds maximum value
+ *
+ * - tormax: The total cumulative distance marker has travelled toroidally
+ *   exceeds maximum value
+ *
+ * - cpumax: Marker simulation has exceeded maximum wall time
+ *
+ * - hybrid: Not an end condition per se but used to notate that the guiding
+ *   center simulation will be resumed as a gyro-orbit simulation
+ *
+ * Note: emin and therm, rhomin and rhomax, and polmax and tormax are pairs
+ * where both members are either active at the same time or neither is i.e.,
+ * they cannot be toggled separately.
+ *
+ * As magnetic field lines have no energy, emin and therm are never checked for
+ * them. Guiding centers are the only markers for which hybrid is checked.
+ *
+ * In the code, the end conditions are represented as bit arrays with each bit
+ * corresponding to a specific end condition. Each marker has a field "endcond",
+ * and when marker meets an end condition, the corresponding bit is flagged.
+ * This way if marker simultaneously meets several end conditions, all can be
+ * flagged.
+ *
+ * Additionally, when marker meets an end condition, its running state is set to
+ * False which notates its simulation should be discontinued. If the end
+ * condition is wall collision, the ID of the wall element the marker collided
+ * with is stored in the marker fields.
  */
 #include <math.h>
 #include "endcond.h"
@@ -11,193 +57,237 @@
 #include "math.h"
 #include "plasma.h"
 
-void endcond_check_fo(particle_simd_fo* p_f, particle_simd_fo* p_i, sim_data* sim) {
-    int i;
+/**
+ * @brief Check end conditions for FO markers
+ *
+ * The end conditions are checked for all markers within the SIMD marker struct.
+ *
+ * @param p_f pointer to SIMD struct storing marker states at the end of current
+ *        time-step
+ * @param p_i pointer to SIMD struct storing marker states at the beginning of
+ *        current time-step
+ */
+void endcond_check_fo(particle_simd_fo* p_f, particle_simd_fo* p_i,
+                      sim_data* sim) {
 
+    /* Note which end conditions are set as active.
+       Only these ones are checked */
     int active_tmax      = sim->endcond_active & endcond_tmax;
-    int active_wall      = sim->endcond_active & endcond_wall; 
+    int active_wall      = sim->endcond_active & endcond_wall;
     int active_emin      = sim->endcond_active & endcond_emin;
     int active_rholim    = sim->endcond_active & endcond_rhomax;
     int active_orbitlim  = sim->endcond_active & endcond_polmax;
     int active_cpumax    = sim->endcond_active & endcond_cpumax;
 
 #pragma omp simd
-    for(i = 0; i < NSIMD; i++) {
-	if(p_f->running[i]) {
+    for(int i = 0; i < NSIMD; i++) {
+        if(p_f->running[i]) {
 
-	    /* Max time */
-	    if(active_tmax) {
-		if(p_f->time[i] > sim->endcond_maxSimTime) {
-		    p_f->endcond[i] |= endcond_tmax;
-		    p_f->running[i] = 0;
-		}
-	    }
+            /* Check if the marker time exceeds simulation time */
+            if(active_tmax) {
+                if(p_f->time[i] > sim->endcond_maxSimTime) {
+                    p_f->endcond[i] |= endcond_tmax;
+                    p_f->running[i] = 0;
+                }
+            }
 
-	    /* Wall hit */
-	    if(active_wall) {
-		int tile = wall_hit_wall(p_i->r[i], p_i->phi[i], p_i->z[i],
-					 p_f->r[i], p_f->phi[i], p_f->z[i], &sim->wall_data);
-		if(tile > 0) {
-		    p_f->walltile[i] = tile;
-		    p_f->endcond[i] |= endcond_wall;
-		    p_f->running[i] = 0;
-		}
-	    }	
+            /* Check, using the wall collision module, whether marker hit wall
+             * during this time-step. Store the wall element ID if it did. */
+            if(active_wall) {
+                int tile = wall_hit_wall(
+                    p_i->r[i], p_i->phi[i], p_i->z[i],
+                    p_f->r[i], p_f->phi[i], p_f->z[i], &sim->wall_data);
+                if(tile > 0) {
+                    p_f->walltile[i] = tile;
+                    p_f->endcond[i] |= endcond_wall;
+                    p_f->running[i] = 0;
+                }
+            }
 
-	    /* Min energy */
-	    if(active_emin) {
-		real vnorm = math_normc(p_f->rdot[i], p_f->phidot[i] * p_f->r[i], p_f->zdot[i]);
-		real gamma = physlib_relfactorv_fo(vnorm);
-		real ekin = CONST_C2 * p_f->mass[i] * (gamma - 1);
+            /* Evaluate marker energy, and check if it is below the minimum
+             * energy limit or local thermal energy limit */
+            if(active_emin) {
+                real vnorm = math_normc(
+                    p_f->rdot[i], p_f->phidot[i] * p_f->r[i], p_f->zdot[i]);
+                real gamma = physlib_relfactorv_fo(vnorm);
+                real ekin = CONST_C2 * p_f->mass[i] * (gamma - 1);
+                real Te = plasma_eval_temp(p_f->rho[i], 0, &sim->plasma_data)
+                    * CONST_KB;
 
-		/* Fixed limit */
-		if(ekin < sim->endcond_minEkin) {
-		    p_f->endcond[i] |= endcond_emin;
-		    p_f->running[i] = 0;
-		}
+                if(ekin < sim->endcond_minEkin) {
+                    p_f->endcond[i] |= endcond_emin;
+                    p_f->running[i] = 0;
+                }
+                if(ekin < (sim->endcond_minEkinPerTe * Te)) {
+                    p_f->endcond[i] |= endcond_therm;
+                    p_f->running[i] = 0;
+                }
+            }
 
-		/* Background temperature */
-		real Te = plasma_eval_temp(p_f->rho[i], 0, &sim->plasma_data) *CONST_KB;
-	        if(ekin < (sim->endcond_minEkinPerTe * Te)) {
-		    p_f->endcond[i] |= endcond_therm;
-		    p_f->running[i] = 0;
-		}
-	    }
-	    
-	    /* Rho lims */
-	    if(active_rholim) {
-		if(p_f->rho[i] > sim->endcond_maxRho) {
-		    p_f->endcond[i] |= endcond_rhomax;
-		    p_f->running[i] = 0;
-		}
-		if(p_f->rho[i] < sim->endcond_minRho) {
-		    p_f->endcond[i] |= endcond_rhomin;
-		    p_f->running[i] = 0;
-		}
-	    }
+            /* Check if marker is not within the rho limits */
+            if(active_rholim) {
+                if(p_f->rho[i] > sim->endcond_maxRho) {
+                    p_f->endcond[i] |= endcond_rhomax;
+                    p_f->running[i] = 0;
+                }
+                if(p_f->rho[i] < sim->endcond_minRho) {
+                    p_f->endcond[i] |= endcond_rhomin;
+                    p_f->running[i] = 0;
+                }
+            }
 
-	    /* Orbit lims */
-	    if(active_orbitlim) {
-		if(fabs(p_f->phi[i]) > sim->endcond_maxTorOrb) {
-		    p_f->endcond[i] |= endcond_tormax;
-		    p_f->running[i] = 0;
-		}
-		if(fabs(p_f->pol[i]) > sim->endcond_maxPolOrb) {
-		    p_f->endcond[i] |= endcond_polmax;
-		    p_f->running[i] = 0;
-		}
-	    }
+            /* Check if marker exceeds toroidal or poloidal limits */
+            if(active_orbitlim) {
+                if(fabs(p_f->phi[i]) > sim->endcond_maxTorOrb) {
+                    p_f->endcond[i] |= endcond_tormax;
+                    p_f->running[i] = 0;
+                }
+                if(fabs(p_f->pol[i]) > sim->endcond_maxPolOrb) {
+                    p_f->endcond[i] |= endcond_polmax;
+                    p_f->running[i] = 0;
+                }
+            }
 
-	    /* CPU time lim */
-	    if(active_cpumax) {
-		if(p_f->cputime[i] > sim->endcond_maxCpuTime) {
-		    p_f->endcond[i] |= endcond_cpumax;
-		    p_f->running[i] = 0;
-		}
-	    }
-	}
+            /* Check if the time spent simulating this marker exceeds the
+             * given limit*/
+            if(active_cpumax) {
+                if(p_f->cputime[i] > sim->endcond_maxCpuTime) {
+                    p_f->endcond[i] |= endcond_cpumax;
+                    p_f->running[i] = 0;
+                }
+            }
+        }
     }
 }
 
-void endcond_check_gc(particle_simd_gc* p_f, particle_simd_gc* p_i, sim_data* sim) {
+/**
+ * @brief Check end conditions for GC markers
+ *
+ * The end conditions are checked for all markers within the SIMD marker struct.
+ *
+ * @param p_f pointer to SIMD struct storing marker states at the end of current
+ *        time-step
+ * @param p_i pointer to SIMD struct storing marker states at the beginning of
+ *        current time-step
+ *
+ * @todo Hybrid condition checks whether marker is over maximum rho limit. More
+ *       smarter check is required.
+ */
+void endcond_check_gc(particle_simd_gc* p_f, particle_simd_gc* p_i,
+                      sim_data* sim) {
     int i;
-    
+
     int active_tmax      = sim->endcond_active & endcond_tmax;
-    int active_wall      = sim->endcond_active & endcond_wall; 
+    int active_wall      = sim->endcond_active & endcond_wall;
     int active_emin      = sim->endcond_active & endcond_emin;
     int active_rholim    = sim->endcond_active & endcond_rhomax;
     int active_orbitlim  = sim->endcond_active & endcond_polmax;
     int active_cpumax    = sim->endcond_active & endcond_cpumax;
-    
+
     #pragma omp simd
     for(i = 0; i < NSIMD; i++) {
 
-	/* Max time */
+        /* Check if the marker time exceeds simulation time */
         if(active_tmax) {
-	    if(p_f->time[i] > sim->endcond_maxSimTime) {
-		p_f->endcond[i] |= endcond_tmax;
-		p_f->running[i] = 0;
-	    }
-	}
+            if(p_f->time[i] > sim->endcond_maxSimTime) {
+                p_f->endcond[i] |= endcond_tmax;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* Wall hit */
-	if(active_wall) {
-	    int tile = wall_hit_wall(p_i->r[i], p_i->phi[i], p_i->z[i],
-				     p_f->r[i], p_f->phi[i], p_f->z[i], &sim->wall_data);
-	    if(tile > 0) {
-		p_f->walltile[i] = tile;
-		p_f->endcond[i] |= endcond_wall;
-		p_f->running[i] = 0;
-	    }
-	}	
+        /* Check, using the wall collision module, whether marker hit wall
+         * during this time-step. Store the wall element ID if it did. */
+        if(active_wall) {
+            int tile = wall_hit_wall(p_i->r[i], p_i->phi[i], p_i->z[i],
+                                     p_f->r[i], p_f->phi[i], p_f->z[i],
+                                     &sim->wall_data);
+            if(tile > 0) {
+                p_f->walltile[i] = tile;
+                p_f->endcond[i] |= endcond_wall;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* Min energy */
-	if(active_emin) {
-	    real Bnorm = math_normc(p_f->B_r[i], p_f->B_phi[i], p_f->B_z[i]);
-	    real gamma = physlib_relfactorv_gc(p_f->mass[i], p_f->mu[i], p_f->vpar[i], Bnorm);
-	    real ekin = CONST_C2 * p_f->mass[i] * (gamma - 1);
+        /* Evaluate marker energy, and check if it is below the minimum
+         * energy limit or local thermal energy limit */
+        if(active_emin) {
+            real Bnorm = math_normc(p_f->B_r[i], p_f->B_phi[i], p_f->B_z[i]);
+            real gamma = physlib_relfactorv_gc(
+                p_f->mass[i], p_f->mu[i], p_f->vpar[i], Bnorm);
+            real ekin = CONST_C2 * p_f->mass[i] * (gamma - 1);
+            real Te = plasma_eval_temp(p_f->rho[i], 0, &sim->plasma_data)
+                * CONST_KB;
 
-	    /* Fixed limit */
-	    if(ekin < sim->endcond_minEkin) {
-		p_f->endcond[i] |= endcond_emin;
-		p_f->running[i] = 0;
-	    }
+            if(ekin < sim->endcond_minEkin) {
+                p_f->endcond[i] |= endcond_emin;
+                p_f->running[i] = 0;
+            }
+            if(ekin < (sim->endcond_minEkinPerTe * Te)) {
+                p_f->endcond[i] |= endcond_therm;
+                p_f->running[i] = 0;
+            }
+        }
 
-	    /* Background temperature */
-	    real Te = plasma_eval_temp(p_f->rho[i], 0, &sim->plasma_data) *CONST_KB;
-	    if(ekin < (sim->endcond_minEkinPerTe * Te)) {
-		p_f->endcond[i] |= endcond_therm;
-		p_f->running[i] = 0;
-	    }
-	}
-	    
-	/* Rho lims */
-	if(active_rholim) {
-	    if(p_f->rho[i] > sim->endcond_maxRho) {
-		p_f->endcond[i] |= endcond_rhomax;
-		p_f->running[i] = 0;
-	    }
-	    if(p_f->rho[i] < sim->endcond_minRho) {
-		p_f->endcond[i] |= endcond_rhomin;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check if marker is not within the rho limits */
+        if(active_rholim) {
+            if(p_f->rho[i] > sim->endcond_maxRho) {
+                p_f->endcond[i] |= endcond_rhomax;
+                p_f->running[i] = 0;
+            }
+            if(p_f->rho[i] < sim->endcond_minRho) {
+                p_f->endcond[i] |= endcond_rhomin;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* Orbit lims */
-	if(active_orbitlim) {
-	    if(fabs(p_f->phi[i]) > sim->endcond_maxTorOrb) {
-		p_f->endcond[i] |= endcond_tormax;
-		p_f->running[i] = 0;
-	    }
-	    if(fabs(p_f->pol[i]) > sim->endcond_maxPolOrb) {
-		p_f->endcond[i] |= endcond_polmax;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check if marker exceeds toroidal or poloidal limits */
+        if(active_orbitlim) {
+            if(fabs(p_f->phi[i]) > sim->endcond_maxTorOrb) {
+                p_f->endcond[i] |= endcond_tormax;
+                p_f->running[i] = 0;
+            }
+            if(fabs(p_f->pol[i]) > sim->endcond_maxPolOrb) {
+                p_f->endcond[i] |= endcond_polmax;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* CPU time lim */
-	if(active_cpumax) {
-	    if(p_f->cputime[i] > sim->endcond_maxCpuTime) {
-		p_f->endcond[i] |= endcond_cpumax;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check if the time spent simulating this marker exceeds the
+         * given limit*/
+        if(active_cpumax) {
+            if(p_f->cputime[i] > sim->endcond_maxCpuTime) {
+                p_f->endcond[i] |= endcond_cpumax;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* Hybrid mode */
-	if(sim->sim_mode == 3) {
-	    if(p_f->rho[i] > sim->endcond_maxRho) {
-	        p_f->endcond[i] |= endcond_hybrid;
-	        p_f->running[i] = 0;
-	    }
-	}
+        /* If hybrid mode is used, check whether this marker meets the hybrid
+         * condition. */
+        if(sim->sim_mode == 3) {
+            if(p_f->rho[i] > sim->endcond_maxRho) {
+                p_f->endcond[i] |= endcond_hybrid;
+                p_f->running[i] = 0;
+            }
+        }
     }
 }
 
-void endcond_check_ml(particle_simd_ml* p_f, particle_simd_ml* p_i, sim_data* sim) {
+/**
+ * @brief Check end conditions for ML markers
+ *
+ * The end conditions are checked for all markers within the SIMD marker struct.
+ *
+ * @param p_f pointer to SIMD struct storing marker states at the end of current
+ *        time-step
+ * @param p_i pointer to SIMD struct storing marker states at the beginning of
+ *        current time-step
+ */
+void endcond_check_ml(particle_simd_ml* p_f, particle_simd_ml* p_i,
+                      sim_data* sim) {
     int i;
 
     int active_tmax      = sim->endcond_active & endcond_tmax;
-    int active_wall      = sim->endcond_active & endcond_wall; 
+    int active_wall      = sim->endcond_active & endcond_wall;
     int active_rholim    = sim->endcond_active & endcond_rhomax;
     int active_orbitlim  = sim->endcond_active & endcond_polmax;
     int active_cpumax    = sim->endcond_active & endcond_cpumax;
@@ -205,55 +295,58 @@ void endcond_check_ml(particle_simd_ml* p_f, particle_simd_ml* p_i, sim_data* si
     #pragma omp simd
     for(i = 0; i < NSIMD; i++) {
 
-	/* Max time */
-	if(active_tmax) {
-	    if(p_f->time[i] > sim->endcond_maxSimTime) {
-		p_f->endcond[i] |= endcond_tmax;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check if the marker time exceeds simulation time */
+        if(active_tmax) {
+            if(p_f->time[i] > sim->endcond_maxSimTime) {
+                p_f->endcond[i] |= endcond_tmax;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* Wall hit */
-	if(active_wall) {
-	    int tile = wall_hit_wall(p_i->r[i], p_i->phi[i], p_i->z[i],
-				     p_f->r[i], p_f->phi[i], p_f->z[i], &sim->wall_data);
-	    if(tile > 0) {
-		p_f->walltile[i] = tile;
-		p_f->endcond[i] |= endcond_wall;
-		p_f->running[i] = 0;
-	    }
-	}	
-	    
-	/* Rho lims */
-	if(active_rholim) {
-	    if(p_f->rho[i] > sim->endcond_maxRho) {
-		p_f->endcond[i] |= endcond_rhomax;
-		p_f->running[i] = 0;
-	    }
-	    if(p_f->rho[i] < sim->endcond_minRho) {
-		p_f->endcond[i] |= endcond_rhomin;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check, using the wall collision module, whether marker hit wall
+         * during this time-step. Store the wall element ID if it did. */
+        if(active_wall) {
+            int tile = wall_hit_wall(p_i->r[i], p_i->phi[i], p_i->z[i],
+                                     p_f->r[i], p_f->phi[i], p_f->z[i],
+                                     &sim->wall_data);
+            if(tile > 0) {
+                p_f->walltile[i] = tile;
+                p_f->endcond[i] |= endcond_wall;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* Orbit lims */
-	if(active_orbitlim) {
-	    if(fabs(p_f->phi[i]) > sim->endcond_maxTorOrb) {
-		p_f->endcond[i] |= endcond_tormax;
-		p_f->running[i] = 0;
-	    }
-	    if(fabs(p_f->pol[i]) > sim->endcond_maxPolOrb) {
-		p_f->endcond[i] |= endcond_polmax;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check if marker is not within the rho limits */
+        if(active_rholim) {
+            if(p_f->rho[i] > sim->endcond_maxRho) {
+                p_f->endcond[i] |= endcond_rhomax;
+                p_f->running[i] = 0;
+            }
+            if(p_f->rho[i] < sim->endcond_minRho) {
+                p_f->endcond[i] |= endcond_rhomin;
+                p_f->running[i] = 0;
+            }
+        }
 
-	/* CPU time lim */
-	if(active_cpumax) {
-	    if(p_f->cputime[i] > sim->endcond_maxCpuTime) {
-		p_f->endcond[i] |= endcond_cpumax;
-		p_f->running[i] = 0;
-	    }
-	}
+        /* Check if marker exceeds toroidal or poloidal limits */
+        if(active_orbitlim) {
+            if(fabs(p_f->phi[i]) > sim->endcond_maxTorOrb) {
+                p_f->endcond[i] |= endcond_tormax;
+                p_f->running[i] = 0;
+            }
+            if(fabs(p_f->pol[i]) > sim->endcond_maxPolOrb) {
+                p_f->endcond[i] |= endcond_polmax;
+                p_f->running[i] = 0;
+            }
+        }
+
+        /* Check if the time spent simulating this marker exceeds the
+         * given limit*/
+        if(active_cpumax) {
+            if(p_f->cputime[i] > sim->endcond_maxCpuTime) {
+                p_f->endcond[i] |= endcond_cpumax;
+                p_f->running[i] = 0;
+            }
+        }
     }
 }
--- endcond.h
+++ endcond.h
@@ -1,6 +1,8 @@
 /**
  * @file endcond.h
  * @brief Header file for endcond.c
+ *
+ * Contains a list declaring all end conditions.
  */
 #ifndef ENDCOND_H
 #define ENDCOND_H
@@ -8,19 +10,32 @@
 #include "particle.h"
 #include "simulate.h"
 
+/**
+ * @brief Marker end condition masks
+ *
+ * These masks are used to mark specific end condition as being active.
+ */
 enum {
-    endcond_tmax   = 0b1,
-    endcond_emin   = 0b10,
-    endcond_therm  = 0b100,
-    endcond_wall   = 0b1000,
-    endcond_rhomin = 0b10000,
-    endcond_rhomax = 0b100000,
-    endcond_polmax = 0b1000000,
-    endcond_tormax = 0b10000000,
-    endcond_cpumax = 0b100000000,
-    endcond_hybrid = 0b1000000000
+    endcond_tmax   = 0b1, /**< Maximum simulation time */
+    endcond_emin   = 0b10, /**< Minimum energy */
+    endcond_therm  = 0b100, /**< Thermalized */
+    endcond_wall   = 0b1000, /**< Wall collision */
+    endcond_rhomin = 0b10000, /**< Minimum rho */
+    endcond_rhomax = 0b100000, /**< Maximum rho */
+    endcond_polmax = 0b1000000, /**< Poloidal limit */
+    endcond_tormax = 0b10000000, /**< Toroidal limit */
+    endcond_cpumax = 0b100000000, /**< Wall time exceeded */
+    endcond_hybrid = 0b1000000000 /**< Hybrid mode condition */
 };
 
+/**
+ * @brief Human-readable end conditions
+ *
+ * @deprecated Intended for human-readable output. However, these don't work as
+ * multiple end conditions can be active simultaneously.
+ *
+ * @todo See if this is used anywhere and remove this
+ */
 enum {
     endcond_id_rejected = -2,
     endcond_id_aborted = -1,
@@ -29,7 +44,6 @@ enum {
     endcond_id_wall = 3,
     endcond_id_therm = 4,
     endcond_id_cpumax = 5,
-	
     endcond_id_rhomin = 10,
     endcond_id_rhomax = 11,
     endcond_id_polmax = 12,
@@ -37,9 +51,12 @@ enum {
 };
 
 #pragma omp declare target
-void endcond_check_gc(particle_simd_gc* p_f, particle_simd_gc* p_i, sim_data* sim);
-void endcond_check_fo(particle_simd_fo* p_f, particle_simd_fo* p_i, sim_data* sim);
-void endcond_check_ml(particle_simd_ml* p_f, particle_simd_ml* p_i, sim_data* sim);
+void endcond_check_gc(particle_simd_gc* p_f, particle_simd_gc* p_i,
+                      sim_data* sim);
+void endcond_check_fo(particle_simd_fo* p_f, particle_simd_fo* p_i,
+                      sim_data* sim);
+void endcond_check_ml(particle_simd_ml* p_f, particle_simd_ml* p_i,
+                      sim_data* sim);
 #pragma omp end declare target
 
 #endif
