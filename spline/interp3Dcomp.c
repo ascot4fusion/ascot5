41515c66335cfc6cf1ace3bcb56d550543a54bc4
Joona Kontula <joona.kontula@aalto.fi>
2018-11-14 14:35:18 +0200

    3D splines correctly wrap phi to spline domain

--- spline/interp3Dcomp.c
+++ spline/interp3Dcomp.c
@@ -176,9 +176,10 @@ int interp3Dcomp_init(interp3D_data* str, real* f, int n_r, int n_phi, int n_z,
  * @param z z-coordinate
  */
 integer interp3Dcomp_eval_B(real* B, interp3D_data* str, real r, real phi, real z) {
-    /** Make sure phi is in interval [0,2pi) */
-    phi = fmod(phi,CONST_2PI);
-    if(phi < 0){phi = CONST_2PI + phi;}
+    /** Make sure phi is in interval [phi_min, phi_max + phi_grid) */
+    phi_range = (str->phi_max + str->phi_grid - str->phi_min);
+    phi = fmod(phi - str->phi_min, phi_range) + str->phi_min;
+    if(phi < 0){phi = phi_range + phi;}
 
     int i_r = (r-str->r_min)/str->r_grid;     /**< index for r variable */
     real dr = (r-(str->r_min+i_r*str->r_grid))/str->r_grid; /**< Normalized r coordinate in
@@ -299,9 +300,10 @@ integer interp3Dcomp_eval_B(real* B, interp3D_data* str, real r, real phi, real 
  * @param z z-coordinate
  */
 integer interp3Dcomp_eval_dB(real* B_dB, interp3D_data* str, real r, real phi, real z) {
-    /** Make sure phi is in interval [0,2pi) */
-    phi = fmod(phi,CONST_2PI);
-    if(phi < 0){phi = CONST_2PI + phi;}
+    /** Make sure phi is in interval [phi_min, phi_max + phi_grid) */
+    phi_range = (str->phi_max + str->phi_grid - str->phi_min);
+    phi = fmod(phi - str->phi_min, phi_range) + str->phi_min;
+    if(phi < 0){phi = phi_range + phi;}
 
     int i_r = (r-str->r_min)/str->r_grid;       /**< index for r variable */
     real dr = (r-(str->r_min+i_r*str->r_grid))/str->r_grid; /**< Normalized r coordinate in
--- spline/interp3Detoc.c
+++ spline/interp3Detoc.c
@@ -198,10 +198,10 @@ void interp3Detoc_init(interp3D_data* str, real* f, int n_r, int n_phi, int n_z,
  * @param z z-coordinate
  */
 void interp3Detoc_eval_B(real* B, interp3D_data* str, real r, real phi, real z) {
-
-    /** Make sure phi is in interval [0,2pi) */
-    phi = fmod(phi,CONST_2PI);
-    if(phi < 0){phi = CONST_2PI - phi;}
+    /** Make sure phi is in interval [phi_min, phi_max + phi_grid) */
+    phi_range = (str->phi_max + str->phi_grid - str->phi_min);
+    phi = fmod(phi - str->phi_min, phi_range) + str->phi_min;
+    if(phi < 0){phi = phi_range + phi;}
 
     int i_r = (r-str->r_min)/str->r_grid;     /**< index for r variable */
     real dr = (r-(str->r_min+i_r*str->r_grid))/str->r_grid; /**< Normalized r coordinate in
@@ -308,9 +308,10 @@ void interp3Detoc_eval_B(real* B, interp3D_data* str, real r, real phi, real z) 
  * @param z z-coordinate
  */
 void interp3Detoc_eval_dB(real* B_dB, interp3D_data* str, real r, real phi, real z) {
-    /** Make sure phi is in interval [0,2pi) */
-    phi = fmod(phi,CONST_2PI);
-    if(phi < 0){phi = CONST_2PI - phi;}
+    /** Make sure phi is in interval [phi_min, phi_max + phi_grid) */
+    phi_range = (str->phi_max + str->phi_grid - str->phi_min);
+    phi = fmod(phi - str->phi_min, phi_range) + str->phi_min;
+    if(phi < 0){phi = phi_range + phi;}
 
     int i_r = (r-str->r_min)/str->r_grid;       /**< index for r variable */
     real dr = (r-(str->r_min+i_r*str->r_grid))/str->r_grid; /**< Normalized r coordinate in
