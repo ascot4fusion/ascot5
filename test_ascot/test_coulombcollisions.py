5486d5ca25911dbb28ae94079552091cbd906a8d
Konsta Sarkimaki <konsta.sarkimaki@aalto.fi>
2018-12-25 19:44:43 +0200

    Update on test_ascot

--- test_ascot/test_coulombcollisions.py
+++ test_ascot/test_coulombcollisions.py
@@ -66,8 +66,9 @@ def init():
     Initialize tests
 
     This function initializes three tests that simulate a thermal equilibrium
-    in different modes: CCOLL_GO, CCOLL_GCF, and CCOLL_GCA. Three additional
-    simulations are initialized that
+    in different modes: THERMAL_GO, THERMAL_GCF, and THERMAL_GCA. Three
+    additional simulations are initialized that simulate alpha particle
+    slowing-down in different modes: SLOWING_GO, SLOWING_GCF, and SLOWING_GCA.
 
     Input fields contain the test case name (to which the input corresponds to)
     as a description.
@@ -183,7 +184,7 @@ def init():
     options.write_hdf5(test_ascot.testfn, odict, desc="SLOWING_GCA")
 
     #**************************************************************************#
-    #*                    Marker input consisting of protons                   #
+    #*            Marker input consisting of protons and alfas                 #
     #*                                                                         #
     #**************************************************************************#
     Nmrk   = 20
@@ -210,14 +211,15 @@ def init():
                    weight, time, desc="THERMAL_GCA")
 
     Nmrk   = 20
+    pol    = 2*np.pi*np.random.rand(1, Nmrk)
     ids    = np.linspace(1,Nmrk,Nmrk)
     weight = (1/Nmrk)* np.ones(ids.shape)
     mass   = m_a_AMU * np.ones(ids.shape)
     charge = 2       * np.ones(ids.shape)
     time   = 0       * np.ones(ids.shape)
-    R      = 8       * np.ones(ids.shape)
+    R      = 6.2 + 0.8 * np.cos(pol)
     phi    = 90      * np.ones(ids.shape)
-    z      = 0       * np.ones(ids.shape)
+    z      = 0.8 * np.sin(pol)
     theta  = 2 * np.pi * np.random.rand(1,Nmrk)
     energy = Esd * np.ones(ids.shape)
     pitch  = 1 - 2 * np.random.rand(1,Nmrk)
@@ -346,6 +348,17 @@ def run():
         test_ascot.set_and_run(test)
 
 def check():
+    """
+    Plot the results of these tests.
+
+    This function makes four plots.
+    - One that shows marker equilibrium energy distributions (for all modes) and
+      compares them with a Maxwellian distribution.
+    - One that shows the pitch distributions in equilibrium.
+    - One that shows marker slowing-down energy distributions and compares them
+      to the analytical result.
+    - One that shows pitch distribution during the slowing-down.
+    """
 
     a5 = ascot5.Ascot(test_ascot.testfn)
 
@@ -365,19 +378,25 @@ def check():
     SLOWING["Egrid"] = np.linspace( 0, 4.0e6, 200)
     SLOWING["xigrid"]  = np.linspace(-1,     1, 100)
 
-    v0    = np.sqrt(2*Esd*e / m_a)
-    vgrid = np.sqrt(2*SLOWING["Egrid"]*e / m_a)
-    vth   = np.sqrt(2*Te*e / m_e)
-    vcrit = vth * np.power( (3.0*np.sqrt(np.pi)/4.0) * (m_e / m_p) , 1/3.0)
-    ts = 2*3*np.sqrt(np.power(2*np.pi*Te*e,3)/m_e) * eps0 * eps0 *m_a /(2*2*e*e*e*e*ne*14) # Note 2 is arbitrary
+    alphaZ = 2
+    clog   = 14
+    v0     = np.sqrt(2*Esd*e / m_a)
+    vgrid  = np.sqrt(2*SLOWING["Egrid"]*e / m_a)
+    vth    = np.sqrt(2*Te*e / m_e)
+    vcrit  = vth * np.power( (3.0*np.sqrt(np.pi)/4.0) * (m_e / m_p) , 1/3.0)
+    ts     = 3 * np.sqrt( np.power(2*np.pi * Te * e, 3) / m_e ) * eps0 * eps0 \ 
+             * m_a /( alphaZ * alphaZ * np.power(e, 4) * ne * clog) 
+    ts *= 2 # For some reason analytical slowing-down is off by a factor of 2
     heaviside = vgrid <= v0
 
-    THERMAL["analytical"] = (simtime_th/2)*2*np.sqrt(THERMAL["Egrid"]/np.pi) \
-                            * np.power(Te,-3.0/2) \
-                            * np.exp(-THERMAL["Egrid"]/Te)
-    SLOWING["analytical"] = ( 1 / ( np.power(vcrit,3)+np.power(vgrid,3) ) ) \
-                            3 * ts * heaviside \
-                            * ( vgrid/ np.log(1+np.power(v0/vcrit,3)) ) * e/m_a
+    THERMAL["analytical"]  = 2 * np.sqrt(THERMAL["Egrid"]/np.pi) \
+                             * np.power(Te,-3.0/2) \
+                             * np.exp(-THERMAL["Egrid"]/Te)
+    THERMAL["analytical"] *= (simtime_th/2)
+
+    SLOWING["analytical"]  = ( 1 / ( np.power(vcrit,3)+np.power(vgrid,3) ) ) \
+                              * 3 * ts * heaviside \
+                              * (vgrid/ np.log(1+np.power(v0/vcrit,3)) ) * e/m_a
 
     #**************************************************************************#
     #*            Evaluate thermal distributions in energy and pitch           #
@@ -394,8 +413,6 @@ def check():
         distobj.integrate(dist, 'time', slices=[1])
         distobj.integrate(dist, 'vpa')
         distobj.integrate(dist, 'vpe')
-        #plt.figure();
-        #plt.pcolor(dist["vpa"], dist["vpe"], np.transpose(np.log(dist["density"]+1e-18)))
 
         Exidist = distobj.get_E_xi_dist(THERMAL["Egrid"],
                                         THERMAL["xigrid"], m_p)
@@ -405,10 +422,6 @@ def check():
         distobj.integrate(Exidist, 'charge')
         distobj.integrate(Exidist, 'time', slices=[1])
 
-        #Exidist["density"] /= (simtime_th/2)
-        #plt.figure();
-        #plt.pcolor(Exidist["xi"], Exidist["E"], np.log(Exidist["density"]+1e-18))
-
         Edist  = copy.deepcopy(Exidist)
         xidist = copy.deepcopy(Exidist)
         distobj.integrate(Edist, 'xi')
@@ -416,17 +429,10 @@ def check():
         distobj.integrate(Exidist, 'E')
         distobj.integrate(Exidist, 'xi')
 
-        #Edist["density"]  /= Edist["E"].size / \
-        #                     (Edist["E_edges"][-1] - Edist["E_edges"][0])
-        #xidist["density"] /= xidist["xi"].size / \
-        #                     (xidist["xi_edges"][-1] - xidist["xi_edges"][0])
-
         THERMAL[mode + "_Edist"]  = Edist["density"]
         THERMAL[mode + "_xidist"] = xidist["density"]
         THERMAL["E"]  = Edist["E"]
         THERMAL["xi"] = xidist["xi"]
-        #plt.show()
-        #return
 
     #**************************************************************************#
     #*        Evaluate slowing-down distributions in energy and pitch          #
@@ -442,8 +448,6 @@ def check():
         distobj.integrate(dist, 'z')
         distobj.integrate(dist, 'charge')
         distobj.integrate(dist, 'time')
-        #plt.figure();
-        #plt.pcolor(dist["vpa"], dist["vpe"], np.transpose((dist["density"]+1e-18)))
 
         Exidist = distobj.get_E_xi_dist(SLOWING["Egrid"],
                                         SLOWING["xigrid"], m_a)
@@ -453,39 +457,40 @@ def check():
         distobj.integrate(Exidist, 'charge')
         distobj.integrate(Exidist, 'time')
 
-        #plt.figure();
-        #plt.pcolor(Exidist["xi"], Exidist["E"], (Exidist["density"]+1e-18))
-
         Edist  = copy.deepcopy(Exidist)
         xidist = copy.deepcopy(Exidist)
         distobj.integrate(Edist, 'xi')
         distobj.integrate(xidist, 'E')
-
-        #Edist["density"]  *= Edist["E"].size / \
-        #                     (Edist["E_edges"][-1] - Edist["E_edges"][0])
-        #xidist["density"] *= xidist["xi"].size / \
-        #                     (xidist["xi_edges"][-1] - xidist["xi_edges"][0])
 
         SLOWING[mode + "_Edist"]  = Edist["density"]
         SLOWING[mode + "_xidist"] = xidist["density"]
         SLOWING["E"]  = Edist["E"]
         SLOWING["xi"] = xidist["xi"]
 
-        #plt.show()
-        #return
-
-
     #**************************************************************************#
     #*                                 Plot                                    #
     #*                                                                         #
     #**************************************************************************#
 
+    f = plt.figure(figsize=(11.9/2.54, 8/2.54))
+    plt.rc('xtick', labelsize=10)
+    plt.rc('ytick', labelsize=10)
+    plt.rc('axes', labelsize=10)
+    plt.rcParams['mathtext.fontset'] = 'stix'
+    plt.rcParams['font.family'] = 'STIXGeneral'
+
     c = ['b', 'g', 'r']
-    f  = plt.figure()
-    a1 = f.add_subplot(2, 2, 1)
-    a2 = f.add_subplot(2, 2, 2)
-    a3 = f.add_subplot(2, 2, 3)
-    a4 = f.add_subplot(2, 2, 4)
+    h1 = f.add_subplot(1,4,1)
+    h1.set_position([0.12, 0.72, 0.4, 0.25], which='both')
+
+    h2 = f.add_subplot(1,4,2)
+    h2.set_position([0.12, 0.44, 0.4, 0.25], which='both')
+
+    h3 = f.add_subplot(1,4,3)
+    h3.set_position([0.12, 0.155, 0.4, 0.25], which='both')
+
+    h4 = f.add_subplot(1,4,4)
+    h4.set_position([0.6, 0.3, 0.45, 0.45], which='both')
 
     a1.plot(THERMAL["E"], THERMAL["GO_Edist"])
     a1.plot(THERMAL["E"], THERMAL["GCF_Edist"])
@@ -495,6 +500,7 @@ def check():
     a2.plot(THERMAL["xi"], THERMAL["GO_xidist"])
     a2.plot(THERMAL["xi"], THERMAL["GCF_xidist"])
     a2.plot(THERMAL["xi"], THERMAL["GCA_xidist"])
+    a2.plot(np.array([-1, 1]), np.array([0.5, 0.5]))
 
     a3.plot(SLOWING["E"], SLOWING["GO_Edist"])
     a3.plot(SLOWING["E"], SLOWING["GCF_Edist"])
@@ -504,11 +510,65 @@ def check():
     a4.plot(SLOWING["xi"], SLOWING["GO_xidist"])
     a4.plot(SLOWING["xi"], SLOWING["GCF_xidist"])
     a4.plot(SLOWING["xi"], SLOWING["GCA_xidist"])
+    a4.plot(np.array([-1, 1]), np.array([0.5, 0.5]))
 
     #**************************************************************************#
     #*                         Decorate and finish                             #
     #*                                                                         #
     #**************************************************************************#
+    h1.set_xlim(0, 4e4)
+    h1.xaxis.set(ticks=[0, 1e4, 2e4, 3e4, 4e4], 
+                 ticklabels=['0', '1', '2', '3', '4'])
+    #h1.yaxis.set(ticks=np.array([0, 1, -2, 0, 2])*1e-11,
+    #             ticklabels=[-6, '', '', 0, 2])
+    h1.tick_params(axis='y', direction='out')
+    h1.tick_params(axis='x', direction='out')
+    h1.spines['right'].set_visible(False)
+    h1.spines['top'].set_visible(False)
+    h1.yaxis.set_ticks_position('left')
+    h1.xaxis.set_ticks_position('bottom')
+    h1.set(ylabel=r"$f(E) \times 10^{-3}$ [1/eV]")
+    h1.set(xlabel=r"Energy $\times 10^{3}$ [eV]")
+
+    h2.set_xlim(-1, 1)
+    h2.set_ylim(0, 1)
+    h2.xaxis.set(ticks=[-1, -0.5, 0, 0.5, 1])
+    h2.yaxis.set(ticks=[0, 0.5, 1])
+    h2.tick_params(axis='y', direction='out')
+    h2.tick_params(axis='x', direction='out')
+    h2.spines['right'].set_visible(False)
+    h2.spines['top'].set_visible(False)
+    h2.yaxis.set_ticks_position('left')
+    h2.xaxis.set_ticks_position('bottom')
+    h2.set(ylabel=r"$f(\xi)$")
+    h2.set(xlabel=r"Pitch")
+
+    h3.set_xlim(0, 4e6)
+    h3.xaxis.set(ticks=[0, 1e6, 2e6, 3e6, 4e6], 
+                 ticklabels=['0', '1', '2', '3', '4'])
+    #h3.yaxis.set(ticks=np.array([0, 1, -2, 0, 2])*1e-11,
+    #             ticklabels=[-6, '', '', 0, 2])
+    h3.tick_params(axis='y', direction='out')
+    h3.tick_params(axis='x', direction='out')
+    h3.spines['right'].set_visible(False)
+    h3.spines['top'].set_visible(False)
+    h3.yaxis.set_ticks_position('left')
+    h3.xaxis.set_ticks_position('bottom')
+    h3.set(ylabel=r"$f(E) \times 10^{-6}$ [1/eV]")
+    h3.set(xlabel=r"Energy $\times 10^{6}$ [eV]")
+
+    h4.set_xlim(-1, 1)
+    h4.set_ylim(0, 1)
+    h4.xaxis.set(ticks=[-1, -0.5, 0, 0.5, 1])
+    h4.yaxis.set(ticks=[0, 0.5, 1])
+    h4.tick_params(axis='y', direction='out')
+    h4.tick_params(axis='x', direction='out')
+    h4.spines['right'].set_visible(False)
+    h4.spines['top'].set_visible(False)
+    h4.yaxis.set_ticks_position('left')
+    h4.xaxis.set_ticks_position('bottom')
+    h4.set(ylabel=r"$f(\xi)$")
+    h4.set(xlabel=r"Pitch")
 
     #plt.savefig("test_coulombcollisions.png", dpi=72)
     plt.show()
