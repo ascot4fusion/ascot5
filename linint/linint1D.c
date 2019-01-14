445269642358831e7b9e293e141b440e4642af06
jfrivero@us.es <jriveror@r000u06l01.marconi.cineca.it>
2019-01-14 14:08:12 +0100

    linint1D.c updated to develop

--- linint/linint1D.c
+++ linint/linint1D.c
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#include <stdlib.h>
-
-/**
- * @brief Returns the interpolated y-value.
- * Saturates to y0 or y1 if x outside interval [x0, x1].
- */
-int linint1D_eval(real* val, real x0, real y0, real x1, real y1, real x){
-  real t;
-  int err = 0;
-
-  t =  (x-x0);
-  t /= (x1-x0);
-
-  *val = y0 + t*(y1-y0);
-  return err;
-=======
 /**
  * @file linint1D.c
  * @brief Linear interpolation
@@ -104,5 +87,4 @@ integer linint1D_eval(real* val, linint1D_data* str, real r) {
  */
 void linint1D_free(linint1D_data* str) {
     free(str->c);
->>>>>>> develop
 }
