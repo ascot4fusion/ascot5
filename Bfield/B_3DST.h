62bec969c90c770dd8f564c194cbaacf1ec959ca
jfrivero@us.es <jriveror@r000u11l06.marconi.cineca.it>
2019-01-24 19:21:33 +0100

    update of Bfield/B_3DST.h

--- Bfield/B_3DST.h
+++ Bfield/B_3DST.h
@@ -1,21 +1,19 @@
 /**
- * @file B_3DS_T.h
- * @brief Header file for B_3DS_T.c
+ * @file B_3DST.h
+ * @brief Header file for B_3DST.c
  */
-#ifndef B_3DS_H
-#define B_3DS_H
-
-#define N_MAX_TIME_SLICE 10
+#ifndef B_3DST_H
+#define B_3DST_H
 
 #include "../ascot5.h"
 #include "../spline/interp2D.h" /* for 2D interpolation routines */
 #include "../spline/interp3D.h" /* for 3D interpolation routines */
-#include "B_3DS.h"
+//#include "../spline/interp4D.h"
+
 /**
  * @brief 3D magnetic field parameters on the host
  */
 typedef struct {
-    int n_time;
     real time[N_MAX_TIME_SLICE];
 
     int psigrid_n_r;            /**< number of r grid points in psi data */
@@ -31,6 +29,7 @@ typedef struct {
     int Bgrid_n_r;              /**< number of r grid points in B data */
     int Bgrid_n_z;              /**< number of z grid points in B data */
     int n_phi;                  /**< number of phi grid points in B data */
+    int n_time;                 /**< number of time grid points in B data */
     real Bgrid_r_min;           /**< minimum r coordinate in the grid in B data */
     real Bgrid_r_max;           /**< maximum r coordinate in the grid in B data */
     real Bgrid_r_grid;          /**< r grid interval (r_max-r_min)/(n_r-1) in B data */
@@ -40,44 +39,55 @@ typedef struct {
     real phi_min;               /**< minimum phi coordinate in the grid in B data */
     real phi_max;               /**< maximum phi coordinate in the grid in B data */
     real phi_grid;              /**< phi grid interval 2pi/(n_phi-1) in B data */
-    
-    real psi0[N_MAX_TIME_SLICE];                  /**< sqrt(psi) value at magnetic axis */
-    real psi1[N_MAX_TIME_SLICE];                  /**< sqrt(psi) value at separatrix */
-    real axis_r[N_MAX_TIME_SLICE];                /**< r coordinate of magnetic axis */
-    real axis_z[N_MAX_TIME_SLICE];                /**< z coordinate of magnetic axis */
+    real time_min;              /**< minimum time coordinate in the grid in B data */
+    real time_max;              /**< maximum time coordinate in the grid in B data */
+    real time_grid;             /**< time grid interval (time_max-time_min)/(n_time-1) in B data */
+
+    real psi0;                  /**< sqrt(psi) value at magnetic axis */
+    real psi1;                  /**< sqrt(psi) value at separatrix */
+    real axis_r;                /**< r coordinate of magnetic axis */
+    real axis_z;                /**< z coordinate of magnetic axis */
     int offload_array_length;   /**< number of elements in offload_array */
-} B_3DS_T_offload_data;
+} B_3DST_offload_data;
 
 /**
  * @brief 3D magnetic field parameters on the target
  */
 typedef struct {
-    int n_time;                 /**< number of time slices in B & psi data */
-    real time[N_MAX_TIME_SLICE];/**< values of time for each time slice */
-    B_3DS_data Bslice[N_MAX_TIME_SLICE];/**< B3DS grids for each time slice */
-} B_3DS_T_data;
+    real psi0;           /**< Poloidal flux value at magnetic axis [v*s*m^-1] */
+    real psi1;           /**< Poloidal flux value at separatrix [V*s*m^-1]    */
+    real axis_r;         /**< R coordinate of magnetic axis [m]               */
+    real axis_z;         /**< z coordinate of magnetic axis [m]               */
+    interp2D_data psi;   /**< 2D psi interpolation data struct                */
+    interp4D_data B_r;   /**< 3D B_r interpolation data struct                */
+    interp4D_data B_phi; /**< 3D B_phi interpolation data struct              */
+    interp4D_data B_z;   /**< 3D B_z interpolation data struct                */
+} B_3DST_data;
 
-void B_3DS_T_init_offload(B_3DS_T_offload_data* offload_data, real** offload_array);
-void B_3DS_T_free_offload(B_3DS_T_offload_data* offload_data, real** offload_array);
+void B_3DST_init_offload(B_3DST_offload_data* offload_data, real** offload_array);
+void B_3DST_free_offload(B_3DST_offload_data* offload_data, real** offload_array);
 
 #pragma omp declare target
-int B_3DS_T_init(B_3DS_T_data* Bdata, B_3DS_T_offload_data* offload_data,
-               real* offload_array);
+void B_3DST_init(B_3DST_data* Bdata, B_3DST_offload_data* offload_data,
+                real* offload_array);
 #pragma omp declare simd uniform(Bdata)
-a5err B_3DS_T_eval_psi(real psi[], real r, real phi, real z, real time, B_3DS_T_data* Bdata);
+a5err B_3DST_eval_psi(real psi[1], real r, real phi, real z, B_3DST_data* Bdata);
 #pragma omp declare simd uniform(Bdata)
-a5err B_3DS_T_eval_psi_dpsi(real psi_dpsi[], real r, real phi, real z, real time, B_3DS_T_data* Bdata);
+a5err B_3DST_eval_psi_dpsi(real psi_dpsi[4], real r, real phi, real z,
+			   B_3DST_data* Bdata); //this is not used really
 #pragma omp declare simd uniform(Bdata)
-a5err B_3DS_T_eval_rho(real rho[], real psi, real time, B_3DS_T_data* Bdata);
+a5err B_3DST_eval_rho(real rho[1], real psi, B_3DST_data* Bdata);
 #pragma omp declare simd uniform(Bdata)
-a5err B_3DS_T_eval_rho_drho(real rho_drho[], real r, real phi, real z, real time, B_3DS_T_data* Bdata);
+a5err B_3DST_eval_rho_drho(real rho_drho[4], real r, real phi, real z,
+                          B_3DST_data* Bdata);
 #pragma omp declare simd uniform(Bdata)
-a5err B_3DS_T_eval_B(real B[], real r, real phi, real z, real time, B_3DS_T_data* Bdata);
+a5err B_3DST_eval_B(real B[3], real r, real phi, real z, real time, B_3DST_data* Bdata);
 #pragma omp declare simd uniform(Bdata)
-a5err B_3DS_T_eval_B_dB(real B_dB[], real r, real phi, real z, real time, B_3DS_T_data* Bdata);
+a5err B_3DST_eval_B_dB(real B_dB[12], real r, real phi, real z, real time,
+                      B_3DST_data* Bdata);
 #pragma omp declare simd uniform(Bdata)
-real B_3DS_T_get_axis_r(real time, B_3DS_T_data* Bdata);
+real B_3DST_get_axis_r(B_3DST_data* Bdata);
 #pragma omp declare simd uniform(Bdata)
-real B_3DS_T_get_axis_z(real time, B_3DS_T_data* Bdata);
-#pragma omp end declare target   
+real B_3DST_get_axis_z(B_3DST_data* Bdata);
+#pragma omp end declare target
 #endif
