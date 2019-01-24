03692e4a8753505e9102459080eee35c5eaeb73a
sarkimk1 <konsta.sarkimaki@aalto.fi>
2019-01-24 17:02:01 +0200

    Python interface works again

--- ascotpy.c
+++ ascotpy.c
@@ -1,9 +1,14 @@
 /**
  * @file ascotpy.c
- * @brief Python interface
+ * @brief C side of the interactive Python interface.
+ *
+ * Functions in this file can be called from Python directly via ascotpy
+ * module.
  */
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <hdf5.h>
 
 #include "ascot5.h"
 #include "simulate.h"
@@ -11,135 +16,129 @@
 #include "E_field.h"
 #include "plasma.h"
 #include "wall.h"
-#include "offload.h"
+#include "neutral.h"
 
+#include "hdf5_interface.h"
 #include "hdf5io/hdf5_helpers.h"
-#include "hdf5io/hdf5_input.h"
 #include "hdf5io/hdf5_bfield.h"
 #include "hdf5io/hdf5_efield.h"
 #include "hdf5io/hdf5_plasma.h"
 #include "hdf5io/hdf5_wall.h"
 
+/** Simulation offload struct for holding offload data structs. */
 static sim_offload_data sim_offload;
+
+/** Simulation struct for holding data structs. */
 static sim_data sim;
 
-static real* B_offload_array;
-static real* E_offload_array;
-static real* plasma_offload_array;
-static real* wall_offload_array;
+static real* Bdata;      /**< Magnetic field data (i.e. offload array) */
+//static real* Edata;      /**< Electric field data (i.e. offload array) */
+//static real* plasmadata; /**< Plasma data (i.e. offload array)         */
+//static real* walldata;   /**< Wall data (i.e. offload array)           */
 
-
-int ascotpy_init_bfield(char* fn) {
+/**
+ * @brief Initialize input data.
+ *
+ * This is similar to what is found in hdf5_interface, except we want to control
+ * what data fields are initialized. It is user's responsibility not to
+ * initialize same data twice.
+ *
+ * @param fn name of the HDF5 file.
+ * @param bfield flag for initializing magnetic field.
+ * @param efield flag for initializing electric field.
+ * @param plasma flag for initializing plasma data.
+ * @param wall flag for initializing wall data.
+ * @param neutral flag for initializing neutral data.
+ *
+ * @return zero if initialization succeeded.
+ */
+int ascotpy_init(char* fn, int bfield, int efield, int plasma, int wall,
+                 int neutral) {
     hdf5_init();
     hid_t f = hdf5_open(fn);
-    int err = hdf5_bfield_init_offload(f, &sim_offload.B_offload_data, &B_offload_array);
-    
-    B_field_init(&sim.B_data, &sim_offload.B_offload_data, B_offload_array);
-    B_field_free_offload(&sim_offload.B_offload_data, &B_offload_array);
-    
-    err = hdf5_close(f);
-    return err;
-}
+    if(f < 0) {
+        /* Failed to open file. */
+        return 1;
+    }
 
-int ascotpy_free_bfield() {
+    char qid[11];
 
+    /* Initialize magnetic field data if requested. */
+    if(bfield) {
+        if( hdf5_find_group(f, "/bfield/") ) {
+            /* Data does not exist. */
+            return 1;
+        }
+        if( hdf5_get_active_qid(f, "/bfield/", qid) ) {
+            /* No active qid found. */
+            return 1;
+        }
+        if( hdf5_bfield_init_offload(f, &sim_offload.B_offload_data, &Bdata,
+                                     qid) ) {
+            /* Initialization failed. */
+            return 1;
+        }
+        B_field_init(&sim.B_data, &sim_offload.B_offload_data, Bdata);
+    }
+
+    if(hdf5_close(f)) {
+        return 1;
+    }
     return 0;
 }
 
-int ascotpy_bfield_eval_B(int Neval, real* R, real* phi, real* z, real* BR, real* Bphi, real* Bz) {
+/**
+ * @brief Free input data.
+ *
+ * @param bfield flag for initializing magnetic field.
+ * @param efield flag for initializing electric field.
+ * @param plasma flag for initializing plasma data.
+ * @param wall flag for initializing wall data.
+ * @param neutral flag for initializing neutral data.
+ */
+int ascotpy_free(int bfield, int efield, int plasma, int wall, int neutral) {
+    if(bfield) {
+        free(Bdata);
+    }
+    return 0;
+}
+
+/**
+ * @brief Evaluate magnetic field vector and derivatives at given coordinates.
+ *
+ * @param Neval number of evaluation points.
+ * @param R R coordinates of the evaluation points [m].
+ * @param phi phi coordinates of the evaluation points [rad].
+ * @param z z coordinates of the evaluation points [m].
+ * @param BR
+ * @param Bphi
+ * @param Bz
+ * @param BR_dR
+ * @param BR_dphi
+ * @param BR_dz
+ * @param Bphi_dR
+ * @param Bphi_dphi
+ * @param Bphi_dz
+ * @param Bz_dR
+ * @param Bz_dphi
+ * @param Bz_dz
+ *
+ * @return zero if evaluation succeeded.
+ */
+int ascotpy_bfield_eval_B_dB(int Neval, real* R, real* phi, real* z,
+                             real* BR, real* Bphi, real* Bz,
+                             real* BR_dR, real* BR_dphi, real* BR_dz,
+                             real* Bphi_dR, real* Bphi_dphi, real* Bphi_dz,
+                             real* Bz_dR, real* Bz_dphi, real* Bz_dz) {
     real B[3];
 
     for(int k = 0; k < Neval; k++) {
-        B_field_eval_B(B, R[k], phi[k], z[k], &sim.B_data);
-	BR[k]   = B[0];
-	Bphi[k] = B[1];
-	Bz[k]   = B[2];
+        if( B_field_eval_B(B, R[k], phi[k], z[k], &sim.B_data) ) {
+            return 1;
+        }
+        BR[k]   = B[0];
+        Bphi[k] = B[1];
+        Bz[k]   = B[2];
     }
-    
-    return 0;
-}
-
-int ascotpy_bfield_eval_psi(int Neval, real* R, real* phi, real* z, real* psi) {
-    real psi_val;
-
-    for(int k = 0; k < Neval; k++) {
-    B_field_eval_psi(&psi_val, R[k], phi[k], z[k], &sim.B_data);
-	psi[k] = psi_val;
-    }
-    
-    return 0;
-}
-
-int ascotpy_bfield_eval_rho(int Neval, real* R, real* phi, real* z, real* rho) {
-    real psi_val;
-    real rho_val;
-    
-    for(int k = 0; k < Neval; k++) {
-    B_field_eval_psi(&psi_val, R[k], phi[k], z[k], &sim.B_data);    
-    B_field_eval_rho(&rho_val, psi_val, &sim.B_data);
-    rho[k] = rho_val;
-    }
-    
-    return 0;
-}
-
-int ascotpy_bfield_eval_axis(int Neval, real* phi, real* R, real* z) {
-
-    for(int k = 0; k < Neval; k++) {
-        R[k] = B_field_get_axis_r(&sim.B_data, phi[k]);
-        z[k] = B_field_get_axis_z(&sim.B_data, phi[k]);
-    }
-    
-    return 0;
-}
-
-int ascotpy_init_efield(char* fn) {
-    hdf5_init();
-    hid_t f = hdf5_open(fn);
-    int err = hdf5_efield_init_offload(f, &sim_offload.E_offload_data, &E_offload_array);
-    
-    E_field_init(&sim.E_data, &sim_offload.E_offload_data, E_offload_array);
-    E_field_free_offload(&sim_offload.E_offload_data, &E_offload_array);
-    
-    err = hdf5_close(f);
-    return err;
-}
-
-int ascotpy_free_efield() {
-
-    return 0;
-}
-
-int ascotpy_init_plasma(char* fn) {
-    hdf5_init();
-    hid_t f = hdf5_open(fn);
-    int err = hdf5_plasma_init_offload(f, &sim_offload.plasma_offload_data, &plasma_offload_array);
-    
-    plasma_init(&sim.plasma_data, &sim_offload.plasma_offload_data, plasma_offload_array);
-    plasma_free_offload(&sim_offload.plasma_offload_data, &plasma_offload_array);
-    
-    err = hdf5_close(f);
-    return err;
-}
-
-int ascotpy_free_plasma() {
-
-    return 0;
-}
-
-int ascotpy_init_wall(char* fn) {
-    hdf5_init();
-    hid_t f = hdf5_open(fn);
-    int err = hdf5_wall_init_offload(f, &sim_offload.wall_offload_data, &wall_offload_array);
-    
-    wall_init(&sim.wall_data, &sim_offload.wall_offload_data, wall_offload_array);
-    wall_free_offload(&sim_offload.wall_offload_data, &wall_offload_array);
-    
-    err = hdf5_close(f);
-    return err;
-}
-
-int ascotpy_free_wall() {
-
     return 0;
 }
--- ascotpy.py
+++ ascotpy.py
@@ -1,124 +1,174 @@
+"""
+Python side of the interactive Python interface.
+
+This module defines Ascotpy class whose methods can be used in Python to call
+Ascot5 functions (written in C) directly. The callable functions are defined in
+library module ascotpy.c which must be compiled first with make ascotpy. This
+module acts as a wrapper for those functions.
+
+File: ascotpy.py
+"""
 import ctypes
 import numpy as np
-from numpy.ctypeslib import ndpointer               
+from numpy.ctypeslib import ndpointer
 
-class ascotpy:
-    ascotlib = None
-    ascotfn  = None
-    bfield_initialized  = False
-    efield_initialized  = False
-    plasma_initialized  = False
-    wall_initialized    = False
+class Ascotpy:
+    """
+    An object representing a running ascot5 process.
+    """
 
     def __init__(self, libpath, ascotfn):
+        """
+        Initialize and start Ascot5 process using given HDF5 file as an input.
+
+        This function starts the process but does not read or initialize any
+        data yet. All flags are set to False and function arguments are defined
+        explicitly (because C uses strong typing whereas Python does not).
+        """
         self.ascotlib = ctypes.CDLL(libpath)
         self.ascotfn = ascotfn.encode('UTF-8')
 
-        # Since Python has weak typing while C has strong, we need to define #
-        # the type of the arguments for all python wrapper functions.        #
-        fun = self.ascotlib.ascotpy_bfield_eval_B
-        fun.restype = None
-        fun.argtypes = [ctypes.c_int,
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
-        
-        fun = self.ascotlib.ascotpy_bfield_eval_psi
-        fun.restype = None
-        fun.argtypes = [ctypes.c_int,
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
-        
-        fun = self.ascotlib.ascotpy_bfield_eval_rho
-        fun.restype = None
-        fun.argtypes = [ctypes.c_int,
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"),
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
-        
-        fun = self.ascotlib.ascotpy_bfield_eval_axis
-        fun.restype = None
-        fun.argtypes = [ctypes.c_int,
-                        ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS"), ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")]
-                        
-    def ascotpy_init_bfield(self):
-        self.ascotlib.ascotpy_init_bfield(self.ascotfn)
-        self.bfield_initialized  = True
+        self.bfield_initialized  = False
+        self.efield_initialized  = False
+        self.plasma_initialized  = False
+        self.neutral_initialized = False
+        self.wall_initialized    = False
 
-    def ascotpy_init_efield(self):
-        self.ascotlib.ascotpy_init_efield(self.ascotfn)
-        self.efield_initialized  = True
+        real_p = ndpointer(ctypes.c_double, flags="C_CONTIGUOUS")
 
-    def ascotpy_init_plasma(self):
-        self.ascotlib.ascotpy_init_plasma(self.ascotfn)
-        self.plasma_initialized  = True
+        fun = self.ascotlib.ascotpy_init
+        fun.restype  = ctypes.c_int
+        fun.argtypes = [ctypes.c_char_p, ctypes.c_int, ctypes.c_int,
+                        ctypes.c_int, ctypes.c_int, ctypes.c_int]
 
-    def ascotpy_init_wall(self):
-        self.ascotlib.ascotpy_init_wall(self.ascotfn)
-        self.wall_initialized  = True
+        fun = self.ascotlib.ascotpy_free
+        fun.restype  = None
+        fun.argtypes = [ctypes.c_int, ctypes.c_int, ctypes.c_int,
+                        ctypes.c_int, ctypes.c_int]
 
-    def ascotpy_eval_bfield(self, R, phi, z, vecB=None, jacB=None, psiB=None, rhoB=None, axis=None):
-        if(not self.bfield_initialized):
-            return 0
-        if(vecB is not None):
-            
-            BR   = np.zeros((len(R),1))
-            Bphi = np.zeros((len(R),1))
-            Bz   = np.zeros((len(R),1))
+        fun = self.ascotlib.ascotpy_bfield_eval_B_dB
+        fun.restype  = ctypes.c_int
+        fun.argtypes = [ctypes.c_int, real_p, real_p, real_p,
+                        real_p, real_p, real_p, real_p, real_p, real_p,
+                        real_p, real_p, real_p, real_p, real_p, real_p]
 
-            self.ascotlib.ascotpy_bfield_eval_B(len(R),R,phi,z,BR,Bphi,Bz)
-            vecB = np.squeeze(np.array([BR, Bphi, Bz]))
-            
-            return vecB             
-        if(psiB is not None):
-        
-            psi = np.zeros((len(R),1))            
-            self.ascotlib.ascotpy_bfield_eval_psi(len(R),R,phi,z,psi)
-            psiB = psi
+    def ascotpy_init(self, bfield=False, efield=False, plasma=False, wall=False,
+                     neutral=False):
+        """
+        Initialize input data.
 
-            return psiB
-        if(rhoB is not None):
-        
-            rho = np.zeros((len(R),1))            
-            self.ascotlib.ascotpy_bfield_eval_rho(len(R),R,phi,z,rho)
-            rhoB = rho
-           
-            return rhoB
-            
-        if(axis is not None):
-        
-            R = np.zeros((len(R),1))
-            z = np.zeros((len(z),1))
-            self.ascotlib.ascotpy_bfield_eval_rho(len(R),phi,R,z)
-            axis = (R,z)
-           
-            return axis
-                  
-    def ascotpy_eval_efield(self, R, phi, z, vecE=None):
-        if( (not self.bfield_initialized) or (not self.efield_initialized) ):
-            return
+        Args:
+            bfield : bool, optional <br>
+                Flag for initializing magnetic field.
+            efield : bool, optional <br>
+                Flag for initializing electric field.
+            plasma : bool, optional <br>
+                Flag for initializing plasma data.
+            wall : bool, optional <br>
+                Flag for initializing wall data.
+            neutral : bool, optional <br>
+                Flag for initializing neutral data.
 
-    def ascotpy_eval_plasma(self, R, phi, z, species=None, temperatur=None, density=None, Anum=None, Znum=None):
-        if( (not self.bfield_initialized) or (not self.plasma_initialized) ):
-            return
+        Raises:
+            RuntimeError if initialization failed.
+        """
+        if bfield:
+            if self.ascotlib.ascotpy_init(self.ascotfn, 1, 0, 0, 0, 0) :
+                raise RuntimeError("Failed to initialize magnetic field")
 
-    def ascotpy_eval_FOccol(self, R, phi, z, species=None, temperatur=None, density=None, Anum=None, Znum=None):
-        return
+            self.bfield_initialized  = True
 
-    def ascotpy_eval_GCccol(self, R, phi, z, species=None, temperatur=None, density=None, Anum=None, Znum=None):
-        return
+    def ascotpy_free(self, bfield=False, efield=False, plasma=False, wall=False,
+                     neutral=False):
+        """
+        Free input data.
 
+        Args:
+            bfield : bool, optional <br>
+                Flag for freeing magnetic field.
+            efield : bool, optional <br>
+                Flag for freeing electric field.
+            plasma : bool, optional <br>
+                Flag for freeing plasma data.
+            wall : bool, optional <br>
+                Flag for freeing wall data.
+            neutral : bool, optional <br>
+                Flag for freeing neutral data.
+        """
+        if bfield:
+            self.ascotlib.ascotpy_free(1, 0, 0, 0, 0)
+            self.bfield_initialized  = False
 
-def main():
-    ascot = ascotpy("/l/sarkimk1/repos/ascot5/ascotpy.so","ascot.h5")
-    ascot.ascotpy_init_bfield()
-    
-    R = np.array([6.2, 7, 8],dtype=np.float64)
-    phi = np.array([0, 0, 0],dtype=np.float64)
-    z = np.array([0.2, 0.2, 0.2],dtype=np.float64)
-    vecB=1
-    vecB = ascot.ascotpy_eval_bfield(R, phi, z, vecB=vecB)
-    print(vecB)
-    
+    def ascotpy_eval_bfield(self, R, phi, z, evalb=False, evalpsi=False,
+                            evalrho=False, evalaxis=False):
+        """
+        Evaluate magnetic field quantities at given coordinates.
+
+        Args:
+            R : array_like <br>
+                Vector of R coordinates where field is evaluated [m].
+            phi : array_like <br>
+                Vector of phi coordinates where field is evaluated [rad].
+            z : array_like <br>
+                Vector of z coordinates where field is evaluated [m].
+            evalb : bool, optional <br>
+                Evaluate magnetic field vector and derivatives.
+            evalpsi : bool, optional <br>
+                Evaluate poloidal flux.
+            evalrho : bool, optional <br>
+                Evaluate normalized poloidal flux.
+            evalaxis : bool, optional <br>
+                Evaluate magnetic axis.
+
+        Returns:
+            Dictionary containing evaluated quantities.
+
+        Raises:
+            AssertionError if this is called magnetic field uninitialized.
+            RuntimeError if evaluation failed.
+        """
+        assert(self.bfield_initialized)
+
+        R   = R.astype(dtype="f8")
+        phi = phi.astype(dtype="f8")
+        z   = z.astype(dtype="f8")
+
+        Neval = R.size
+        out = {}
+        if evalb:
+
+            out["br"]       = np.zeros(R.shape, dtype="f8")
+            out["bphi"]     = np.zeros(R.shape, dtype="f8")
+            out["bz"]       = np.zeros(R.shape, dtype="f8")
+            out["brdr"]     = np.zeros(R.shape, dtype="f8")
+            out["brdphi"]   = np.zeros(R.shape, dtype="f8")
+            out["brdz"]     = np.zeros(R.shape, dtype="f8")
+            out["bphidr"]   = np.zeros(R.shape, dtype="f8")
+            out["bphidphi"] = np.zeros(R.shape, dtype="f8")
+            out["bphidz"]   = np.zeros(R.shape, dtype="f8")
+            out["bzdr"]     = np.zeros(R.shape, dtype="f8")
+            out["bzdphi"]   = np.zeros(R.shape, dtype="f8")
+            out["bzdz"]     = np.zeros(R.shape, dtype="f8")
+
+            self.ascotlib.ascotpy_bfield_eval_B_dB(
+                Neval, R, phi, z,
+                out["br"], out["bphi"], out["bz"],
+                out["brdr"], out["brdphi"], out["brdz"],
+                out["bphidr"], out["bphidphi"], out["bphidz"],
+                out["bzdr"], out["bzdphi"], out["bzdz"])
+
+        return out
+
 if __name__ == '__main__':
-    main()
+    # For testing purposes.
+    import os
+    ascot = Ascotpy(os.path.abspath("ascotpy.so"), "ascot.h5")
+    ascot.ascotpy_init(bfield=True)
 
+    R   = np.array([6.2,   7, 8])
+    phi = np.array([  0,   0, 0])
+    z   = np.array([0.0, 0.2, 0.2])
 
+    bvals = ascot.ascotpy_eval_bfield(R, phi, z, evalb=True)
+
+    ascot.ascotpy_free(bfield=True)
