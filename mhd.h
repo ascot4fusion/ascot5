bc62bdc9df0e16a1f6c4121a61c3e433f0f09ac3
etolman@mit.edu <etolman@mit.edu>
2019-02-07 00:38:35 -0800

    Updating to reflect change in quantities returned by boozer #48

--- mhd.h
+++ mhd.h
@@ -61,9 +61,9 @@ void mhd_free_offload(mhd_offload_data* offload_data,
 void mhd_init(mhd_data* MHDdata, mhd_offload_data* offload_data,
               real* offload_array);
 
-#pragma omp declare simd uniform(boozerdata, mhddata)
+#pragma omp declare simd uniform(boozerdata, mhddata, Bdata)
 a5err mhd_eval(real mhd_dmhd[10], real phase, real r, real phi, real z, real t,
-               boozer_data* boozerdata, mhd_data* mhddata);
+               boozer_data* boozerdata, mhd_data* mhddata, B_field_data* Bdata);
 
 #pragma omp declare simd uniform(boozerdata, mhddata, Bdata)
 a5err mhd_perturbations(real pert_field[6], real phase, real r, real phi, real z, real t, boozer_data* boozerdata, mhd_data* mhddata, B_field_data* Bdata);
