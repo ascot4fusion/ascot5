47492c92e9428de5dffe0fb0bf44d9d77b7b8c09
sarkimk1 <konsta.sarkimaki@aalto.fi>
2019-02-22 19:07:49 +0200

    Read HDF5 for plasma 1DS

--- hdf5io/hdf5_plasma.c
+++ hdf5io/hdf5_plasma.c
@@ -170,71 +170,70 @@ int hdf5_plasma_read_1DS(hid_t f, plasma_1DS_offload_data* offload_data,
 
     #undef PLSPATH
     #define PLSPATH "/plasma/plasma_1DS-XXXXXXXXXX/"
-    herr_t err = 0;
-    int i, j;
-    err = H5LTfind_dataset(f, "/plasma/P_1D");
 
-    int n_ions;
-    err = H5LTget_attribute_int(f, "/plasma/", "n_ions", &n_ions);
+    /* Read rhogrid and number of species */
+    int n_rho, n_ions;
+    if( hdf5_read_int(PLSPATH "n_ions", &n_ions,
+                      f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_int(PLSPATH "n_rho",  &offload_data->n_rho,
+                      f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_double(PLSPATH "rho_min",  &offload_data->rho_min,
+                         f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_double(PLSPATH "rho_max",  &offload_data->rho_max,
+                         f, qid, __FILE__, __LINE__) ) {return 1;}
+
     offload_data->n_species = n_ions + 1; /* Include electrons */
+    n_rho = offload_data->n_rho;
 
-    err = H5LTget_attribute_int(f, "/plasma/P_1D", "n_rho", &(offload_data->n_rho));
-    int n_rho = offload_data->n_rho;
 
-    /* Allocate space for rho + temperature for each ion species and electrons
-     * + density for each ion species and electrons */
-    offload_data->offload_array_length = n_rho + 2*offload_data->n_species*n_rho;
+    /* Read species information */
+    int temparr[MAX_SPECIES];
+
+    if( hdf5_read_int(PLSPATH "Z_num", temparr,
+                      f, qid, __FILE__, __LINE__) ) {return 1;}
+    for(int i = 0; i < n_ions; i++) {
+        offload_data->charge[i+1] = temparr[i] * CONST_E;
+    }
+    /* Electron charge -1 */
+    offload_data->charge[0] = -1 * CONST_E;
+
+    /* Read Amass and calculate mass */
+    if( hdf5_read_int(PLSPATH "A_mass", temparr,
+                      f, qid, __FILE__, __LINE__) ) {return 1;}
+    for(int i = 0; i < n_ions; i++) {
+        offload_data->mass[i+1] = temparr[i] * CONST_U;
+    }
+    /* Electron mass */
+    offload_data->mass[0] = CONST_M_E;
+
+    /* Allocate space for density (for each species) and
+       temperature (for electrons and ions - all ions have same temperature) */
+    offload_data->offload_array_length =
+        2*n_rho + offload_data->n_species*n_rho;
     *offload_array = (real*) malloc(sizeof(real)
                                     * offload_data->offload_array_length);
 
     /* Pointers to beginning of different data series to make code more
      * readable */
-    real* rho = &(*offload_array)[0];
-    real* temp_e = &(*offload_array)[n_rho];
-    real* temp_i = &(*offload_array)[n_rho*2];
-    real* dens_e = &(*offload_array)[n_rho*2
-                                     + n_rho*n_ions];
-    real* dens_i = &(*offload_array)[n_rho*2
-                                     + n_rho*n_ions
-                                     + n_rho];
+    real* temp_e = &(*offload_array)[0];
+    real* temp_i = &(*offload_array)[n_rho*1];
+    real* dens_e = &(*offload_array)[n_rho*2];
+    real* dens_i = &(*offload_array)[n_rho*3];
 
-    /* Read Znum and calculate charge */
-    int Znum[n_ions];
-    err = H5LTread_dataset_int(f,"/plasma/Z_num",Znum);
-    /* Electron charge -1 */
-    offload_data->charge[0] = -1 * CONST_E;
-    for(i = 0; i < n_ions; i++) {
-        offload_data->charge[i+1] = Znum[i] * CONST_E;
-    }
+    /* Read densities, and temperatures into allocated array */
+    if( hdf5_read_double(PLSPATH "temp_e", temp_e,
+                         f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_double(PLSPATH "dens_e", dens_e,
+                         f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_double(PLSPATH "temp_i", temp_i,
+                         f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_double(PLSPATH "dens_i", dens_i,
+                         f, qid, __FILE__, __LINE__) ) {return 1;}
 
-    /* Read Amass and calculate mass */
-    int Amass[n_ions];
-    err = H5LTread_dataset_int(f,"/plasma/A_mass", Amass);
-    offload_data->mass[0] = CONST_M_E;
-    for(i = 0; i < n_ions; i++) {
-        offload_data->mass[i+1] = Amass[i] * CONST_U;
-    }
-
-    /* Read actual data into array */
-    err = H5LTread_dataset_double(f,"/plasma/P_1D/rho", rho);
-    err = H5LTread_dataset_double(f,"/plasma/P_1D/temp_e", temp_e);
-    for(i = 0; i < n_rho; i++) {
+    for(int i = 0; i < n_rho; i++) {
         temp_e[i] = temp_e[i] * CONST_E;
-    }
-    err = H5LTread_dataset_double(f,"/plasma/P_1D/dens_e", dens_e);
-
-    /* All ions have same temperature */
-    err = H5LTread_dataset_double(f,"/plasma/P_1D/temp_i", temp_i);
-    for(i = 0; i < n_rho; i++) {
         temp_i[i] = temp_i[i] * CONST_E;
     }
 
-    real temp_dens_i[n_ions*n_rho];
-    err = H5LTread_dataset_double(f,"/plasma/P_1D/dens_i", temp_dens_i);
-    for(j = 0; j < n_ions; j++) {
-        for(i = 0; i < n_rho; i++) {
-            dens_i[j*n_rho + i] = temp_dens_i[j*n_rho + i];
-        }
-    }
-    return err;
+    return 0;
 }
