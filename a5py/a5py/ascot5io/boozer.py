6e4a8c475e4304c5fe7814ea7d0b71ad08b53b1f
sarkimk1 <konsta.sarkimaki@aalto.fi>
2019-03-05 22:15:54 +0200

    Renamed some boozer HDF5 fields and documented arguments

--- a5py/a5py/ascot5io/boozer.py
+++ a5py/a5py/ascot5io/boozer.py
@@ -9,8 +9,6 @@ import numpy as np
 from . ascot5file import add_group
 from . ascot5data import AscotData
 
-## ADD COMMENTS AND SIZE DEFINITIONS OF THE DATA FIELDS
-
 def write_hdf5(fn, psimin, psimax, npsi, thetamin, thetamax, ntheta, rmin,
                rmax, nr, zmin, zmax, nz, r0, z0, psiin, psiout, psi_rz,
                theta_psithetageom, nu_psitheta, rs, zs, nrzs, desc=None):
@@ -20,8 +18,51 @@ def write_hdf5(fn, psimin, psimax, npsi, thetamin, thetamax, ntheta, rmin,
     Args:
         fn : str <br>
             Full path to the HDF5 file.
+        psimin : float <br>
+            Minimum psi grid value.
+        psimax : float <br>
+            Maximum psi grid value.
+        npsi : int <br>
+            Number of psi grid points.
+        ntheta : int <br>
+            Number of theta grid (both boozer and geometric) values.
+        rmin : float <br>
+            Minimum R grid value.
+        rmax : float <br>
+            Maximum R grid value.
+        nr : int <br>
+            Number of R grid points.
+        zmin : float <br>
+            Minimum z grid value.
+        zmax : float <br>
+            Maximum z grid value.
+        nz : int <br>
+            Number of z grid points.
+        r0 : float <br>
+            Magnetic axis R coordinate.
+        z0 : float <br>
+            Magnetic axis z coordinate.
+        psi0 : float <br>
+            Coordinate psi on axis.
+        psi1 : float <br>
+            Coordinate psi on separatrix.
+        psi_rz : array_like (nz,nr) <br>
+            Coordinate psi(R,z).
+        theta_psithetageom : array_like (ntheta,npsi) <br>
+            Coordinate theta(psi, thetageom).
+        nu_psitheta : array_like (ntheta,psi) <br>
+            nu(psi, theta).
+        nrsz : int <br>
+            Number of separatrix Rz points.
+        rs : array_like (nrsz,1) <br>
+            Separatrix R coordinates.
+        zs : array_like (nrsz,1) <br>
+            Separatrix z coordinates.
         desc : str, optional <br>
             Input's description.
+
+    Returns:
+        Name of the new input that was written.
     """
 
     parent = "boozer"
@@ -31,21 +72,21 @@ def write_hdf5(fn, psimin, psimax, npsi, thetamin, thetamax, ntheta, rmin,
         g = add_group(f, parent, group, desc=desc)
 
         # grid specifications
-        g.create_dataset("psi_min",    (1,), data=psimin,     dtype="f8")
-        g.create_dataset("psi_max",    (1,), data=psimax,     dtype="f8")
-        g.create_dataset("npsi",       (1,), data=npsi,       dtype="i8")
-        g.create_dataset("thetamin",   (1,), data=thetamin,   dtype="f8")
-        g.create_dataset("thetamax",   (1,), data=thetamax,   dtype="f8")
-        g.create_dataset("ntheta",     (1,), data=ntheta,     dtype="i8")
-        g.create_dataset("r_min",      (1,), data=rmin,       dtype="f8")
-        g.create_dataset("r_max",      (1,), data=rmax,       dtype="f8")
-        g.create_dataset("nr",         (1,), data=nr,         dtype="i8")
-        g.create_dataset("z_min",      (1,), data=zmin,       dtype="f8")
-        g.create_dataset("z_max",      (1,), data=zmax,       dtype="f8")
-        g.create_dataset("nz",         (1,), data=nz,         dtype="i8")
-        g.create_dataset("r0",         (1,), data=r0,         dtype="f8")
-        g.create_dataset("z0",         (1,), data=z0,         dtype="f8")
-        g.create_dataset("nrzs",       (1,), data=nrzs,       dtype="i8")
+        g.create_dataset("psimin",   (1,), data=psimin,   dtype="f8")
+        g.create_dataset("psimax",   (1,), data=psimax,   dtype="f8")
+        g.create_dataset("npsi",     (1,), data=npsi,     dtype="i8")
+        g.create_dataset("thetamin", (1,), data=thetamin, dtype="f8")
+        g.create_dataset("thetamax", (1,), data=thetamax, dtype="f8")
+        g.create_dataset("ntheta",   (1,), data=ntheta,   dtype="i8")
+        g.create_dataset("rmin",     (1,), data=rmin,     dtype="f8")
+        g.create_dataset("rmax",     (1,), data=rmax,     dtype="f8")
+        g.create_dataset("nr",       (1,), data=nr,       dtype="i8")
+        g.create_dataset("zmin",     (1,), data=zmin,     dtype="f8")
+        g.create_dataset("zmax",     (1,), data=zmax,     dtype="f8")
+        g.create_dataset("nz",       (1,), data=nz,       dtype="i8")
+        g.create_dataset("r0",       (1,), data=r0,       dtype="f8")
+        g.create_dataset("z0",       (1,), data=z0,       dtype="f8")
+        g.create_dataset("nrzs",     (1,), data=nrzs,     dtype="i8")
 
         # the outermost poloidal psi-surface contour
         g.create_dataset("rs", (nrzs,), data=rs, dtype="f8")
@@ -57,8 +98,12 @@ def write_hdf5(fn, psimin, psimax, npsi, thetamin, thetamax, ntheta, rmin,
 
         # tabulated coordinates maps
         g.create_dataset("psi_rz", (nr,nz), data=psi_rz, dtype="f8")
-        g.create_dataset("theta_psithetageom", (npsi,ntheta), data=theta_psithetageom, dtype="f8")
-        g.create_dataset("nu_psitheta", (npsi,ntheta), data=nu_psitheta, dtype="f8")
+        g.create_dataset("theta_psithetageom", (npsi,ntheta),
+                         data=theta_psithetageom, dtype="f8")
+        g.create_dataset("nu_psitheta", (npsi,ntheta),
+                         data=nu_psitheta, dtype="f8")
+
+    return g.name
 
 
 def read_hdf5(fn, qid):
@@ -69,27 +114,21 @@ def read_hdf5(fn, qid):
         fn : str <br>
             Full path to the HDF5 file.
         qid : str <br>
-            qid of the efield to be read.
+            QID of the data to be read.
 
     Returns:
-        Dictionary containing Boozer data.
+        Dictionary containing input data.
     """
 
-    path = "boozer" + "/Boozer-" + qid
+    path = "boozer/Boozer_" + qid
 
+    out = {}
     with h5py.File(fn,"r") as f:
-        out = {}
-
-        # Metadata.
-        out["qid"]  = qid
-        out["date"] = f[path].attrs["date"]
-        out["desc"] = f[path].attrs["desc"]
-
-        # Actual data.
-        for k in f[path].keys():
-            out[k] = f[path][k][:]
+        for key in f[path]:
+            out[key] = f[path][key][:]
 
     return out
+
 
 def write_hdf5_dummy(fn, desc="Dummy"):
     """
@@ -103,8 +142,6 @@ def write_hdf5_dummy(fn, desc="Dummy"):
     psimin     = 0
     psimax     = 1
     npsi       = 6
-    thetamin   = 0.0
-    thetamax   = 2*np.math.pi
     ntheta     = 10
     rmin       = 0.1
     rmax       = 10.0
@@ -125,6 +162,15 @@ def write_hdf5_dummy(fn, desc="Dummy"):
     theta_psithetageom = np.ones((npsi,ntheta))
     nu_psitheta = np.ones((npsi,ntheta))
 
-    write_hdf5(fn, psimin, psimax, npsi, thetamin, thetamax, ntheta, rmin,
+    write_hdf5(fn, psimin, psimax, npsi, ntheta, rmin,
                rmax, nr, zmin, zmax, nz, r0, z0, psiin, psiout, psi_rz,
-               theta_psithetageom, nu_psitheta, rs, zs, nrzs, desc=desc)
+               theta_psithetageom, nu_psitheta, nrzs, rs, zs, desc=desc)
+
+
+class Boozer(AscotData):
+    """
+    Object representing boozer data.
+    """
+
+    def read(self):
+        return read_hdf5(self._file, self.get_qid())
