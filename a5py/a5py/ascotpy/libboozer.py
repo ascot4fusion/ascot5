de665c5b64cc7bb3f47a615d3a843651c01b5027
etolman@mit.edu <etolman@mit.edu>
2019-03-18 19:44:14 -0700

    add libboozer.py file #48

---
+++ a5py/a5py/ascotpy/libboozer.py
@@ -0,0 +1,34 @@
+"""
+Methods to evaluate quantities from boozer data.
+
+File: libbbozer.py
+"""
+import numpy as np
+
+from a5py.ascotpy.libascot import LibAscot
+
+
+import importlib.util as util
+
+plt = util.find_spec("matplotlib")
+if plt:
+    import matplotlib.pyplot as plt
+
+class LibBoozer(LibAscot):
+
+    quantities = ["psi", "theta", "zeta", "dpsidr", "dpsidphi", "dpsidz",\
+                  "dthetadr", "dthetadphi", "dthetadz", "dzetadr", \
+                  "dzetadphi", "dzetadz"]
+
+    def evaluate(self, R, phi, z, t, quantity):
+
+        out = None
+
+		if quantity in ["psi", "theta", "zeta", "dpsidr", "dpsidphi", "dpsidz",\
+                        "dthetadr", "dthetadphi", "dthetadz", "dzetadr", \
+                        "dzetadphi", "dzetadz"]:
+            out = self.eval_mhd(R, phi, z, t)[quantity]
+
+        assert out is not None, "Unknown quantity"
+
+        return out
