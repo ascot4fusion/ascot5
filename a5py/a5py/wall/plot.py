e5e5bd0ab7ad78a5be58a87317a5cf75edf09316
Joona Kontula <joona.kontula@aalto.fi>
2019-03-28 11:17:43 +0200

    Improved wall plotting algorithm

--- a5py/a5py/wall/plot.py
+++ a5py/a5py/wall/plot.py
@@ -7,6 +7,8 @@ import numpy as np
 
 import importlib.util as util
 
+import time
+
 plt = util.find_spec("matplotlib")
 if plt:
         import matplotlib.pyplot as plt
@@ -58,44 +60,38 @@ def plot_intersection(x1x2x3, y1y2y3, z1z2z3, phi, axes=None):
 
     # Polar coordinates, p1p2p3 in range [-pi, pi]
     r1r2r3, p1p2p3 = mathlib.cart2pol(x1x2x3, y1y2y3)
-    p1p2p3 = np.degrees(p1p2p3)
-    # Intersection happens if
-    # phi_i < phi and phi_j >= phi for some i, j = [1 2 3]
-    # Phi to range [-180, 180)
-    phi = np.degrees(phi)
-    phi = np.mod(phi + 180, 360) - 180
+    # Wrap phi to range [-pi, pi)
+    phi = np.mod(phi + np.pi, 2*np.pi) - np.pi
     # Set p1p2p3 == phi to zero
     p1p2p3 = p1p2p3 - phi
-    ind_crossing = np.amax(p1p2p3, 1) - np.amin(p1p2p3, 1) < 180
-    ind_int = np.logical_and(np.abs(np.sum(np.copysign(1, p1p2p3), 1)) < 3,
-                             ind_crossing)
-    # Remove extra data
-    x1x2x3 = x1x2x3[ind_int, :]
-    y1y2y3 = y1y2y3[ind_int, :]
-    z1z2z3 = z1z2z3[ind_int, :]
-    r1r2r3 = r1r2r3[ind_int, :]
-    p1p2p3 = p1p2p3[ind_int, :]
+    ind_crossing = np.amax(p1p2p3, 1) - np.amin(p1p2p3, 1) < np.pi
+    # Intersection happens if (any(p1p2p3) < phi and any(p1p2p3) >= phi)
+    p1p2p3_sign = np.copysign(1, p1p2p3)
+    p1p2p3_polarity = np.sum(p1p2p3_sign, 1)
+    ind_int = np.logical_and(np.abs(p1p2p3_polarity) < 3, ind_crossing)
+    # Remove unnecessary data
+    x1x2x3          = x1x2x3[ind_int, :]
+    y1y2y3          = y1y2y3[ind_int, :]
+    z1z2z3          = z1z2z3[ind_int, :]
+    r1r2r3          = r1r2r3[ind_int, :]
+    p1p2p3          = p1p2p3[ind_int, :]
+    p1p2p3_polarity = p1p2p3_polarity[ind_int]
+    p1p2p3_sign     = p1p2p3_sign[ind_int, :]
     n = x1x2x3.shape[0]
     # Find intersection line for all crossing triangles
-    r = np.zeros((n, 2))
-    z = np.zeros((n, 2))
-    for i in range(n):
-        # For the crossing triangles, one of the points is always on the other
-        # side of phi-plane as the other two
-        polarity = np.sum(np.copysign(1, p1p2p3[i, :]))
-        p_diff = np.flatnonzero(np.copysign(1, p1p2p3[i, :]) != polarity)
-        # The triangle edge is linear, so we can easily calculate the
-        # cross-section points
-        p_same = np.setdiff1d([0, 1, 2], p_diff)
-        for j in range(2):
-            phi_diff = (
-                    - p1p2p3[i, p_same[j]]  # Distance to phi = 0
-                    / (p1p2p3[i, p_diff] - p1p2p3[i, p_same[j]]) # Width of phi
-                    )
-            r[i, j] = (r1r2r3[i, p_same[j]]
-                       + (r1r2r3[i, p_diff] - r1r2r3[i, p_same[j]]) * phi_diff)
-            z[i, j] = (z1z2z3[i, p_same[j]]
-                       + (z1z2z3[i, p_diff] - z1z2z3[i, p_same[j]]) * phi_diff)
+    p1p2p3_polarity = np.tile(p1p2p3_polarity, (3,1)).T
+
+    phi_diff = p1p2p3[p1p2p3_sign != p1p2p3_polarity]
+    phi_same = np.reshape(p1p2p3[p1p2p3_sign == p1p2p3_polarity], (n,2))
+    phi_diffs = (- phi_same / (phi_diff[:,None] - phi_same))
+
+    r_diff = r1r2r3[p1p2p3_sign != p1p2p3_polarity]
+    r_same = np.reshape(r1r2r3[p1p2p3_sign == p1p2p3_polarity], (n,2))
+    r = r_same + (r_diff[:,None] - r_same ) * phi_diffs
+
+    z_diff = z1z2z3[p1p2p3_sign != p1p2p3_polarity]
+    z_same = np.reshape(z1z2z3[p1p2p3_sign == p1p2p3_polarity], (n,2))
+    z = z_same + (z_diff[:,None] - z_same ) * phi_diffs
 
     axes.plot(r.T, z.T, 'k')
     axes.axis("scaled")
