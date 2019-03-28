c61380c25cb1ef2fac919fae53cf64a8667b86f0
Joona Kontula <joona.kontula@aalto.fi>
2019-03-28 11:41:51 +0200

    Wall plot updated to final version (for now)

--- a5py/a5py/wall/plot.py
+++ a5py/a5py/wall/plot.py
@@ -7,8 +7,6 @@ import numpy as np
 
 import importlib.util as util
 
-import time
-
 plt = util.find_spec("matplotlib")
 if plt:
         import matplotlib.pyplot as plt
@@ -60,39 +58,32 @@ def plot_intersection(x1x2x3, y1y2y3, z1z2z3, phi, axes=None):
 
     # Polar coordinates, p1p2p3 in range [-pi, pi]
     r1r2r3, p1p2p3 = mathlib.cart2pol(x1x2x3, y1y2y3)
-    # Wrap phi to range [-pi, pi)
+    # Wrap phi to range [-pi, pi) and set p1p2p3 == phi to zero
     phi = np.mod(phi + np.pi, 2*np.pi) - np.pi
-    # Set p1p2p3 == phi to zero
     p1p2p3 = p1p2p3 - phi
-    ind_crossing = np.amax(p1p2p3, 1) - np.amin(p1p2p3, 1) < np.pi
-    # Intersection happens if (any(p1p2p3) < phi and any(p1p2p3) >= phi)
+    # Filter out triangles crossing -pi/pi
+    ind_invalid = np.amax(p1p2p3, 1) - np.amin(p1p2p3, 1) < np.pi
+    # Intersection happens if any(p1p2p3) < phi = 0 and any(p1p2p3) >= phi = 0
     p1p2p3_sign = np.copysign(1, p1p2p3)
     p1p2p3_polarity = np.sum(p1p2p3_sign, 1)
-    ind_int = np.logical_and(np.abs(p1p2p3_polarity) < 3, ind_crossing)
-    # Remove unnecessary data
-    x1x2x3          = x1x2x3[ind_int, :]
-    y1y2y3          = y1y2y3[ind_int, :]
-    z1z2z3          = z1z2z3[ind_int, :]
-    r1r2r3          = r1r2r3[ind_int, :]
-    p1p2p3          = p1p2p3[ind_int, :]
-    p1p2p3_polarity = p1p2p3_polarity[ind_int]
-    p1p2p3_sign     = p1p2p3_sign[ind_int, :]
-    n = x1x2x3.shape[0]
+    ind_inter = np.logical_and(np.abs(p1p2p3_polarity) < 3, ind_invalid)
+
     # Find intersection line for all crossing triangles
     p1p2p3_polarity = np.tile(p1p2p3_polarity, (3,1)).T
-    ind_diff = p1p2p3_sign != p1p2p3_polarity
-    ind_same = p1p2p3_sign == p1p2p3_polarity
+    ind_inter = np.tile(ind_inter, (3,1)).T
+    ind_diff = np.logical_and(p1p2p3_sign != p1p2p3_polarity, ind_inter)
+    ind_same = np.logical_and(p1p2p3_sign == p1p2p3_polarity, ind_inter)
 
     phi_diff = p1p2p3[ind_diff]
-    phi_same = np.reshape(p1p2p3[ind_same], (n,2))
+    phi_same = np.reshape(p1p2p3[ind_same], (-1,2))
     phi_diffs = (- phi_same / (phi_diff[:,None] - phi_same))
 
     r_diff = r1r2r3[ind_diff]
-    r_same = np.reshape(r1r2r3[ind_same], (n,2))
+    r_same = np.reshape(r1r2r3[ind_same], (-1,2))
     r = r_same + (r_diff[:,None] - r_same ) * phi_diffs
 
     z_diff = z1z2z3[ind_diff]
-    z_same = np.reshape(z1z2z3[ind_same], (n,2))
+    z_same = np.reshape(z1z2z3[ind_same], (-1,2))
     z = z_same + (z_diff[:,None] - z_same ) * phi_diffs
 
     axes.plot(r.T, z.T, 'k')
