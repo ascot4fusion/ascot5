1df250072a759afda5c17e94065a22598b8d5bbf
sarkimk1 <konsta.sarkimaki@aalto.fi>
2019-04-05 14:14:07 +0300

    Phi angle can be designated in the wall plot

--- a5py/a5py/gui/wallframe.py
+++ a5py/a5py/gui/wallframe.py
@@ -9,6 +9,7 @@ import tkinter.ttk as ttk
 import numpy as np
 
 from .plotframe import PlotFrame
+from .components import NumEntry
 
 class WallFrame(PlotFrame):
     """
@@ -24,6 +25,13 @@ class WallFrame(PlotFrame):
         self._gui  = gui
         self._wall = wall
 
+        panel = self.get_sidepanel()
+        self.phi_entry = NumEntry(panel, labeltext="phi [deg]:", defval=0)
+        self.phi_entry.grid(row=0, column=0)
+
+        plotbutton = tkinter.Button(panel, text="Plot", command=self._plot)
+        plotbutton.grid(row=1, column=0, sticky="WE")
+
         self._plot()
 
 
@@ -31,8 +39,10 @@ class WallFrame(PlotFrame):
         """
         Read control states and plot.
         """
+        phi  = self.phi_entry.getval() * np.pi / 180
+
         fig  = self.get_fig()
         axes = fig.add_subplot(1,1,1)
-        self._wall.plotRz(axes=axes)
+        self._wall.plotRz(axes=axes, phi=phi)
 
         self.draw()
