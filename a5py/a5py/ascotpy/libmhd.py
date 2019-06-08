38f39d18e6b5bc665e8e83da2c1f279aa1785632
sarkimk1 <konsta.sarkimaki@aalto.fi>
2019-06-08 11:29:47 +0300

    ascotpy evaluates mhd b/db

--- a5py/a5py/ascotpy/libmhd.py
+++ a5py/a5py/ascotpy/libmhd.py
@@ -17,7 +17,7 @@ if plt:
 class LibMhd(LibAscot):
 
     quantities = ["mhd_br", "mhd_bphi", "mhd_bz","mhd_er", "mhd_ephi",
-                  "mhd_ez","mhd_phi"]
+                  "mhd_ez","mhd_phi", "db/b"]
 
     def evaluate(self, R, phi, z, t, quantity):
 
@@ -25,6 +25,16 @@ class LibMhd(LibAscot):
         if quantity in ["mhd_br", "mhd_bphi", "mhd_bz", "mhd_er", "mhd_ephi",
                         "mhd_ez", "mhd_phi"]:
             out = self.eval_mhd(R, phi, z, t)[quantity]
+
+        if quantity in ["db/b"]:
+            bpert = self.eval_mhd(R, phi, z, t)
+            b = self.eval_bfield(R, phi, z, t, evalb=True)
+
+            bpert = np.sqrt(bpert["mhd_br"]**2 + bpert["mhd_bphi"]**2
+                            +  bpert["mhd_bz"]**2)
+            b = np.sqrt(b["br"]**2 + b["bphi"]**2 + b["bz"]**2)
+
+            out = bpert/b
 
         assert out is not None, "Unknown quantity"
 
