f6d33355b98e1d220ab4c6b8118d3406b968c416
sarkimk1 <konsta.sarkimaki@aalto.fi>
2019-06-16 22:25:03 +0300

    Ripple map added to ascotpy and gui

--- a5py/a5py/ascotpy/libbfield.py
+++ a5py/a5py/ascotpy/libbfield.py
@@ -45,7 +45,43 @@ class LibBfield(LibAscot):
         return out
 
 
+    def evaluateripple(self, R, z, t, nphi):
+        nin = R.size
+        phigrid = np.linspace(0, 2*np.pi, nphi+1)[:-1]
+        R = np.meshgrid(R, phigrid, indexing="ij")[0]
+        z = np.meshgrid(z, phigrid, indexing="ij")[0]
+        t, phigrid = np.meshgrid(t, phigrid, indexing="ij")
+        out = np.abs(self.eval_bfield(R.ravel(), phigrid.ravel(), z.ravel(),
+                                      t.ravel(), evalb=True)["bphi"])
+        out = out.reshape(nin, nphi)
+        bmax = np.nanmax(out, axis=1)
+        bmin = np.nanmin(out, axis=1)
+        return (bmax - bmin) / (bmax + bmin)
+
+
     def plotseparatrix(self, R, phi, z, t, axes):
         out = self.evaluate(R, phi, z, t, "rho", grid=True)
 
-        mesh = axes.contour(R, z, np.transpose(out[:,0,:,0]), [1], colors='black',zorder=1)
+        mesh = axes.contour(R, z, np.transpose(out[:,0,:,0]), [1],
+                            colors='black',zorder=1)
+
+
+    def plotripple(self, Rgrid, zgrid, time, nphi, axes=None):
+        R, z, t = np.meshgrid(Rgrid, zgrid, time, indexing="ij")
+        rip = self.evaluateripple(R, z, t, nphi).reshape(Rgrid.size, zgrid.size)
+
+        newfig = axes is None
+        if newfig:
+            plt.figure()
+            axes = plt.gca()
+
+        CS = axes.contour(Rgrid, zgrid, 100 * rip.transpose(),
+                          [0.01, 0.05, 0.1, 0.5, 1, 5])
+        axes.clabel(CS)
+
+        axes.set_aspect("equal", adjustable="box")
+        axes.set_xlim(Rgrid[0], Rgrid[-1])
+        axes.set_ylim(zgrid[0], zgrid[-1])
+
+        if newfig:
+            plt.show(block=False)
