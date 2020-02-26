83c0fbbc6266a9e1d9987077fef83d9ebd7952c3
Jari Varje <jari.varje@aalto.fi>
2020-02-26 11:04:07 +0000

    Add a couple of new moments to 1ddists, seem to be working

--- a5py/a5py/dist/radial.py
+++ a5py/a5py/dist/radial.py
@@ -10,18 +10,51 @@ import scipy.constants as const
 from . import basic as distmod
 from . import conversion as distconv
 
-def eval1d(ascotpy, dist, quantity, rhomin, rhomax, nrho, ma=None, qa=None):
+def eval1d(ascotpy, dist, quantity, rhomin, rhomax, nrho, ma=None, qa=None, vol=None, area=None):
 
-    # Find rho volume
-    vol = evalrhovol(
-        ascotpy, rhomin, rhomax, nrho,
-        dist["r_edges"][0], dist["r_edges"][-1], dist["r_edges"].size,
-        dist["phi_edges"][0], dist["phi_edges"][-1], dist["phi_edges"].size,
-        dist["z_edges"][0], dist["z_edges"][-1], dist["z_edges"].size )
+    dist = copy.deepcopy(dist)
+
+    if vol is None or area is None:
+        # Find rho volume
+        vol,area = evalrhovol(
+            ascotpy, rhomin, rhomax, nrho,
+            dist["r_edges"][0], dist["r_edges"][-1], dist["r_edges"].size,
+            dist["phi_edges"][0], dist["phi_edges"][-1], dist["phi_edges"].size,
+            dist["z_edges"][0], dist["z_edges"][-1], dist["z_edges"].size )
 
     # Evaluate the requested quantity in 5D
     if quantity == "density":
         dist = distmod.squeeze(dist, vpar=0, vperp=0, time=0, charge=0)
+
+    if quantity == "energydensity":
+        emax = 0.5*ma*np.power(np.maximum( dist["vpar_edges"][-1],
+                                           dist["vperp_edges"][-1] ), 2) / const.e
+        dist = distconv.convert_vpavpe_to_Exi(dist, ma,
+                                              E_edges=np.linspace(0, emax,100),
+                                              xi_edges=np.linspace(-1,1,50))
+        dist = distmod.squeeze(dist, pitch=0, time=0, charge=0)
+
+        distE = copy.deepcopy(dist)
+        for ir in range(dist["r"].size):
+            for ip in range(dist["phi"].size):
+                for iz in range(dist["z"].size):
+                    distE["distribution"][ir, ip, iz, :] = dist["distribution"][ir,ip,iz,:] * dist["energy"] 
+
+        dist = distmod.squeeze(distE, energy=0)
+
+    if quantity == "toroidalcurrent":
+        dist = distmod.squeeze(dist, vperp=0, time=0, charge=0)
+
+        distj = copy.deepcopy(dist)
+
+        for ir in range(dist["r"].size):
+            for ip in range(dist["phi"].size):
+                for iz in range(dist["z"].size):
+                    bphi = ascotpy.evaluate(dist["r"][ir], dist["phi"][ip], dist["z"][iz], 0.0, "bphi")
+                    bnorm = ascotpy.evaluate(dist["r"][ir], dist["phi"][ip], dist["z"][iz], 0.0, "bnorm")
+                    for ivpar in range(dist["vpar"].size):
+                        distj["distribution"][ir, ip, iz, ivpar] = dist["distribution"][ir,ip,iz,ivpar] * dist["vpar"][ivpar] * qa * bphi / bnorm
+        dist = distmod.squeeze(distj, vpar=0)
 
     if quantity == "epowerdeposition":
         emax = 0.5*ma*np.power(np.maximum( dist["vpar_edges"][-1],
@@ -66,10 +99,12 @@ def eval1d(ascotpy, dist, quantity, rhomin, rhomax, nrho, ma=None, qa=None):
                      + (newdist["rho_edges"][1] - newdist["rho_edges"][0]) / 2
     newdist["nrho"] = nrho
     newdist["abscissae"] = ["rho"]
+    newdist["vol"] = vol.ravel()
+    newdist["area"] = area.ravel()
 
     newdist[quantity]  = np.histogram( rho.ravel(), newdist["rho_edges"],
                                        weights=dist.ravel() )[0]
-    newdist[quantity] /= vol.ravel()
+    newdist[quantity] /= newdist["vol"]
 
     return newdist
 
@@ -79,6 +114,7 @@ def evalrhovol(ascotpy, rhomin, rhomax, nrho, rmin, rmax, nr,
 
     # Volume of the test space (rectangular toroid)
     V_tot = 0.5 * (rmax**2 - rmin**2) * np.deg2rad(phimax - phimin) * (zmax - zmin)
+    A_tot = (rmax - rmin) * (zmax - zmin)
     drho = (rhomax - rhomin) / nrho
 
     points_in_bins = np.zeros((nrho, 1))
@@ -100,4 +136,4 @@ def evalrhovol(ascotpy, rhomin, rhomax, nrho, rmin, rmax, nr,
 
         n_tot = n_tot + n
 
-    return (points_in_bins/n_tot) * V_tot
+    return (points_in_bins/n_tot) * V_tot, (points_in_bins/n_tot) * A_tot
