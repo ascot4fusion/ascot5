42fb34d5df6db5b7b6bb07fc409d15c818f55101
Konsta Särkimäki <konsta.sarkimaki@chalmers.se>
2020-04-22 22:52:23 +0200

    Aesthetical edits

--- a5py/a5py/ascot5io/ascot5data.py
+++ a5py/a5py/ascot5io/ascot5data.py
@@ -19,11 +19,14 @@ class AscotData():
         self._path   = hdf5.name
         self._opened = None
 
+
     def __enter__(self):
         return self._open()
 
+
     def __exit__(self, exception_type, exception_value, traceback):
         self._close()
+
 
     def get_desc(self):
         f = self._open()
@@ -31,29 +34,32 @@ class AscotData():
         self._close()
         return val
 
+
     def get_date(self):
         f = self._open()
         val = ascot5file.get_date(f.file, self._group)
         self._close()
         return val
 
+
     def get_qid(self):
         return ascot5file.get_qid(self._group)
+
 
     def get_type(self):
         path = self._path.split("/")[-1]
         return path[:-11]
 
+
     def get_name(self):
         return self._group
-    
+
+
     def set_as_active(self):
-        '''
+        """
         Set the current group as active.
-        '''
+        """
         import a5py.ascot5io.ascot5tools as tools
-        
-        
         tools.call_ascot5file(self._file, "set_active", self._path)
 
 
@@ -63,6 +69,7 @@ class AscotData():
 
         self._opened = h5py.File(self._file, "a")[self._path]
         return self._opened
+
 
     def _close(self):
         self._opened.file.close()
