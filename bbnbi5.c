08578ae8fcd7dc59dd9c7a23733964007d6fa9b3
Jari Varje <jari.varje@aalto.fi>
2020-05-11 12:46:40 +0100

    Support for multiple injectors in bbnbi5

--- bbnbi5.c
+++ bbnbi5.c
@@ -73,6 +73,8 @@ int main(int argc, char** argv) {
     hdf5_nbi_read(f, &n_inj, &inj);
     hdf5_close(f);
 
+    real total_power = 0;
+
     for(int i=0; i < n_inj; i++) {
         printf("Injector %d:\n", i+1);
         printf("id: %d\n", inj[i].id);
@@ -85,11 +87,33 @@ int main(int argc, char** argv) {
         printf("znum: %d\n", inj[i].znum);
         printf("mass: %le\n", inj[i].mass);
         printf("\n");
+
+        total_power += inj[i].power;
     }
+
 
     /* Simulate requested number of markers into array of particle structs */
     particle* p = (particle*) malloc(nprt*sizeof(particle));
-    nbi_generate(nprt, p, &inj[0], &B_data, &plasma_data, &wall_data, &rng);
+    int nprt_generated = 0;
+
+    for(int i = 0; i < n_inj; i++) {
+        int nprt_inj;
+
+        if(i == n_inj-1) {
+            nprt_inj = nprt - nprt_generated;
+        }
+        else {
+            nprt_inj = inj[i].power * nprt;
+        }
+
+        nbi_generate(nprt_inj, &p[nprt_generated], &inj[i], &B_data,
+                     &plasma_data, &wall_data, &rng);
+
+        nprt_generated += nprt_inj;
+        printf("Generated %d markers for injector %d.\n", nprt_inj, i+1);
+    }
+
+    printf("\nWriting %d markers.\n", nprt_generated);
 
     /* Copy markers from particle structs into input_particle structs to be
      * written into the h5 file */
@@ -125,6 +149,8 @@ int main(int argc, char** argv) {
     hdf5_write_string_attribute(f, "/marker", "active",  qid);
 
     hdf5_close(of);
+
+    printf("\nDone.\n");
 
     return 0;
 }
