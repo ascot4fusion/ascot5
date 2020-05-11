c8d3018b016fec5fd7b3d3dd73bd5d265256fc13
Konsta Särkimäki <konsta.sarkimaki@chalmers.se>
2020-05-11 13:38:44 +0200

    Fixed guiding center EOm with MHD

--- simulate/step/step_gceom_mhd.h
+++ simulate/step/step_gceom_mhd.h
@@ -64,27 +64,27 @@ static void step_gceom_mhd(real* ydot, real* y, real mass, real charge,
         + y[0] * ydot[1] * gradalpha[1]
         +        ydot[2] * gradalpha[2];
 
+    real gradalphacrossB[3];
+    math_cross(gradalpha, B, gradalphacrossB);
+
     real Bstar[3];
-    Bstar[0] = B[0] + (y[3] / ( charge * normB ) + alpha)
-                      * curlB[0];
-    Bstar[1] = B[1] + (y[3] / ( charge * normB ) + alpha)
-                      * curlB[1];
-    Bstar[2] = B[2] + (y[3] / ( charge * normB ) + alpha)
-                      * curlB[2];
+    Bstar[0] = B[0] + gradalphacrossB[0] + alpha * curlB[0]
+        + ( y[3] / charge ) * ( curlB[0] / normB
+                                - gradBcrossB[0] / (normB*normB));
+    Bstar[1] = B[1] + gradalphacrossB[1] + alpha * curlB[1]
+        + ( y[3] / charge ) * ( curlB[1] / normB
+                                - gradBcrossB[0] / (normB*normB));
+    Bstar[2] = B[2] + gradalphacrossB[2] + alpha * curlB[2]
+        + ( y[3] / charge ) * ( curlB[2] / normB
+                                - gradBcrossB[2] / (normB*normB));
 
     real Estar[3];
-    Estar[0] = E[0] - mhd_dmhd[7]
-        + normB * y[3] * gradalpha[0] / ( gamma * mass )
-        - ( y[4] + y[3]*y[3] / ( mass * normB) ) * gradB[0]
-        / ( charge * gamma );
-    Estar[1] = E[0] - mhd_dmhd[8]
-        + normB * y[3] * gradalpha[1] / ( gamma * mass )
-        - ( y[4] + y[3]*y[3] / ( mass * normB) ) * gradB[1]
-        / ( charge * gamma );
-    Estar[2] = E[0] - mhd_dmhd[9]
-        + normB * y[3] * gradalpha[2] / ( gamma * mass )
-        - ( y[4] + y[3]*y[3] / ( mass * normB) ) * gradB[2]
-        / ( charge * gamma );
+    Estar[0] = E[0] - mhd_dmhd[7] - y[4] * gradB[0] / ( charge * gamma )
+        - B[0] * mhd_dmhd[1];
+    Estar[1] = E[1] - mhd_dmhd[8] - y[4] * gradB[1] / ( charge * gamma )
+        - B[1] * mhd_dmhd[1];
+    Estar[2] = E[2] - mhd_dmhd[9] - y[4] * gradB[2] / ( charge * gamma )
+        - B[2] * mhd_dmhd[1];
 
     real Bhat[3];
     Bhat[0] = B[0] / normB;
@@ -102,8 +102,7 @@ static void step_gceom_mhd(real* ydot, real* y, real mass, real charge,
                + EstarcrossBhat[1]) / (y[0]*BhatDotBstar);
     ydot[2] = (y[3] * Bstar[2] / ( gamma * mass )
                + EstarcrossBhat[2]) / BhatDotBstar;
-    ydot[3] = charge * math_dot(Bstar,Estar) / BhatDotBstar
-        - alphadot * charge * normB;
+    ydot[3] = charge * math_dot(Bstar,Estar) / BhatDotBstar;
     ydot[4] = 0;
     ydot[5] = charge * normB / (gamma*mass);
 
