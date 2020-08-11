074e2e4d4600edece62fa7d19b8ca318c71fcbf4
Konsta Särkimäki <konsta.sarkimaki@chalmers.se>
2020-08-11 14:17:53 +0200

    Removed some empty spaces

--- a5py/a5py/ascot5io/boozer.py
+++ a5py/a5py/ascot5io/boozer.py
@@ -179,7 +179,7 @@ def write_hdf5_dummy(fn, desc="Dummy"):
     psimin     = 0
     psimax     = 1
     npsi       = 6
-    nthetag    = 10 
+    nthetag    = 10
     ntheta     = 10
     rmin       = 0.1
     rmax       = 10.0
@@ -212,11 +212,11 @@ class Boozer(AscotData):
 
     def read(self):
         return read_hdf5(self._file, self.get_qid())
-    
+
     def write(self,fn,data = None, desc=None):
         if data is None:
             data = self.read()
-        
+
         if desc is None:
             return write_hdf5(fn=fn, **data)
         else:
