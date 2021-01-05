614e8f2e8021fd50e6b5eafdbc78a4efbabf56ef
Filippo <zontafil@gmail.com>
2021-01-05 20:55:08 +0200

    BMC: Fix wall check not working when the whole wall is the target

--- bmc/bmc_wall.c
+++ bmc/bmc_wall.c
@@ -29,7 +29,7 @@ int bmc_walltile_in_target(integer walltile) {
  **/
 int bmc_wall_2d_hit_target(real r0, real r1, real phi0, real phi1, real z0, real z1, wall_2d_data* w2d) {
     int tile = wall_2d_hit_wall(r0, phi0, z0, r1, phi1, z1, w2d);
-    if (tile >= 0) {
+    if (tile > 0) {
         return bmc_walltile_in_target(tile);
     }
     return 0;
