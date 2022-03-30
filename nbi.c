bc6d6bb50a611ee794c911769bfe04da256dabd8
sadig mulas <sadig.mulas@ciemat.es>
2022-03-30 12:17:59 +0300

    syntax error in last commit solved

--- nbi.c
+++ nbi.c
@@ -252,11 +252,11 @@ void nbi_generate(int nprt, particle* p, int* nprt_shined, particle* p_shined, n
 
     for(int i = 0; i < nprt; i++) {
         /*p[i].weight = n->power * (1 - totalShine/totalPower) / totalPower;*/
-        p[i].weight = n->power /(totalPower+totalShine) 
+        p[i].weight = n->power /(totalPower+totalShine); 
     }
 
     for(int i = 0; i < *nprt_shined; i++) {
         /*p_shined[i].weight = n->power * (1 - totalShine/totalPower) / totalPower;*/
-        p[i].weight = n->power /(totalPower+totalShine)
+        p[i].weight = n->power /(totalPower+totalShine);
     }
 }
