c55156bc731378c1d49271e09fe8ba9f3098b6ca
Simppa Akaslompolo <simppa.akaslompolo@aalto.fi>
2022-10-04 09:44:08 +0200

    Testing reading 3D-wall data from IMAS.

--- a5py/a5py/ascot5io/imas.py
+++ a5py/a5py/ascot5io/imas.py
@@ -1,4 +1,4 @@
-import imas
+import numpy as np
 
 class a5imas:
 
@@ -7,6 +7,9 @@ class a5imas:
         self.ids_name = "ids" #This should be overwritten by dervied classes
 
     def open(self, user, tokamak, version, shot, run, occurrence=0):
+
+        # Put this inside the function, not to disturb usage where imas is not available
+        import imas
 
         # ./idsdump.py g2jvarje test 3 92436 0272 distributions
         # user = "g2jvarje"
@@ -65,6 +68,10 @@ class wall_2d(a5imas):
 
         """
 
+        # Put this inside the function, not to disturb usage where imas is not available
+        import imas
+
+
         timeIndex = 0
         unit      = 0
 
@@ -83,3 +90,108 @@ class wall_2d(a5imas):
         }
 
         return w
+
+class wall_3d(a5imas):
+
+    def __init__(self):
+        super().__init__()
+        self.ids_name = "wall"
+
+
+    def read(self, user, tokamak, version, shot, run, occurrence=0 ):
+        """
+        Read an IMAS 3D-wall into a dictionary, that is a drop-in replacement for a dict read from hdf5.
+
+        The data is read from
+              wall.description_ggd[0].grid_ggd[0].grid_ggd.space[0]
+        Flag is set to the index of description_ggd == 0
+
+        """
+
+        itm = self.open( user, tokamak, version, shot, run, occurrence )
+
+        description_ggd_index = 0
+        grid_ggd_index        = 0
+        space_index           = 0
+        
+        
+        desc_ggd = self.ids.wall.description_ggd[description_ggd_index]
+        grid_ggd = desc_ggd.grid_ggd[grid_ggd_index]
+        space    = grid_ggd.space[space_index]
+        
+        if not np.all( space.coordinates_type == np.array([1, 2, 3]) ):
+            raise ValueError("Space coordinates not [1,2,3] in IMAS (3d)wall ids u:{} db:{} v:{} s:{} r:{} o:{} desc:{} grid:{} sp:{}".format(
+                user,tokamak,version,shot,run,occurrence,description_ggd_index,grid_ggd_index,space_index ))
+
+
+        node_coordinates,edge_indexes,tria_indexes = self.get_nodes_edges_polygons(space)
+
+        self.close()
+
+
+        # Convert 3D wall data to ASCOT5 format
+                             
+        xyz = self.get_xyz(node_coordinates,tria_indexes)
+        
+
+        nelements = xyz.shape[0]
+        flag      = np.ones( (nelements,1) ) * description_ggd_index
+        w = {
+            "x1x2x3"         : xyz[:,:,0],
+            "y1y2y3"         : xyz[:,:,1],
+            "z1z2z3"         : xyz[:,:,2],
+            "nelements"      : nelements,
+            "flag"           : flag,
+            "flagIdList"     : flag,
+            "flagIdStrings"  : ["IMAS 3D-wall ids u:{} db:{} v:{} s:{} r:{} o:{} desc:{} grid:{} sp:{}".format(
+                user,tokamak,version,shot,run,occurrence,description_ggd_index,grid_ggd_index,space_index )],
+
+        }
+
+        return w
+
+
+    def get_nodes_edges_polygons(self,space):
+        '''
+        Copy the node coordinates, edge indexes and polygons from the space to separae numpy arrays:
+
+        returns node_coordinates[3,n_nodes], edge_indexes[2,n_edges],tria_indexes[3,n_tris]
+
+        '''
+
+
+        n_nodes=len(space.objects_per_dimension[0].object)
+        n_edges=len(space.objects_per_dimension[1].object)
+        n_tris =len(space.objects_per_dimension[2].object)
+        node_coordinates = np.zeros( (3,n_nodes),dtype=float )
+        edge_indexes     = np.zeros( (2,n_edges),dtype=int   )
+        tria_indexes     = np.zeros( (3,n_tris), dtype=int   )
+
+        for i in range(n_nodes):
+            node_coordinates[:,i] = space.objects_per_dimension[0].object[i].geometry
+
+        for i in range(n_edges):
+            edge_indexes[:,i] = space.objects_per_dimension[1].object[i].nodes
+
+        for i in range(n_tris):
+            tria_indexes[:,i] = space.objects_per_dimension[2].object[i].nodes
+
+        return node_coordinates,edge_indexes,tria_indexes
+
+    def get_xyz(self,node_coordinates,tria_indexes):
+        '''
+        Parse out triangle corner coordinates. Assume one based indexing in the input (first = 1).
+
+        Returns  xyz[ntri,3,3], where the last dimension [ntri,ncorn,:]  is x,y,z. 
+
+
+        '''
+        ntri =  tria_indexes.shape[1]
+
+        xyz = np.zeros( (ntri, 3, 3 ),dtype=float )
+
+        for itri in range( ntri ):
+            for i in range(3):
+                xyz[itri,i,:] = node_coordinates[:,tria_indexes[i,itri]-1]
+
+        return xyz
--- a5py/a5py/examples/read_from_imas.py
+++ a5py/a5py/examples/read_from_imas.py
@@ -9,6 +9,18 @@
 
 import a5py.ascot5io.imas
 import a5py.ascot5io.wall_2D
+import a5py.ascot5io.wall_3D
 w2d=a5py.ascot5io.imas.wall_2d()
-wdict=w2d.read("g2jvarje","test","3",92436,272)
+w3d=a5py.ascot5io.imas.wall_3d()
+# Gateway
+# wdict=w2d.read("g2jvarje","test","3",92436,272)
+
+# ITER SDCC
+wdict3=w3d.read("akaslos","mywall","3",11,11)
+
+wdict=w2d.read("akaslos","test","3",92436,272)
+
+a5py.ascot5io.wall_3D.write_hdf5('from_imas.h5',**wdict3)
 a5py.ascot5io.wall_2D.write_hdf5('from_imas.h5',**wdict)
+
+
