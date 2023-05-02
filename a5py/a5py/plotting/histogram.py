408ac3a1938053c3617645d21bd177f3025e2c15
Konsta Sarkimaki <sarkimk1@login3.triton.aalto.fi>
2023-05-02 14:56:27 +0300

    Updated plotting routines

---
+++ a5py/a5py/plotting/histogram.py
@@ -0,0 +1,109 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+from .helpers import openfigureifnoaxes
+
+@openfigureifnoaxes(projection=None)
+def hist1d(x, xbins=None, weights=None, log=[False, False], xlabel=None,
+           legend=None, axes=None):
+    """
+    Plot (stacked) histogram in 1D.
+
+    Args:
+        x : array_like <br>
+            Array or list of arrays of data to be binned and plotted.
+        xbins : int or array_like, optional <br>
+            Number of bins or array storing bin edges for the x coordinate.
+        weights : array_like, optional <br>
+            Values the datapoints are weighted with. Same format as for x.
+        log : [bool, bool], optional <br>
+            Make [x-axis, y-axis] logarithmic.
+        xlabel : str, optional <br>
+            Label for the x-axis.
+        legend : str, array_like <br>
+            List of strings to label the data with. The length of the list must
+            be same as the number of data arrays provided.
+        axes : Axes, optional <br>
+            The Axes object to draw on.
+    """
+
+    # Set log scales
+    if log[0]: x = [np.log10(x) for x in x]
+    if log[0] and ( xbins is not None and not isinstance(xbins, int) ):
+        xbins = np.log10(xbins)
+
+    if not log[1]: axes.ticklabel_format(style="sci", axis="y", scilimits=(0,0))
+
+    # Plot and legend
+    axes.hist(x, xbins, density=False, stacked=True, log=log[1],
+              weights=weights, rwidth=1)
+    axes.legend(legend, frameon=False)
+
+    # Set labels
+    if log[0] and xlabel is not None: xlabel = "log10( "+xlabel+" )"
+    axes.set_xlabel(xlabel)
+
+    ylabel = "Markers per bin" if weights is None else "Particles per bin"
+    axes.set_ylabel(ylabel)
+
+
+@openfigureifnoaxes(projection=None)
+def hist2d(x, y, xbins=None, ybins=None, weights=None,
+           log=[False, False, False], xlabel=None, ylabel=None, axesequal=False,
+           axes=None, cax=None):
+    """
+    Plot histogram in 2D.
+
+    Args:
+        x : array_like <br>
+            x-coordinates of the data to be binned and plotted.
+        y : array_like <br>
+            y-coordinates of the data to be binned and plotted.
+        xbins : int or array_like, optional <br>
+            Number of bins or array storing bin edges for the x coordinate.
+        ybins : int or array_like, optional <br>
+            Number of bins or array storing bin edges for the y coordinate.
+        weights : array_like, optional <br>
+            Values the datapoints are weighted with. Same size as x and y.
+        log : [bool, bool, bool], optional <br>
+            Make [x-axis, y-axis, color axis] logarithmic.
+        xlabel : str, optional <br>
+            Label for the x-axis.
+        ylabel : str, optional <br>
+            Label for the y-axis.
+        axesequal : bool, optional <br>
+            Flag to set aspect ratio of [x,y] axes equal.
+        axes : Axes, optional <br>
+            The Axes object to draw on.
+        cax : Axes, optional <br>
+            The Axes object for the color bar, otherwise taken from axes.
+    """
+    # Set log scales
+    if log[0]: x = [np.log10(np.abs(x)) for x in x]
+    if log[0] and ( xbins is not None and not isinstance(xbins, int) ):
+        xbins = np.log10(xbins)
+
+    if log[1]: y = [np.log10(np.abs(y)) for y in y]
+    if log[1] and ( ybins is not None and not isinstance(ybins, int) ):
+        ybins = np.log10(ybins)
+
+    # Plot and colorbar
+    h,_,_,m = axes.hist2d(x, y, bins=[xbins, ybins], weights=weights)
+
+    norm = None
+    if log[2]:
+        norm = mpl.colors.LogNorm(np.amin(h), np.amax(h))
+    cbar = plt.colorbar(m, norm=norm, ax=axes, cax=cax)
+
+    # Set labels
+    if log[0] and xlabel is not None: xlabel = "log10( "+xlabel+" )"
+    axes.set_xlabel(xlabel)
+    if log[1] and ylabel is not None: ylabel = "log10( "+ylabel+" )"
+    axes.set_ylabel(ylabel)
+
+    clabel = "Markers per bin" if weights is None else "Particles per bin"
+    cbar.set_label(clabel)
+
+    if axesequal:
+        axes.set_aspect("equal", adjustable="box")
---
+++ a5py/a5py/plotting/wall3d.py
@@ -0,0 +1,202 @@
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pyvista as pv
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from .helpers import openfigureifnoaxes
+
+@openfigureifnoaxes(projection=None)
+def still(wallmesh, points=None, data=None, log=False, cpos=None, cfoc=None,
+          cang=None, axes=None, cax=None):
+    """
+    Take a still shot of the mesh and display it using matplotlib backend.
+
+    The rendering is done using vtk but the vtk (interactive) window is not
+    displayed. It is recommended to use the interactive plot to find desired
+    camera position and produce the actual plot using this method. The plot
+    is shown using imshow and acts as a regular matplotlib plot.
+
+    Args:
+        wallmesh : Polydata <br>
+            Mesh representing the wall.
+        points : array_like, optional <br>
+            Array Npoint x 3 defining points (markers) to be shown. For
+            each point [x, y, z] coordinates are given.
+        cpos : array_like, optional <br>
+            Camera position coordinates [x, y, z].
+        cfoc : array_like, optional <br>
+            Camera focal point coordinates [x, y, z].
+        cang : array_like, optional <br>
+            Camera angle [azimuth, elevation, roll].
+        axes : Axes, optional <br>
+            The Axes object to draw on.
+        cax : Axes, optional <br>
+            The Axes object for the color data (if c contains data), otherwise
+            taken from axes.
+    """
+    p = pv.Plotter(off_screen=True)
+    if data is None:
+        p.add_mesh(wallmesh, color=[0.9,0.9,0.9])
+    else:
+        cmap = mpl.colormaps["Reds"].copy()
+        cmap.set_bad(color=[0.9,0.9,0.9])
+        p.add_mesh(wallmesh, scalars=data, cmap=cmap, log_scale=log)
+        p.remove_scalar_bar()
+
+        maxval = np.nanmax(wallmesh.cell_data[data])
+        minval = np.nanmin(wallmesh.cell_data[data])
+
+    if points is not None:
+        p.theme.color = 'black'
+        p.add_points(points, render_points_as_spheres=True, point_size=10)
+
+    # Set camera
+    if cpos is not None:
+        p.camera.position = cpos
+    if cfoc is not None:
+        p.camera.focal_point = cfoc
+    if cang is not None:
+        p.camera.azimuth   = cang[0]
+        p.camera.elevation = cang[1]
+        p.camera.roll      = cang[2]
+
+    p.show()
+    axes.imshow(p.image)
+    axes.set_xticks([])
+    axes.set_yticks([])
+
+    if data is not None:
+        if log:
+            norm = mpl.colors.LogNorm(vmin=minval, vmax=maxval)
+        else:
+            norm = mpl.colors.Normalize(vmin=0, vmax=maxval)
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        cbar = plt.colorbar(sm, ax=axes, cax=cax)
+        #cbar.set_label(r"Energy load J/m$^2$")
+
+
+def interactive(wallmesh, *args, points=None, data=None, log=False, cpos=None,
+                cfoc=None, cang=None):
+    """
+    Open vtk window to display interactive view of the wall mesh.
+
+    Args:
+        wallmesh : Polydata <br>
+            Mesh representing the wall.
+        *args : tuple (str, method), optional <br>
+            Key (str) method pairs. When key is pressed when the plot is
+            displayed, the associated method is called. The method should take
+            Plotter instance as an argument.
+        points : array_like, optional <br>
+            Array Npoint x 3 defining points (markers) to be shown. For
+            each point [x, y, z] coordinates are given.
+        cpos : array_like, optional <br>
+            Camera position coordinates [x, y, z].
+        cfoc : array_like, optional <br>
+            Camera focal point coordinates [x, y, z].
+        cang : array_like, optional <br>
+            Camera angle [azimuth, elevation, roll].
+    """
+    p = pv.Plotter()
+
+    if data is None:
+        p.add_mesh(wallmesh, color=[0.9,0.9,0.9], log_scale=log)
+    else:
+        cmap = mpl.colormaps["Reds"].copy()
+        cmap.set_bad(color=[0.9,0.9,0.9])
+        p.add_mesh(wallmesh, scalars=data, cmap=cmap, log_scale=log)
+
+    if points is not None:
+        p.theme.color = 'black'
+        p.add_points(points, render_points_as_spheres=True, point_size=10)
+
+    # Set events
+    for i in range(len(args)):
+        def wrapper(*wargs):
+            args[i][1](p)
+
+        p.add_key_event(args[i][0], wrapper)
+
+    # Set camera
+    if cpos is not None:
+        p.camera.position = cpos
+    if cfoc is not None:
+        p.camera.focal_point = cfoc
+    if cang is not None:
+        p.camera.azimuth   = cang[0]
+        p.camera.elevation = cang[1]
+        p.camera.roll      = cang[2]
+
+    p.show()
+
+
+@openfigureifnoaxes(projection=None)
+def loadvsarea(wetted, loads, axes=None):
+    """
+    """
+    idx = np.argsort(-loads)
+    wetted = np.cumsum(wetted[idx])
+    loads  = loads[idx]
+
+    axes.set_xscale('log')
+    axes.set_yscale('linear')
+    #axes.set_ylim((1e4, 2e8))
+    #ax.set_xlim((1,14))
+    #ax.spines['left'].set_visible(False)
+    #ax.yaxis.set_ticks_position('right')
+    #ax.yaxis.set_visible(False)
+
+    #divider = make_axes_locatable(axes)
+    #axes1 = divider.append_axes("left", size=1.8, pad=0, sharex=axes)
+    #axes1.set_yscale('log')
+    #ax1.set_xscale('linear')
+    #ax1.set_xlim((1e-4, 9.99e-1))
+    #ax1.spines['right'].set_visible(False)
+    #ax1.yaxis.set_ticks_position('left')
+    #plt.setp(ax1.get_xticklabels(), visible=True)
+    axes.plot(loads, wetted)
+    axes.set_xlabel(r"Wet area [m$^2$]")
+    axes.set_ylabel(r"Wet area [m$^2$]")
+
+
+def defaultcamera(wallmesh):
+    """
+    Get default camera (helper function for the 3D plots).
+
+    Default camera is located at R = (Rmax + Rmin) / 2, phi = 0,
+    z = (zmax, zmin) / 2, where the min/max values are taken from the bounding
+    box of the wall mesh. The focal point is at same (R,z) but phi = 10. The
+    camera angle is zero in all parameters.
+
+    Args:
+        wallmesh : Polydata <br>
+            Mesh representing the wall.
+    Returns:
+        cpos : array_like, optional <br>
+            Camera position coordinates [x, y, z].
+        cfoc : array_like, optional <br>
+            Camera focal point coordinates [x, y, z].
+        cang : array_like, optional <br>
+            Camera angle [azimuth, elevation, roll].
+    """
+    # Find min/max values
+    Rmax = np.sqrt( np.amax(wallmesh.points[:,0]**2 + wallmesh.points[:,1]**2) )
+    Rmin = np.sqrt( np.amin(wallmesh.points[:,0]**2 + wallmesh.points[:,1]**2) )
+    zmax = np.amax( wallmesh.points[:,2] )
+    zmin = np.amin( wallmesh.points[:,2] )
+
+    # Camera position in (R,phi,z)
+    cpos = np.array([( Rmax + Rmin ) / 2,  0, ( zmax + zmin ) / 2])
+    cfoc = np.array([( Rmax + Rmin ) / 2, 10, ( zmax + zmin ) / 2])
+    cang = np.array([0, 0, 0])
+
+    # Coordinate transformation (R,phi,z) -> (x,y,z)
+    cpos = np.array([cpos[0] * np.cos( np.pi * cpos[1] / 180 ),
+                     cpos[0] * np.sin( np.pi * cpos[1] / 180 ),
+                     cpos[2]])
+    cfoc = np.array([cfoc[0] * np.cos( np.pi * cfoc[1] / 180 ),
+                     cfoc[0] * np.sin( np.pi * cfoc[1] / 180 ),
+                     cfoc[2]])
+
+    return (cpos, cfoc, cang)
