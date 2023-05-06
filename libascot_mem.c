c67a3f03ccb1674b7e7b97d072a28e9f9e3bde81
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-05-06 16:18:37 +0300

    Minor cleaning

--- libascot_mem.c
+++ libascot_mem.c
@@ -1,26 +1,40 @@
+/**
+ * @file libascot_mem.c
+ * @brief Provides memory de/allocation routines for the libascot
+ */
 #include "libascot_mem.h"
 #include <stdlib.h>
 
-/**
- *  @brief A routine to allocate an array of particles
- *
- */
-input_particle* libascot_allocate_input_particles(int number_of_particles){
-	return (input_particle*) malloc(number_of_particles * sizeof(input_particle) );
-}
 
 /**
- *  @brief A routine to allocate an array of reals
+ * @brief A routine to allocate an array of particles
  *
+ * @param number_of_particles number of markers for which space is allocated
+ *
+ * @return pointer to the allocated array
  */
-real* libascot_allocate_reals(size_t size){
-  return ( real* ) malloc( size * sizeof(real) );
+input_particle* libascot_allocate_input_particles(int number_of_particles) {
+    return (input_particle*) malloc(number_of_particles * sizeof(input_particle) );
 }
 
+
 /**
- *  @brief A wrapper to C stdlib free()
+ * @brief A routine to allocate an array of reals
  *
+ * @param size size of the array
+ *
+ * @return pointer to the allocated array
  */
-void libascot_deallocate(void *arr){
-  free( arr );
+real* libascot_allocate_reals(size_t size) {
+    return ( real* ) malloc( size * sizeof(real) );
+}
+
+
+/**
+ * @brief A wrapper to C stdlib free()
+ *
+ * @param arr array to be freed
+ */
+void libascot_deallocate(void *arr) {
+    free(arr);
 }
--- libascot_mem.h
+++ libascot_mem.h
@@ -1,18 +1,19 @@
-/*
- * libascot_mem.h
+/**
+ * @file libascot_mem.h
+ * @brief Header file for libascot_mem.c
  *
  *  Created on: Nov 25, 2021
  *      Author: sjjamsa
  */
-
 #ifndef LIBASCOT_MEM_H_
 #define LIBASCOT_MEM_H_
 
 #include "particle.h"
 
 input_particle* libascot_allocate_input_particles(int number_of_particles);
+
 real* libascot_allocate_reals(size_t size);
+
 void libascot_deallocate(void *arr);
 
-
-#endif /* LIBASCOT_MEM_H_ */
+#endif
