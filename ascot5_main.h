471dd952147dabfe48ff6e4643cb2960f323e809
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-05-06 15:19:12 +0300

    Reorganizing ascot5_main

--- ascot5_main.h
+++ ascot5_main.h
@@ -13,40 +13,35 @@
 #include "offload.h"
 #include "simulate.h"
 
-int prepare_offload(
-    sim_offload_data* sim, int mpi_size, int mpi_rank, int n_tot,
-    offload_package* offload_data, real** B_offload_array,
-    real** E_offload_array, real** plasma_offload_array,
+int pack_offload_array(
+    sim_offload_data* sim, offload_package* offload_data,
+    real** B_offload_array, real** E_offload_array, real** plasma_offload_array,
     real** neutral_offload_array, real** wall_offload_array,
     int** wall_int_offload_array, real** boozer_offload_array,
-    real** mhd_offload_array, input_particle** p, particle_state** ps,
-    int* nprts, real** offload_array, int** int_offload_array,
-    real** diag_offload_array);
+    real** mhd_offload_array, real** offload_array, int** int_offload_array);
+
+int prepare_markers(
+    sim_offload_data* sim, int mpi_size, int mpi_rank, int n_tot,
+    input_particle** pin, particle_state** pout, int* nprts,
+    real* B_offload_array);
 
 int write_rungroup(
     sim_offload_data* sim, int mpi_size, int mpi_rank, int mpi_root,
     int n_tot, particle_state* ps, char* qid);
 
 int offload_and_simulate(
-    sim_offload_data* sim, int mpi_rank, int nprts, particle_state* ps,
-    offload_package* offload_data, real* offload_array,
-    int* int_offload_array, real* diag_offload_array);
-
-int gather_output(
-    sim_offload_data* sim, int mpi_size, int mpi_rank, int mpi_root,
-    int n_tot, particle_state* ps, particle_state** ps_gathered,
-    real* diag_offload_array);
+    sim_offload_data* sim, int mpi_size, int mpi_rank, int mpi_root, int n_tot,
+    int nprts, particle_state* pin, offload_package* offload_data,
+    real* offload_array, int* int_offload_array, particle_state** pout,
+    real** diag_offload_array);
 
 int write_output(
     sim_offload_data* sim, int mpi_rank, int mpi_root,
     particle_state* ps_gathered, int n_tot,
     real* diag_offload_array);
 
-int free_simulation_data(
-    sim_offload_data* sim, particle_state** ps, offload_package* offload_data,
-    real** offload_array, int** int_offload_array, real** diag_offload_array);
-
-int free_particlestate(particle_state* ps);
+int free_simulation_output(
+    sim_offload_data* sim, particle_state **ps, real** diag_offload_array);
 
 void print_marker_summary(particle_state* ps, int n_tot);
 
