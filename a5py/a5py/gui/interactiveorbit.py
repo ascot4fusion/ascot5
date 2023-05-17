f07eacde27298f29362a91f9c06e72253b7c918c
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-05-17 14:54:34 +0300

    Marker simulation in GUI

--- a5py/a5py/gui/interactiveorbit.py
+++ a5py/a5py/gui/interactiveorbit.py
@@ -2,37 +2,97 @@ import tkinter as tk
 from tkinter import ttk
 
 from .components import PlotFrame, NumEntry
-from .livelink import initandrun, clean
+
+from a5py.ascot5io import options
+from a5py.ascot5io import mrk as marker
 
 class OrbitFrame(ttk.Frame):
 
     def init(self, canvas):
         f1 = ttk.Frame(self)
-        f1.pack()
-        r_entry = NumEntry(f1, labeltext="R [m] = ", entrywidth=5,
-                           labelwidth=8, anchor="w", defval=6.7)
-        r_entry.pack()
-        plotbutton = tk.Button(f1, text="Run and plot", width=8)
-        plotbutton.pack()
+        f2 = ttk.Frame(self)
+        f1.pack(fill="x")
+        f2.pack(fill="both")
+
+        f1.columnconfigure(1, weight=1)
+
+        self.milent = NumEntry(f1, labeltext="Mileage [s]", entrywidth=10,
+                               labelwidth=12, anchor="w", defval=0.0001)
+        self.pntent = NumEntry(f1, labeltext="N points", entrywidth=10,
+                               labelwidth=12, anchor="w", defval=1000,
+                               isint=True)
+
+        self.milent.grid(column=0, row=0)
+        self.pntent.grid(column=0, row=1)
+
+        self.plotbutton = tk.Button(f1, text="Run and plot", width=8)
+        self.plotbutton.grid(column=1, row=0, rowspan=2)
+
+        self.r_entry = NumEntry(f2, labeltext="R [m]", entrywidth=5,
+                                labelwidth=12, anchor="w", defval=6.7)
+        self.p_entry = NumEntry(f2, labeltext="phi [deg]", entrywidth=5,
+                                labelwidth=12, anchor="w", defval=0.0)
+        self.z_entry = NumEntry(f2, labeltext="z [m]", entrywidth=5,
+                                labelwidth=12, anchor="w", defval=0.0)
+        self.e_entry = NumEntry(f2, labeltext="Energy [eV]", entrywidth=10,
+                                labelwidth=14, anchor="w", defval=10.0e6)
+        self.k_entry = NumEntry(f2, labeltext="Pitch [vpa/v]", entrywidth=10,
+                                labelwidth=14, anchor="w", defval=0.99)
+        self.r_entry.grid(column=0, row=0)
+        self.p_entry.grid(column=0, row=1)
+        self.z_entry.grid(column=0, row=2)
+        self.e_entry.grid(column=1, row=0)
+        self.k_entry.grid(column=1, row=1)
 
         self.canvas     = canvas
-        self.r_entry    = r_entry
-        self.plotbutton = plotbutton
 
-        self.first = True
 
     def display(self, gui):
 
+        gui.ascot.simulation_initinput()
+
         def plot():
-            r = self.r_entry.getval()
-            M = initandrun(gui.ascot.h5fn, r, self.first)
-            if self.first:
-                self.first = False
+            r      = self.r_entry.getval()
+            phi    = self.p_entry.getval()
+            z      = self.z_entry.getval()
+            energy = self.e_entry.getval()
+            pitch  = self.k_entry.getval()
+
+            mrk = marker.generatemrk(nmrk=1, mrktype="gc", species="electron")
+            mrk["r"][:]      = r
+            mrk["phi"][:]    = phi
+            mrk["z"][:]      = z
+            mrk["energy"][:] = energy
+            mrk["pitch"][:]  = pitch
+
+            mil = 1e-5
+            npoint = 1000
+            interval = mil/npoint
+
+            ids = mrk["ids"]
+
+            opt = options.generateopt(clean=True)
+            opt.update({"SIM_MODE"               : 2,
+                        "ENABLE_ADAPTIVE"        : 1,
+                        "ENABLE_ORBIT_FOLLOWING" : 1,
+                        "ENDCOND_SIMTIMELIM"     : 1,
+                        "ENDCOND_MAX_SIMTIME"    : 100,
+                        "ENDCOND_MAX_MILEAGE"    : mil,
+                        "ENABLE_ORBITWRITE"      : 1,
+                        "ORBITWRITE_MODE"        : 1,
+                        "ORBITWRITE_NPOINT"      : npoint,
+                        "ORBITWRITE_INTERVAL"    : interval
+            })
+
+            gui.ascot.simulation_initmarkers("gc", mrk)
+            gui.ascot.simulation_initoptions(opt)
+            gui.ascot.simulation_run()
+            rorb = gui.ascot.simulation_getorbit("r", ids)
+            zorb = gui.ascot.simulation_getorbit("z", ids)
+            gui.ascot.simulation_freeoutput()
 
             self.canvas.fig_rzview.clear()
-            idx=1;
-            self.canvas.fig_rzview.axis.plot(M.get_orbit(idx,"r"), M.get_orbit(idx,"z"))
-            clean(M)
+            self.canvas.fig_rzview.axis.plot(rorb, zorb)
             self.canvas.fig_rzview.draw()
 
         self.plotbutton.configure(command=plot)
