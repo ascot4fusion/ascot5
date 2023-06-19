9911435ee4a0443ef1c59d390c40d09af3d4c68a
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-06-19 22:32:48 +0300

    Made some atomic routines private

--- simulate/atomic.c
+++ simulate/atomic.c
@@ -24,6 +24,17 @@
 #include "../asigma.h"
 #include "atomic.h"
 
+#pragma omp declare target
+#pragma omp declare simd uniform(asgm_data)
+a5err atomic_rates(
+    real* rate_eff_ion, real* rate_eff_rec, int z_1, int a_1, real m_1,
+    const int* z_2, const int* a_2, const real* m_2, asigma_data* asgm_data,
+    int q, real E, int N_pls_spec, real* T, real T_0, real* n, real n_0);
+#pragma omp declare simd
+a5err atomic_react(
+    int* q, real dt, real rate_eff_ion, real rate_eff_rec, int z_1, real rnd);
+#pragma omp end declare target
+
 /**
  * @brief Determine if atomic reactions occur during one time-step
  *
@@ -40,7 +51,7 @@
  */
 void atomic_fo(particle_simd_fo* p, real* h,
                plasma_data* p_data, neutral_data* n_data,
-               random_data* r_data, asigma_data* asigma_data) {
+               random_data* r_data, asigma_data* asigmadata) {
 
     /* Generate random numbers and get plasma information before going to the *
      * SIMD loop                                                              */
--- simulate/atomic.h
+++ simulate/atomic.h
@@ -1,8 +1,6 @@
 /**
  * @file atomic.h
  * @brief Header file for atomic.c
- *
- * @todo If possible, define more parameters as uniform!
  */
 #ifndef ATOMIC_H
 #define ATOMIC_H
@@ -17,20 +15,7 @@
 #pragma omp declare target
 void atomic_fo(particle_simd_fo* p, real* h,
                plasma_data* p_data, neutral_data* n_data,
-               random_data* r_data, asigma_data* asgm_data);
-#pragma omp declare simd uniform(asgm_data)
-a5err atomic_rates(real* rate_eff_ion, real* rate_eff_rec,
-                   int z_1, int a_1, real m_1,
-                   const int* z_2, const int* a_2, const real* m_2,
-                   asigma_data* asgm_data,
-                   int q, real E,
-                   int N_pls_spec,
-                   real* T, real T_0,
-                   real* n, real n_0);
-#pragma omp declare simd
-a5err atomic_react(int* q, real dt,
-                   real rate_eff_ion, real rate_eff_rec,
-                   int z_1, real rnd);
+               random_data* r_data, asigma_data* asigma_data);
 #pragma omp end declare target
 
 #endif
