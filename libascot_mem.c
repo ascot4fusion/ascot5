21d3049a3276edc985822211b91c70caa6265743
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-06-26 14:19:12 +0300

    Function to libascot to initialize marker state

--- libascot_mem.c
+++ libascot_mem.c
@@ -2,21 +2,31 @@
  * @file libascot_mem.c
  * @brief Provides memory de/allocation routines for the libascot
  */
-#include "libascot_mem.h"
 #include <stdlib.h>
-
+#include "libascot_mem.h"
+#include "particle.h"
 
 /**
- * @brief A routine to allocate an array of particles
+ * @brief A routine to allocate an array of input particles
  *
- * @param number_of_particles number of markers for which space is allocated
+ * @param nmrk number of markers for which space is allocated
  *
  * @return pointer to the allocated array
  */
-input_particle* libascot_allocate_input_particles(int number_of_particles) {
-    return (input_particle*) malloc(number_of_particles * sizeof(input_particle) );
+input_particle* libascot_allocate_input_particles(int nmrk) {
+    return (input_particle*) malloc(nmrk * sizeof(input_particle) );
 }
 
+/**
+ * @brief A routine to allocate an array of particle states
+ *
+ * @param nmrk number of markers for which space is allocated
+ *
+ * @return pointer to the allocated array
+ */
+particle_state* libascot_allocate_particle_states(int nmrk) {
+    return (particle_state*) malloc(nmrk * sizeof(particle_state) );
+}
 
 /**
  * @brief A routine to allocate an array of reals
--- libascot_mem.h
+++ libascot_mem.h
@@ -1,17 +1,14 @@
 /**
  * @file libascot_mem.h
  * @brief Header file for libascot_mem.c
- *
- *  Created on: Nov 25, 2021
- *      Author: sjjamsa
  */
 #ifndef LIBASCOT_MEM_H_
 #define LIBASCOT_MEM_H_
 
 #include "particle.h"
 
-input_particle* libascot_allocate_input_particles(int number_of_particles);
-
+input_particle* libascot_allocate_input_particles(int nmrk);
+particle_state* libascot_allocate_particle_states(int nmrk);
 real* libascot_allocate_reals(size_t size);
 
 void libascot_deallocate(void *arr);
