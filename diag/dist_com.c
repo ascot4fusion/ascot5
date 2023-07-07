29ca07391363e875c6daf191fddb0ce429bd09e0
Pitkäranta Mari <pitkarm2@d22-0142.org.aalto.fi>
2023-07-07 17:00:53 +0300

    COM chagnes

--- diag/dist_com.c
+++ diag/dist_com.c
@@ -64,6 +64,81 @@ void dist_COM_init(dist_COM_data* dist_data,
     dist_data->histogram = &offload_array[0];
 }
 
+/** 
+ * @brief Update the histogram from full-orbit markers
+ */
+void dist_COM_update_fo(dist_COM_data* dist, B_field_data* Bdata,
+                        particle_simd_fo* p_f, particle_simd_fo* p_i) {
+    real Ekin;
+    real Ptor;
+    real Bnorm;
+    real psi;
+    real mu;
+    real xi;
+    real pnorm;
+    real ppar;
+
+    int i_mu[NSIMD];
+    int i_Ekin[NSIMD];
+    int i_Ptor[NSIMD];
+
+    int ok[NSIMD];
+    real weight[NSIMD];
+
+    #pragma omp simd
+    for(int i = 0; i < NSIMD; i++) {
+        if(p_f->running[i]) {
+
+            B_field_eval_psi(&psi, p_f->r[i], p_f->phi[i], p_f->z[i],
+                             p_f->time[i], Bdata);
+
+	    real B [] = {p_f->B_r[i], p_f->B_phi[i], p_f->B_z[i]};
+	    Bnorm = math_normc(p_f->B_r[i], p_f->B_phi[i], p_f->B_z[i]);
+
+	    real p [] = {p_f->p_r[i], p_f->p_phi[i], p_f->p_z[i]};
+	    pnorm = math_normc(p_f->p_r[i], p_f->p_phi[i], p_f->p_z[i]);
+	    
+	    ppar = math_dot(p, B) / Bnorm;
+	    
+	    xi = ppar / pnorm;
+	    
+	    mu = physlib_gc_mu(p_f->mass[i], pnorm, xi, Bnorm);
+	    
+            i_mu[i] = floor((mu - dist->min_mu)
+                            / ((dist->max_mu - dist->min_mu)/dist->n_mu));
+            Ekin = physlib_Ekin_pnorm(p_f->mass[i], pnorm);
+
+            i_Ekin[i] = floor((Ekin - dist->min_Ekin)
+                              /  ((dist->max_Ekin - dist->min_Ekin) / dist->n_Ekin));
+            Ptor = phys_ptoroid_fo(p_f->charge[i], p_f->r[i], p_f->p_phi[i], psi);
+
+            i_Ptor[i] = floor((Ptor - dist->min_Ptor)
+                              /  ((dist->max_Ptor - dist->min_Ptor)/dist->n_Ptor));
+	    
+            if(i_mu[i]   >= 0 && i_mu[i]   <= dist->n_mu - 1   &&
+               i_Ekin[i] >= 0 && i_Ekin[i] <= dist->n_Ekin - 1 &&
+               i_Ptor[i] >= 0 && i_Ptor[i] <= dist->n_Ptor - 1 ) {
+                ok[i] = 1;
+                weight[i] = p_f->weight[i] * (p_f->time[i] - p_i->time[i]);
+            }
+            else {
+                ok[i] = 0;
+            }
+        }
+    }
+
+    for(int i = 0; i < NSIMD; i++) {
+        if(p_f->running[i] && ok[i]) {
+            unsigned long index = dist_COM_index(i_mu[i], i_Ekin[i], i_Ptor[i],
+                                                dist->n_mu,  dist->n_Ekin,
+                                                dist->n_Ptor);
+
+            #pragma omp atomic
+            dist->histogram[index] += weight[i];
+        }
+    }
+}
+
 /**
  * @brief Update the histogram from guiding center markers
  *
@@ -108,7 +183,7 @@ void dist_COM_update_gc(dist_COM_data* dist, B_field_data* Bdata,
 
             i_Ptor[i] = floor((Ptor - dist->min_Ptor)
                               /  ((dist->max_Ptor - dist->min_Ptor)/dist->n_Ptor));
-
+	    
             if(i_mu[i]   >= 0 && i_mu[i]   <= dist->n_mu - 1   &&
                i_Ekin[i] >= 0 && i_Ekin[i] <= dist->n_Ekin - 1 &&
                i_Ptor[i] >= 0 && i_Ptor[i] <= dist->n_Ptor - 1 ) {
