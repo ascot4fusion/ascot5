e1c1386e0004dd3bc6c51b701e34fb91eb2087cf
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-09-15 11:51:31 +0300

    Testing doc generation

--- doc/conf.py
+++ doc/conf.py
@@ -10,6 +10,31 @@ sys.path.insert(0, os.path.abspath('../../a5py'))
 
 import a5py
 
+from inspect import getsourcefile
+
+# Get path to directory containing this file, conf.py.
+DOCS_DIRECTORY = os.path.dirname(os.path.abspath(getsourcefile(lambda: 0)))
+
+def ensure_pandoc_installed(_):
+    import pypandoc
+
+    # Download pandoc if necessary. If pandoc is already installed and on
+    # the PATH, the installed version will be used. Otherwise, we will
+    # download a copy of pandoc into docs/bin/ and add that to our PATH.
+    pandoc_dir = os.path.join(DOCS_DIRECTORY, "bin")
+    # Add dir containing pandoc binary to the PATH environment variable
+    if pandoc_dir not in os.environ["PATH"].split(os.pathsep):
+        os.environ["PATH"] += os.pathsep + pandoc_dir
+    pypandoc.ensure_pandoc_installed(
+        quiet=True,
+        targetfolder=pandoc_dir,
+        delete_installer=True,
+    )
+
+
+def setup(app):
+    app.connect("builder-inited", ensure_pandoc_installed)
+
 # -- Project information -------------------------------------------------------
 project   = 'ASCOT5'
 copyright = '2023, Ascot Group'
