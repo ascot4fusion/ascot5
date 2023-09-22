4d1ee5f2c4011783327909325826d2c25ff08cf4
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-09-22 15:33:00 +0300

    NBI input now has a proper offload

--- src/hdf5io/hdf5_nbi.h
+++ src/hdf5io/hdf5_nbi.h
@@ -1,3 +1,7 @@
+/**
+ * @file hdf5_nbi.h
+ * @brief Header file for hdf5_nbi.c
+ */
 #ifndef HDF5_NBI_H
 #define HDF5_NBI_H
 
@@ -5,6 +9,7 @@
 #include "../ascot5.h"
 #include "../nbi.h"
 
-int hdf5_nbi_read(hid_t f, int* n_inj, nbi_injector** inj);
+int hdf5_nbi_init_offload(hid_t f, nbi_offload_data* offload_data,
+                          real** offload_array);
 
 #endif
