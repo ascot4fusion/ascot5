317c36e156a1e8aeac0320eb9ac6e24e6ed55ac1
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-10-05 22:55:08 +0300

    Imported stuff from ascot2py to libascot

--- a5py/ascotpy/libascot.py
+++ a5py/ascotpy/libascot.py
@@ -40,6 +40,10 @@ try:
     PTR_INT  = _ndpointerwithnull(ctypes.c_int,    flags="C_CONTIGUOUS")
     PTR_SIM  = ctypes.POINTER(ascot2py.struct_c__SA_sim_offload_data)
     PTR_ARR  = ctypes.POINTER(ctypes.c_double)
+    STRUCT_DIST5DOFFLOAD = ascot2py.struct_c__SA_dist_5D_offload_data
+    STRUCT_DIST5D        = ascot2py.struct_c__SA_dist_5D_data
+    STRUCT_AFSITHERMAL   = ascot2py.struct_c__SA_afsi_thermal_data
+    STRUCT_AFSIDATA      = ascot2py.struct_c__SA_afsi_data
     _LIBASCOT = ascot2py._libraries['libascot.so']
 
 except OSError as error:
@@ -48,6 +52,10 @@ except OSError as error:
     PTR_INT   = None
     PTR_SIM   = None
     PTR_ARR   = None
+    STRUCT_DIST5DOFFLOAD = None
+    STRUCT_DIST5D        = None
+    STRUCT_AFSITHERMAL   = None
+    STRUCT_AFSIDATA      = None
     msg = \
       "Failed to import libascot.so: verify that it has been compiled, it is "\
     + "located in the build folder, and all dependencies are available.\n"    \
