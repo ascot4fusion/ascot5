b2ead63ff100c2d2a975b40bfc5b4672b2ffe29d
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-10-16 17:30:30 +0300

    Fixed and cleaned tutorials

---
+++ a5py/testascot/tutorials.py
@@ -0,0 +1,39 @@
+import os
+import glob
+import subprocess
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
+
+inplace = False
+
+os.chdir("../../doc/tutorials/")
+notebooks = glob.glob("*.ipynb")
+
+preprocessor = ExecutePreprocessor(timeout=600, kernel="feature9")
+errors = {}
+for nb in notebooks:
+    if nb != "slowingdown.ipynb": continue
+    subprocess.run(["rm", "-f", "ascot.h5"])
+    with open(nb) as f:
+        nbin = nbformat.read(f, nbformat.NO_CONVERT)
+
+    try:
+        nbout = preprocessor.preprocess(nbin)
+    except CellExecutionError as err:
+        errors[nb] = err
+
+    if inplace:
+        with open(nb, "w", encoding="utf-8") as f:
+            nbformat.write(nb, f)
+
+subprocess.run(["rm", "-f", "ascot.h5"])
+if len(errors) > 0:
+    print("\nFollowing exceptions were logged:\n")
+    for f,e in errors.items():
+        print(f + ":\n")
+        print(e)
+        print("")
+    raise Exception("Failed to run tutorials.")
+
+print("Tutorials completed without errors")
