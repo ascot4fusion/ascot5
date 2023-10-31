b32c14fb58e53010872746557a502d70713a2351
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-10-31 23:23:10 +0200

    Trying to build tutorials

---
+++ .github/actions/compile/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+# https://medium.com/@janloo/github-actions-detected-dubious-ownership-in-repository-at-github-workspace-how-to-fix-b9cc127d4c04
+sh -c "git config --global --add safe.directory $PWD"
+
+echo pwd
+pwd
+
+make $1
+
+echo Complete
