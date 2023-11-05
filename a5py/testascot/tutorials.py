385889624674a0d01c2d5cc2e0e301e25f232d02
sarkimk1 <konsta.sarkimaki@aalto.fi>
2023-11-06 00:28:37 +0200

    Updated tutorials

--- a5py/testascot/tutorials.py
+++ a5py/testascot/tutorials.py
@@ -11,12 +11,11 @@ inplace = True
 os.chdir("../../doc/tutorials/")
 notebooks = glob.glob("*.ipynb")
 
-# Kernel must be specified when using a virtual environment
-#preprocessor = ExecutePreprocessor(timeout=600, kernel="feature8")
 preprocessor = ExecutePreprocessor(timeout=600)
 errors = {}
-for nb in ["wallload.ipynb"]:#notebooks:
-    if nb in ["slowingdown.ipynb", "atomic.ipynb"]: continue
+for nb in notebooks:
+    # Skip notebooks that are not standalone
+    if nb in ["atomic.ipynb"]: continue
     subprocess.run(["rm", "-f", "ascot.h5"])
     with open(nb) as f:
         nbin = nbformat.read(f, nbformat.NO_CONVERT)
