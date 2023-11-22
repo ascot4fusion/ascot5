30f49aa6972d23c16903785fece5855186982dc8
Ian Dolby <137097973+Ian-Dolby@users.noreply.github.com>
2023-11-22 10:50:38 +0000

    Create __init__.py

---
+++ a5py/templates/transp2ascot5.5/inputhelpers/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 17:41:45 2023
+
+@author: hvkg84
+"""
+
+import cocos
+import get_Bfield
+import get_Efield
+import get_plasma
+import get_neutrals
+import get_wall
+import get_markers
