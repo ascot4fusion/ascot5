1f90db9235840f159fc7395d0e47aea4a6b97de3
Simppa Akaslompolo <simppa.akaslompolo@aalto.fi>
2023-12-08 11:13:20 +0100

    Use scipy.constants for atomic/nuclear data in species

--- a5py/physlib/species.py
+++ a5py/physlib/species.py
@@ -1,33 +1,65 @@
 """List of commonly encountered marker species and their properties.
 """
 import numpy as np
-import unyt
+from unyt import kg
+import scipy.constants as constants
 
 def species(name, charge=None):
-    """Assumes the particle is fully ionized. Charge state can be given
-    explicitly with charge parameter.
+    """
+
+    Assumes the particle is fully ionized. Charge state can be given explicitly
+    with charge parameter.
     """
 
     data = None
     if name in ["e", "electron"]:
-        data = {"anum":0, "znum":0, "charge":-1*unyt.e, "mass":0.0005486*unyt.amu}
+        data = {"anum":0, "znum":0, "charge":-1, "mass":constants.electron_mass*kg}
 
     if name in ["n", "neutron"]:
-        data = {"anum":1, "znum":0, "charge":0*unyt.e,  "mass":1.009*unyt.amu}
+        data = {"anum":1, "znum":0, "charge":0,  "mass":constants.neutron_mass*kg}
 
     if name in ["p", "proton", "H", "H1"]:
-        data = {"anum":1, "znum":1, "charge":1*unyt.e,  "mass":1.007*unyt.amu}
+        data = {"anum":1, "znum":1, "charge":1,  "mass":constants.proton_mass*kg}
 
     if name in ["deuterium", "D", "H2"]:
-        data = {"anum":2, "znum":1, "charge":1*unyt.e,  "mass":2.014*unyt.amu}
+        data = {"anum":2, "znum":1, "charge":1,  "mass":constants.physical_constants['deuteron mass'][0]*kg}
 
     if name in ["tritium", "T", "H3"]:
-        data = {"anum":3, "znum":1, "charge":1*unyt.e,  "mass":3.016*unyt.amu}
+        data = {"anum":3, "znum":1, "charge":1,  "mass":constants.physical_constants['triton mass'][0]*kg}
+
+    if name in ["helion","He3"]:
+        data = {"anum":3, "znum":2, "charge":2,  "mass":constants.physical_constants['helion mass'][0]*kg}
 
     if name in ["He4", "alpha"]:
-        data = {"anum":4, "znum":2, "charge":2*unyt.e,  "mass":4.003*unyt.amu}
+        data = {"anum":4, "znum":2, "charge":2,  "mass":constants.physical_constants['alpha particle mass'][0]*kg}
 
     if charge is not None:
-        data["charge"] = charge*unyt.e
+        data["charge"] = charge
 
     return data
+
+
+def test_species():
+    for s in ['e','n','p','D','T','He3','alpha']:
+        print(s,species(s))
+
+def autodetect_species(anum,znum,charge=None):
+
+    if anum == 0 and znum == 0:
+        return species('e',charge)
+    if anum == 1:
+        if znum == 0:
+            return species('n',charge)
+        if znum == 1:
+            return species('p',charge)
+    if anum == 2 and znum == 1:
+        return species('D', charge)
+    if anum == 3:
+        if znum == 1:
+            return species('T',charge)
+        if znum == 2:
+            return species('He3',charge)
+    if anum == 4 and znum == 2:
+        return species('alpha',charge)
+
+    raise(ValueError("Unknown species anum={} znum={}".format(anum,znum)))
