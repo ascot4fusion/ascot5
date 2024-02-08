8ffa2e4112033ba1468bf7a446143462cf0d9f80
Konsta Särkimäki <konsta.sarkimaki@gmail.com>
2024-02-08 19:55:27 +0200

    Version deduced from VERSION file when git not available

--- src/gitver.sh
+++ src/gitver.sh
@@ -2,6 +2,12 @@
 GIT_BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
 GIT_VERSION="$(git describe --abbrev=7 --dirty --always --tags)"
 
+if [ -z "$GIT_VERSION" ]
+then
+    GIT_BRANCH="main"
+    GIT_VERSION=$( <VERSION)
+fi
+
 echo "#ifndef GITVER_H" > gitver.h
 echo "#define GITVER_H" >> gitver.h
 echo "" >> gitver.h
