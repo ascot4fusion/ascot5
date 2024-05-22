b0810f046127dc2b4a6e7169b07bc93764bd0c6f
Simppa Akaslompolo <simppa.akaslompolo@aalto.fi>
2024-05-22 15:45:03 +0200

    Example reading 2D eq from imas

--- a5py/_nonfunctional/distsource.py
+++ a5py/_nonfunctional/distsource.py
@@ -40,6 +40,17 @@ a5py.ascot5io.B_2DS.write_hdf5('from_imas.h5',**eqdict)
 
 #exit()
 
+print('Reading 2D equilibrium')
+eq=a5py.ascot5io.imas.B_2DS()
+eqdict=eq.read("akaslos","test","3",92436,306)
+
+#print(eqdict)
+
+print("writing 2d equilibriuem to 'from_imas.h5'.")
+a5py.ascot5io.B_2DS.write_hdf5('from_imas.h5',**eqdict)
+
+#exit()
+
 print('Reading distsource')
 # markers from distsource
 mrkr=a5py.ascot5io.imas.marker()
