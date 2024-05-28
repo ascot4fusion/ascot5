6240baa9a011e305cabb72b993652b00ea9f6053
peyberne <mathieu.peybernes@epfl.ch>
2024-05-28 08:55:27 +0200

    [GPU] add gpu data transfer files

---
+++ copytogpu.h
@@ -0,0 +1,11 @@
+/**
+ * @file copytogpu.h
+ * @brief Header file for copytogpu.c
+ */
+#ifndef COPYTOGPU_H
+#define COPYTOGPU_H
+void simulate_fo_fixed_copy_to_gpu(sim_data* sim, particle_simd_fo *p_ptr, particle_simd_fo *p0_ptr, B_field_data* Bdata, E_field_data* Edata, particle_loc*  p_loc, real* hin, real* rnd);
+
+void simulate_fo_fixed_copy_from_gpu(sim_data* sim, particle_simd_fo *p_ptr);
+
+#endif
