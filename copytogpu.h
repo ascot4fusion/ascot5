1a61a853f9f1e8ae4479355d1cd8c8653a9f522d
Konsta Särkimäki <konsta.sarkimaki@gmail.com>
2024-09-17 23:30:37 +0300

    Separated marker offload from other inputs

--- copytogpu.h
+++ copytogpu.h
@@ -4,7 +4,7 @@
  */
 #ifndef COPYTOGPU_H
 #define COPYTOGPU_H
-void simulate_fo_fixed_copy_to_gpu(sim_data* sim, particle_simd_fo *p_ptr, particle_simd_fo *p0_ptr, real* hin, real* rnd);
+void simulate_copy_to_gpu(sim_data* sim);
 
 void simulate_fo_fixed_copy_from_gpu(sim_data* sim, particle_simd_fo *p_ptr);
 
