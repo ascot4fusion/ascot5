3505396f5161224b3fbeeee02403b1d51c4824fb
Pablo Oyola <100874429+pablo-oyola@users.noreply.github.com>
2024-12-09 17:27:47 -0500

    #133 - WIP: reading of the HDF5 for RF still on the works

--- src/hdf5io/hdf5_rffields.c
+++ src/hdf5io/hdf5_rffields.c
@@ -16,12 +16,12 @@
 
 int hdf5_rffields_init(hid_t f, RF2D_fields* data, char* qid) {
     char path[256];
-    int err = 1;
+    int err = 0;
 
     #ifdef RFPATH
     #undef RFPATH
     #endif
-    #define RFPATH "/rffields/RF_2D_XXXXXXXXXX/" 
+    #define RFPATH "/rffield/rffield_XXXXXXXXXX/" 
 
     // Internal parameters to store temporarily what 
     // we read from the HDF5 file
@@ -31,17 +31,10 @@ int hdf5_rffields_init(hid_t f, RF2D_fields* data, char* qid) {
     int n_tor; // Toroidal mode number
     char tmp[256];
 
-    /* Read data the QID corresponds to */
-    hdf5_gen_path(RFPATH, qid, path);
-    if(hdf5_find_group(f, path) == 0) {
-        print_err("Error: RF_2D group not found in HDF5 file\n");
-        return 1;
-    }
-
     /* Read data */
-    if( hdf5_read_int(RFPATH "n_r", &n_r,
+    if( hdf5_read_int(RFPATH "nr", &n_r,
                       f, qid, __FILE__, __LINE__) ) {return 1;}
-    if( hdf5_read_int(RFPATH "n_z", &n_z,
+    if( hdf5_read_int(RFPATH "nz", &n_z,
                       f, qid, __FILE__, __LINE__) ) {return 1;}
     if( hdf5_read_double(RFPATH "rmin", &rmin,
                          f, qid, __FILE__, __LINE__) ) {return 1;}
@@ -51,25 +44,33 @@ int hdf5_rffields_init(hid_t f, RF2D_fields* data, char* qid) {
                          f, qid, __FILE__, __LINE__) ) {return 1;}
     if( hdf5_read_double(RFPATH "zmax", &zmax,
                          f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_double(RFPATH "omega", &omega,
+                            f, qid, __FILE__, __LINE__) ) {return 1;}
+    if( hdf5_read_int(RFPATH "ntor", &n_tor,
+                        f, qid, __FILE__, __LINE__) ) {return 1;}
     
     int nsize = n_r*n_z;
-    real* buffer = (real*) malloc(12*nsize*sizeof(real));
     real* fields[12]; // Pointers to the fields in the buffer
-    char fieldnames[12][9] = {"Er_real", "Er_imag", "Ephi_real", "Ephi_imag", "Ez_real", "Ez_imag",
-                              "Br_real", "Br_imag", "Bphi_real", "Bphi_imag", "Bz_real", "Bz_imag"
-                             };
+    char *fieldnames[] = {
+        "Er_real", "Er_imag", "Ephi_real", "Ephi_imag", "Ez_real", "Ez_imag",
+        "Br_real", "Br_imag", "Bphi_real", "Bphi_imag", "Bz_real", "Bz_imag"
+    };
 
-    memset(tmp, 0, 256*sizeof(char));
+    for(int i = 0; i < 12; i++) fields[i] = NULL;
+
     for(int i = 0; i < 12; i++) {
-        fields[i] = buffer + i*nsize;
-        strcpy(tmp, RFPATH);
-        strcat(tmp, fieldnames[i]);
+        fields[i] = (real*) malloc(nsize * sizeof(real));
+        snprintf(tmp, 256, "%s%s", RFPATH, fieldnames[i]);
         if( hdf5_read_double(tmp, fields[i],
-                             f, qid, __FILE__, __LINE__) ) {err = 1; break;}
-        memset(tmp, 0, 256*sizeof(char));
+                             f, qid, __FILE__, __LINE__) ){
+            err = 1;
+            break;
+            }
     }
     if(err) {
-        free(buffer);
+        for(int i = 0; i < 12; i++) {
+            if(fields[i] != NULL) free(fields[i]);
+        }
         return 1;
     }
 
@@ -77,7 +78,10 @@ int hdf5_rffields_init(hid_t f, RF2D_fields* data, char* qid) {
     err = rffield_init(data, rmin, rmax, n_r, zmin, zmax, n_z, n_tor, omega, \
                     fields[0], fields[1], fields[2], fields[3], fields[4], fields[5], \
                     fields[6], fields[7], fields[8], fields[9], fields[10], fields[11]);
-    free(buffer);
+
+    for(int i = 0; i < 12; i++) {
+            if(fields[i] != NULL) free(fields[i]);
+        }
 
     return err;
 
