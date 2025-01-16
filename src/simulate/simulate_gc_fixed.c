62c4e3b576a61da5261bff17537deb7d935e3ae8
jsjoona <joona.sissonen@gmail.com>
2025-01-16 20:13:27 +0200

    Fixed a few small bugs in GC ICRH

--- src/simulate/simulate_gc_fixed.c
+++ src/simulate/simulate_gc_fixed.c
@@ -26,6 +26,8 @@
 DECLARE_TARGET_SIMD_UNIFORM(sim)
 real simulate_gc_fixed_inidt(sim_data* sim, particle_simd_gc* p, int i);
 
+#define DUMMY_TIMESTEP_VAL 1.0 /**< Dummy time step value */
+
 /**
  * @brief Simulates guiding centers using fixed time-step
  *
@@ -47,11 +49,12 @@ real simulate_gc_fixed_inidt(sim_data* sim, particle_simd_gc* p, int i);
 void simulate_gc_fixed(particle_queue* pq, sim_data* sim) {
     int cycle[NSIMD]      __memalign__; /* Flag indigating whether a new marker
                                            was initialized */
-    real hin[NSIMD]       __memalign__;  // Fixed time step (used by default)
-    real hin_default[NSIMD] __memalign__;
-    //real hnext_recom[NSIMD]     __memalign__; /* Next time step (same as hin almost
-     //                                      always, except for when RFOF needs
-       //                                    smaller)                           */
+    real hin[NSIMD]       __memalign__;  /* Time step given as an input into the
+                                           integrators. Almost always default.*/
+    real hin_default[NSIMD] __memalign__; /* The default fixed time step.     */
+    real hnext_recom[NSIMD]     __memalign__; /* Next time step, only used to
+                                                store the value when RFOF has
+                                                rejected a time step.         */
     real hout_rfof[NSIMD] __memalign__; /* The time step that RFOF recommends.
                                             Small positive means that resonance
                                             is close, small negative means that
@@ -61,8 +64,6 @@ void simulate_gc_fixed(particle_queue* pq, sim_data* sim) {
                                             smaller time step given by the
                                             negative of hout_rfof             */
 
-    // Start by using the default
-    //for(int i = 0; i<NSIMD; i++) {hnext[i] = hin[i];};
 
     real cputime, cputime_last; // Global cpu time: recent and previous record
 
@@ -75,6 +76,8 @@ void simulate_gc_fixed(particle_queue* pq, sim_data* sim) {
     for(int i=0; i< NSIMD; i++) {
         p.id[i] = -1;
         p.running[i] = 0;
+        hout_rfof[i] = DUMMY_TIMESTEP_VAL;
+        hnext_recom[i] = DUMMY_TIMESTEP_VAL;
     }
 
     /* Initialize running particles */
@@ -99,6 +102,7 @@ void simulate_gc_fixed(particle_queue* pq, sim_data* sim) {
      * - Store current state
      * - Integrate motion due to background EM-field (orbit-following)
      * - Integrate scattering due to Coulomb collisions
+     * - Perform ICRH kick with RFOF if in wave-particle resonance
      * - Advance time
      * - Check for end condition(s)
      * - Update diagnostics
@@ -161,9 +165,9 @@ void simulate_gc_fixed(particle_queue* pq, sim_data* sim) {
                 if(p.running[i] && hout_rfof[i] < 0){
                     // Screwed up big time
                     p.running[i] = 0;
-                    hin[i] = hout_rfof[i];  /* Use the smaller time-step
-                                                 suggested by RFOF on the next
-                                                 round. */
+                    hnext_recom[i] = hout_rfof[i];  /* Use the smaller time-step
+                                                       suggested by RFOF on the
+                                                       next round. */
                 } else if(p.running[i]) {
                     // Everything went better than expected
                     hin[i] = hin_default[i];  // use the original fixed step
@@ -178,13 +182,16 @@ void simulate_gc_fixed(particle_queue* pq, sim_data* sim) {
         cputime = A5_WTIME;
         #pragma omp simd
         for(int i = 0; i < NSIMD; i++) {
+            if(hnext_recom[i] < 0) {
+                /* Screwed up big time (negative time-step only when RFOF
+                    rejected) */
+                particle_copy_gc(&p0, i, &p, i);
+                hin[i] = -hnext_recom[i];
+            }
             if(p.running[i]) {
-                // Check whether the time-step was ok
-                if(hin[i] < 0) {
-                    /* Screwed up big time (negative time-step only when RFOF
-                       rejected) */
-                    particle_copy_gc(&p0, i, &p, i);
-                    hin[i] = -hin[i];
+                if(hnext_recom[i] < 0) {
+                    // unsuccessful step, only reset the recommendation
+                    hnext_recom[i] = DUMMY_TIMESTEP_VAL;
                 } else {
                     // The step was successful
                     p.time[i]    += ( 1.0 - 2.0 * ( sim->reverse_time > 0 ) ) * hin[i];
