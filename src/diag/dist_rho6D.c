9cba06d8f8ce41eb143c6bb49d58b1ed3c8457aa
Konsta Särkimäki <konsta.sarkimaki@gmail.com>
2025-04-04 16:07:09 +0300

    The atomic clause inside SIMD loop in distributions was moved outside the loop when GPU=0

--- src/diag/dist_rho6D.c
+++ src/diag/dist_rho6D.c
@@ -150,7 +150,6 @@ void dist_rho6D_update_fo(dist_rho6D_data* dist, particle_simd_fo* p_f,
         }
     }
 #ifndef GPU
-    GPU_PARALLEL_LOOP_ALL_LEVELS
     for(int i = 0; i < p_f->n_mrk; i++) {
         if(p_f->running[i]) {
             GPU_ATOMIC
