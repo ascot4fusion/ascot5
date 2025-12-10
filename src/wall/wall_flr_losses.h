0a271e95382c4e93b64052808fddaa534fef748b
Pablo Oyola <100874429+pablo-oyola@users.noreply.github.com>
2025-12-10 09:53:09 -0500

    Changed tabs into spaces.

--- src/wall/wall_flr_losses.h
+++ src/wall/wall_flr_losses.h
@@ -1,10 +1,10 @@
 /**
- * @file flr_losses.h
- * @brief Header file for flr_losses.c. Contains the routines to 
+ * @file wall_flr_losses.h
+ * @brief Header file for wall_flr_losses.c. Contains the routines to 
  * determine the FLR losses of GC markers.
  */
-#ifndef FLR_LOSSES_H
-#define FLR_LOSSES_H
+#ifndef WALL_FLR_LOSSES_H
+#define WALL_FLR_LOSSES_H
 
 #include "../ascot5.h"
 #include "../offload.h"
@@ -16,9 +16,9 @@
 
 GPU_DECLARE_TARGET_SIMD_UNIFORM(B, wall, rnd)
 int flr_losses_eval(real r, real phi, real z, real ppar, real mu,
-					real mass, real charge, real time,
-					B_field_data* B, wall_data* wall, random_data* rnd,
-					int* walltile_out, int* err_out);
+                    real mass, real charge, real time,
+                    B_field_data* B, wall_data* wall, random_data* rnd,
+                    int* walltile_out, int* err_out);
 GPU_DECLARE_TARGET_SIMD_UNIFORM_END
 
 #endif 
