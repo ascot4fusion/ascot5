5a030a35dde5ac89091aed54bcbf7bcad351acd3
Konsta Särkimäki <konsta.sarkimaki@gmail.com>
2026-02-06 10:13:02 +0200

    Fixed suzuki model for impurities #180

--- src/suzuki.c
+++ src/suzuki.c
@@ -52,17 +52,19 @@ real A_lowE[3][10] = {
 /**
  * @brief Charge number corresponding to the tabulated values of Bijk
  */
-integer Z_imp[]    = {   2,   6,   6,   4,   8,   7,   3,   5,  26 };
+integer Z_imp[]    = {   2,   6,   6,   8,   7,   5,  26 };
 
 /**
  * @brief Minimum valid Zeff corresponding to the tabulated values of Bijk
  */
-real Zeffmin_imp[] = { 1.0, 1.0, 5.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0 };
+real Zeffmin_imp[] = { 1.0, 1.0, 5.0, 1.0, 1.0, 1.0, 1.0 };
 
 /**
  * @brief Maximum valid Zeff corresponding to the tabulated values of Bijk
+ *
+ * See the note in the high energy table.
  */
-real Zeffmax_imp[] = { 2.1, 5.0, 6.0, 4.0, 5.0, 5.0, 3.0, 5.0, 5.0 };
+real Zeffmax_imp[] = { 2.1, 5.0, 6.0, 5.0, 5.0, 5.0, 5.0 };
 
 /**
  * @brief Fitting parameters for Bijk in equation 28
@@ -70,22 +72,24 @@ real Zeffmax_imp[] = { 2.1, 5.0, 6.0, 4.0, 5.0, 5.0, 3.0, 5.0, 5.0 };
  * Table 2bc (partially) from Suzuki's paper for H, D, and T.
  *
  * These values are valid in range 100 < E (keV/amu) < 10000.
+ *
+ * See the note in the high energy table.
  */
-real B_highE[9][12] = {
+real B_highE[7][12] = {
     {  0.231,     0.343,    -0.185,    -0.162e-1,  0.105,    -0.703e-1,
        0.531e-1,  0.342e-2, -0.838e-2,  0.415e-2, -0.335e-2, -0.221e-3 },
     { -0.101e1,  -0.865e-2, -0.124,    -0.145e-1,  0.391,     0.161e-1,
        0.298e-1,  0.332e-2, -0.248e-1, -0.104e-2, -0.152e-2, -0.189e-3 },
     { -0.100e1,  -0.255e-1, -0.125,    -0.142e-1,  0.388,     0.206e-1,
        0.297e-1,  0.326e-2, -0.246e-1, -0.131e-2, -0.148e-2, -0.180e-3 },
-    { -0.613,     0.552e-1, -0.167,    -0.159e-1,  0.304,     0.154e-2,
-       0.436e-1,  0.378e-2, -0.201e-1, -0.216e-3, -0.251e-2, -0.227e-3 },
+    //{ -0.613,     0.552e-1, -0.167,    -0.159e-1,  0.304,     0.154e-2,
+    //   0.436e-1,  0.378e-2, -0.201e-1, -0.216e-3, -0.251e-2, -0.227e-3 },
     { -0.102e1,  -0.148e-1, -0.674e-1, -0.917e-2,  0.359,     0.143e-1,
        0.139e-1,  0.184e-2, -0.209e-1, -0.732e-3, -0.502e-3, -0.949e-4 },
     { -0.102e1,  -0.139e-1, -0.979e-1, -0.117e-1,  0.375,     0.156e-1,
        0.224e-1,  0.254e-2, -0.226e-1, -0.889e-3, -0.104e-2, -0.139e-3 },
-    { -0.441,     0.129,    -0.170,    -0.162e-1,  0.277,    -0.156e-1,
-       0.466e-1,  0.379e-2, -0.193e-1,  0.753e-3, -0.286e-2, -0.239e-3 },
+    //{ -0.441,     0.129,    -0.170,    -0.162e-1,  0.277,    -0.156e-1,
+    //   0.466e-1,  0.379e-2, -0.193e-1,  0.753e-3, -0.286e-2, -0.239e-3 },
     { -0.732,     0.183e-1, -0.155,    -0.172e-1,  0.321,     0.946e-2,
        0.397e-1,  0.420e-2, -0.204e-1, -0.619e-3, -0.224e-2, -0.254e-3 },
     { -0.820,    -0.636e-2,  0.542e-1,  0.395e-2,  0.202,     0.806e-3,
@@ -98,22 +102,25 @@ real B_highE[9][12] = {
  * Table 3bc (partially) from Suzuki's paper for H, D, and T.
  *
  * These values are valid in range 10 < E (keV/amu) < 100.
+ *
+ * Note: Li and Be are same in Suzuki's paper. Until it is known what the
+ * correct values are, these are disabled.
  */
-real B_lowE[9][12] = {
+real B_lowE[7][12] = {
     { -0.792,     0.420e-1,  0.530e-1,  -0.139e-1,  0.301,    -0.264e-1,
       -0.299e-1,  0.607e-2,  0.272e-3,   0.611e-2,  0.347e-2, -0.919e-3 },
     {  0.161,     0.598e-1, -0.336e-2,  -0.426e-2, -0.157,    -0.396e-1,
        0.460e-2,  0.219e-2,  0.391e-1,   0.711e-2, -0.144e-2, -0.385e-3 },
     {  0.158,     0.554e-1, -0.431e-2,  -0.335e-2, -0.155,    -0.374e-1,
        0.537e-2,  0.174e-2,  0.388e-1,   0.683e-2, -0.160e-2, -0.322e-3 },
-    {  0.112,     0.495e-1,  0.116e-1,  -0.286e-2, -0.149,    -0.331e-1,
-      -0.426e-2,  0.980e-3,  0.447e-1,   0.652e-2, -0.356e-3, -0.203e-3 },
+    //{  0.112,     0.495e-1,  0.116e-1,  -0.286e-2, -0.149,    -0.331e-1,
+    //  -0.426e-2,  0.980e-3,  0.447e-1,   0.652e-2, -0.356e-3, -0.203e-3 },
     {  0.111,     0.541e-1, -0.346e-3,  -0.368e-2, -0.108,    -0.347e-1,
        0.193e-2,  0.181e-2,  0.280e-1,   0.604e-2, -0.841e-3, -0.317e-3 },
     {  0.139,     0.606e-1, -0.306e-2,  -0.455e-2, -0.133,    -0.394e-1,
        0.399e-2,  0.236e-2,  0.335e-1,   0.690e-2, -0.124e-2, -0.405e-3 },
-    {  0.112,     0.495e-1,  0.116e-1,  -0.286e-2, -0.149,    -0.331e-1,
-      -0.426e-2,  0.980e-3,  0.447e-1,   0.652e-2, -0.356e-3, -0.203e-3 },
+    //{  0.112,     0.495e-1,  0.116e-1,  -0.286e-2, -0.149,    -0.331e-1,
+    //  -0.426e-2,  0.980e-3,  0.447e-1,   0.652e-2, -0.356e-3, -0.203e-3 },
     {  0.122,     0.527e-1, -0.430e-3,  -0.318e-2, -0.151,    -0.364e-1,
        0.343e-2,  0.151e-2,  0.420e-1,   0.692e-2, -0.141e-2, -0.290e-3 },
     { -0.110e-1,  0.202e-1,  0.946e-3,  -0.409e-2, -0.666e-2, -0.117e-1,
@@ -208,8 +215,8 @@ a5err suzuki_sigmav(real* sigmav, real EperAmu, real vnorm, real ne, real te,
     }
     sigma_H /= dens_H;
 
-    /* Equations 26 & 27 for Sz */
-    real sigma_Z = 0.0;
+    /* Equations 26 & 27 for Sz with Rui's correction for the sum */
+    real sigma_Z = 0, numerator = 0, denominator = 0;
     for(int i = 0; i < n_Z; i++) {
         int ind_B = -1;
         for(int j = 0; j < 9; j++) {
@@ -222,7 +229,8 @@ a5err suzuki_sigmav(real* sigmav, real EperAmu, real vnorm, real ne, real te,
             /* Data for the input species not tabulated */
             err = error_raise( ERR_INPUT_EVALUATION, __LINE__, EF_SUZUKI );
         }
-        sigma_Z += ni[ind_Z[i]] / ne * znum[ind_Z[i]]
+        denominator += znum[ind_Z[i]] * (znum[ind_Z[i]] - 1) * ni[ind_Z[i]];
+        numerator += znum[ind_Z[i]] * (znum[ind_Z[i]] - 1) * ni[ind_Z[i]]
             * (B[ind_B][0]
                + B[ind_B][1]  * U
                + B[ind_B][2]  * logN
@@ -236,6 +244,7 @@ a5err suzuki_sigmav(real* sigmav, real EperAmu, real vnorm, real ne, real te,
                + B[ind_B][10] * logE * logE * logN
                + B[ind_B][11] * logE * logE * logN * U);
     }
+    sigma_Z = numerator / denominator;
 
     /* Equation 24 and convert cm^2 to m^2*/
     *sigmav = sigma_H * (1 + (Zeff - 1) * sigma_Z) * 1e-4;
